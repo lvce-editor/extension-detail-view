a912feaad2647e5ec33b8a821b6a7c8dca5a7c51
Le Vivilet <levivlet@pm.me>
2024-12-03 20:25:02 +0100

    lint

--- packages/extension-detail-view-worker/src/parts/ErrorHandling/ErrorHandling.ts
+++ packages/extension-detail-view-worker/src/parts/ErrorHandling/ErrorHandling.ts
@@ -1,7 +1,7 @@
-export const logError = async (error: any, prefix = '') => {
+export const logError = async (error: any, _prefix = '') => {
   console.error(error)
 }
 
-export const handleError = async (error: any, notify = true, prefix = '') => {
+export const handleError = async (error: any, _notify = true, _prefix = '') => {
   console.error(error)
 }
--- packages/extension-detail-view-worker/src/parts/RenderMarkdown/RenderMarkdown.ts
+++ packages/extension-detail-view-worker/src/parts/RenderMarkdown/RenderMarkdown.ts
@@ -1,6 +1,6 @@
 import * as marked from 'marked'
 
-export const renderMarkdown = async (markdown: string, { baseUrl = '' } = {}) => {
+export const renderMarkdown = async (markdown: string, {} = {}) => {
   const html = await marked.marked(markdown, {})
   return html
 }
