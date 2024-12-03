d7bae512264c52335ef63e5e830e684dc4789f4a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-03 22:20:35 +0100

    feature: add getExtensionDetailVirtualDom function (#13)
    * feature: add getExtensionDetailVirtualDom function
    
    * lint
    
    * memory

--- packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,11 @@
+import * as GetExtensionDetailVirtualDom from '../GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts'
 import * as HandleIconError from '../HandleIconError/HandleIconError.ts'
-import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
+import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const commandMap = {
   'RenderMarkdown.renderMarkdown': RenderMarkdown.renderMarkdown,
   'HandleIconError.handleIconError': HandleIconError.handleIconError,
   'ExtensionDetail.loadContent': LoadContent.loadContent,
+  'ExtensionDetail.getVirtualDom': GetExtensionDetailVirtualDom.getExtensionDetailVirtualDom,
 }
---
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts
@@ -0,0 +1,30 @@
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as GetExtensionDetailHeaderVirtualDom from '../GetExtensionDetailHeaderVirtualDom/GetExtensionDetailHeaderVirtualDom.ts'
+import * as GetMarkdownVirtualDom from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetVirtualDomChildCount from '../GetVirtualDomChildCount/GetVirtualDomChildCount.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getExtensionDetailVirtualDom = (extensionDetail: any, sanitizedReadmeHtml: any) => {
+  const markdownDom = GetMarkdownVirtualDom.getMarkdownVirtualDom(sanitizedReadmeHtml)
+  const childCount = GetVirtualDomChildCount.getVirtualDomChildCount(markdownDom)
+  const dom = [
+    {
+      type: VirtualDomElements.Div,
+      className: 'Viewlet ExtensionDetail',
+      childCount: childCount + 1,
+    },
+
+    ...GetExtensionDetailHeaderVirtualDom.getExtensionDetailHeaderVirtualDom(extensionDetail),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Markdown,
+      role: AriaRoles.Document,
+      onContextMenu: DomEventListenerFunctions.HandleReadmeContextMenu,
+      childCount,
+    },
+    ...markdownDom,
+  ]
+  return dom
+}
