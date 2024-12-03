df664ccedc2606bbe85dcb16a7c95624c839003b
Le Vivilet <levivlet@pm.me>
2024-12-03 20:20:14 +0100

    feature: configure monorepo

---
+++ packages/test-integration/test/create-search.test.js
@@ -0,0 +1,17 @@
+import { test } from '@jest/globals'
+import { setup } from '../src/test.js'
+
+test.skip('open-detail-view', async () => {
+  const rpc = await setup()
+  const uid = 1
+  const x = 0
+  const y = 0
+  const width = 0
+  const height = 0
+  const workspacePath = ''
+  const assetDir = ''
+  const itemHeight = 22
+  const value = ''
+  const replacement = ''
+  await rpc.invoke('TextSearch.create', uid, x, y, width, height, workspacePath, assetDir, itemHeight, value, replacement)
+})
