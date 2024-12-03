8acce7e61aa71ed2f3fca7859d6d4abc0369ccba
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-03 21:15:01 +0100

    feature: add markdown integration test (#9)

---
+++ packages/test-integration/test/render-markdown-error-content-is-not-of-type-string.test.js
@@ -0,0 +1,11 @@
+import { expect, test } from '@jest/globals'
+import { setup } from '../src/test.js'
+
+test('render-markdown - error - content is not of type string', async () => {
+  const rpc = await setup()
+  const content = undefined
+  await expect(rpc.invoke('RenderMarkdown.renderMarkdown', content)).rejects.toThrow(
+    new Error(`marked(): input parameter is undefined or null
+Please report this to https://github.com/markedjs/marked.`),
+  )
+}, 10_000)
