16f9198cd2c0f98b4888dc92863dc8d5fea151ac
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-03 22:28:42 +0100

    feature: add getMenuEntries function (#12)
    * feature: add getmenuEntries function
    
    * tsc
    
    * tsc

--- packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,4 +1,5 @@
 import * as GetExtensionDetailVirtualDom from '../GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts'
+import * as GetMenuEntries from '../GetMenuEntries/GetMenuEntries.ts'
 import * as HandleIconError from '../HandleIconError/HandleIconError.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
@@ -7,5 +8,6 @@ export const commandMap = {
   'RenderMarkdown.renderMarkdown': RenderMarkdown.renderMarkdown,
   'HandleIconError.handleIconError': HandleIconError.handleIconError,
   'ExtensionDetail.loadContent': LoadContent.loadContent,
+  'ExtensionDetail.getMenuEntries': GetMenuEntries.getMenuEntries,
   'ExtensionDetail.getVirtualDom': GetExtensionDetailVirtualDom.getExtensionDetailVirtualDom,
 }
