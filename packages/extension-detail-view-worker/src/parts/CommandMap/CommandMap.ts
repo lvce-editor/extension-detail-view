3ef568cc26c9a53b8a298dc8a2758320da551c3d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-03 22:09:05 +0100

    feature: add handleIconError function (#11)

--- packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
+import * as HandleIconError from '../HandleIconError/HandleIconError.ts'
 import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 
 export const commandMap = {
   'RenderMarkdown.renderMarkdown': RenderMarkdown.renderMarkdown,
+  'HandleIconError.handleIconError': HandleIconError.handleIconError,
   'ExtensionDetail.loadContent': LoadContent.loadContent,
 }
