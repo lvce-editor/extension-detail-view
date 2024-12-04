7b8d75671b7241e9f00d014ee6370905cd36b167
Le Vivilet <levivlet@pm.me>
2024-12-04 21:51:18 +0100

    feature: use classnames enum

--- packages/extension-detail-view-worker/src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts
@@ -1,9 +1,10 @@
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetExtensionDetailHeaderVirtualDom from '../GetExtensionDetailHeaderVirtualDom/GetExtensionDetailHeaderVirtualDom.ts'
 import * as GetMarkdownVirtualDom from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetVirtualDomChildCount from '../GetVirtualDomChildCount/GetVirtualDomChildCount.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getExtensionDetailVirtualDom = (extensionDetail: any, sanitizedReadmeHtml: any) => {
@@ -12,7 +13,7 @@ export const getExtensionDetailVirtualDom = (extensionDetail: any, sanitizedRead
   const dom = [
     {
       type: VirtualDomElements.Div,
-      className: 'Viewlet ExtensionDetail',
+      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.ExtensionDetail),
       childCount: childCount + 1,
     },
 
