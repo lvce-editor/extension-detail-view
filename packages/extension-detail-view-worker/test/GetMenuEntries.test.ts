61fd954448ab47d2cd4acd2a01812e166b077f04
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-04 22:04:30 +0100

    feature: add getMenuEntries test (#35)

---
+++ packages/extension-detail-view-worker/test/GetMenuEntries.test.ts
@@ -0,0 +1,58 @@
+import { expect, test } from '@jest/globals'
+import * as GetMenuEntries from '../src/parts/GetMenuEntries/GetMenuEntries.ts'
+import * as MenuItemFlags from '../src/parts/MenuItemFlags/MenuItemFlags.ts'
+import * as ExtensionDetailStrings from '../src/parts/ExtensionDetailStrings/ExtensionDetailStrings.ts'
+
+test('handles isLink', () => {
+  expect(GetMenuEntries.getMenuEntries({ isLink: true })).toEqual([
+    {
+      id: 'openInNewTab',
+      label: ExtensionDetailStrings.openImageInNewTab(),
+      flags: MenuItemFlags.None,
+      command: 'Open.openUrl',
+      args: [undefined],
+    },
+    {
+      id: 'copy',
+      label: ExtensionDetailStrings.copy(),
+      flags: MenuItemFlags.None,
+      command: 'ClipBoard.execCopy',
+    },
+  ])
+})
+
+test('handles isImage', () => {
+  expect(GetMenuEntries.getMenuEntries({ isImage: true })).toEqual([
+    {
+      id: 'openImageInNewTab',
+      label: ExtensionDetailStrings.openImageInNewTab(),
+      flags: MenuItemFlags.None,
+      command: 'Open.openUrl',
+      args: [undefined],
+    },
+    {
+      id: 'saveImageAs',
+      label: ExtensionDetailStrings.saveImageAs(),
+      flags: MenuItemFlags.None,
+      command: 'SaveFileAs.saveFileAs',
+      args: ['image.png', undefined],
+    },
+    {
+      id: 'copy',
+      label: ExtensionDetailStrings.copy(),
+      flags: MenuItemFlags.None,
+      command: 'ClipBoard.execCopy',
+    },
+  ])
+})
+
+test('handles neither isLink nor isImage', () => {
+  expect(GetMenuEntries.getMenuEntries({})).toEqual([
+    {
+      id: 'copy',
+      label: ExtensionDetailStrings.copy(),
+      flags: MenuItemFlags.None,
+      command: 'ClipBoard.execCopy',
+    },
+  ])
+})
