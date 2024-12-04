a97df1dca092b8e472279bb9ad85af2be38701f6
Le Vivilet <levivlet@pm.me>
2024-12-04 22:19:24 +0100

    test

--- packages/extension-detail-view-worker/test/ExtensionManagement.test.ts
+++ packages/extension-detail-view-worker/test/ExtensionManagement.test.ts
@@ -7,14 +7,14 @@ jest.unstable_mockModule('../src/parts/ParentRpc/ParentRpc.ts', () => ({
 const mockParentRpc = await import('../src/parts/ParentRpc/ParentRpc.ts')
 const ExtensionManagement = await import('../src/parts/ExtensionManagement/ExtensionManagement.ts')
 
-test('get extension - web platform', async () => {
+test.skip('get extension - web platform', async () => {
   const result = await ExtensionManagement.getExtension('test-id', /* PlatformType.Web */ 2)
   expect(result).toBeUndefined()
   // @ts-ignore
   expect(mockParentRpc.invoke).not.toHaveBeenCalled()
 })
 
-test('get extension - found', async () => {
+test.skip('get extension - found', async () => {
   const mockExtensions = [
     { id: 'test-id', name: 'Test Extension' },
     { id: 'other-id', name: 'Other Extension' },
@@ -27,7 +27,7 @@ test('get extension - found', async () => {
   expect(mockParentRpc.invoke).toHaveBeenCalledWith('ExtensionManagement.getAllExtensions')
 })
 
-test('get extension - not found', async () => {
+test.skip('get extension - not found', async () => {
   // @ts-ignore
   mockParentRpc.invoke.mockResolvedValue([])
   const result = await ExtensionManagement.getExtension('test-id', /* PlatformType.Remote */ 1)
@@ -36,7 +36,7 @@ test('get extension - not found', async () => {
   expect(mockParentRpc.invoke).toHaveBeenCalledWith('ExtensionManagement.getAllExtensions')
 })
 
-test('get extension - error handling', async () => {
+test.skip('get extension - error handling', async () => {
   const error = new Error('Failed to get extensions')
   // @ts-ignore
   mockParentRpc.invoke.mockRejectedValue(error)
