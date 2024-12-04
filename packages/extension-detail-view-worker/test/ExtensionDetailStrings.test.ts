613dcf2ccd6aadaae7758d488d22cf37bc486d1e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-04 22:37:46 +0100

    feature: use explicit return types (#51)

--- packages/extension-detail-view-worker/test/ExtensionDetailStrings.test.ts
+++ packages/extension-detail-view-worker/test/ExtensionDetailStrings.test.ts
@@ -16,10 +16,3 @@ test('openImageInNewTab', () => {
 test('saveImageAs', () => {
   expect(ExtensionDetailStrings.saveImageAs()).toBe('Save Image as')
 })
-
-test('UiStrings enum values', () => {
-  expect(ExtensionDetailStrings.UiStrings.Copy).toBe('Copy')
-  expect(ExtensionDetailStrings.UiStrings.OpenInNewTab).toBe('Open in New Tab')
-  expect(ExtensionDetailStrings.UiStrings.OpenImageInNewTab).toBe('Open Image in New Tab')
-  expect(ExtensionDetailStrings.UiStrings.SaveImageAs).toBe('Save Image as')
-})
