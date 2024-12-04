0acc146faa1a666c54f0652543cea81649da83e5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-04 21:55:29 +0100

    feature: add more getVirtualDomTag tests (#28)

--- packages/extension-detail-view-worker/test/GetVirtualDomTag.test.ts
+++ packages/extension-detail-view-worker/test/GetVirtualDomTag.test.ts
@@ -1,33 +1,54 @@
 import { expect, test } from '@jest/globals'
 import * as GetVirtualDomTag from '../src/parts/GetVirtualDomTag/GetVirtualDomTag.ts'
 import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import * as ElementTags from '../src/parts/ElementTags/ElementTags.ts'
 
-test('h1', () => {
-  expect(GetVirtualDomTag.getVirtualDomTag('h1')).toBe(VirtualDomElements.H1)
+test('heading tags', () => {
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.H1)).toBe(VirtualDomElements.H1)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.H2)).toBe(VirtualDomElements.H2)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.H3)).toBe(VirtualDomElements.H3)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.H4)).toBe(VirtualDomElements.H4)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.H5)).toBe(VirtualDomElements.H5)
 })
 
-test('h2', () => {
-  expect(GetVirtualDomTag.getVirtualDomTag('h2')).toBe(VirtualDomElements.H2)
+test('structural tags', () => {
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Div)).toBe(VirtualDomElements.Div)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Article)).toBe(VirtualDomElements.Article)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Section)).toBe(VirtualDomElements.Section)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Nav)).toBe(VirtualDomElements.Nav)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Header)).toBe(VirtualDomElements.Header)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Footer)).toBe(VirtualDomElements.Footer)
 })
 
-test('h3', () => {
-  expect(GetVirtualDomTag.getVirtualDomTag('h3')).toBe(VirtualDomElements.H3)
+test('inline tags', () => {
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Span)).toBe(VirtualDomElements.Span)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.A)).toBe(VirtualDomElements.A)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Img)).toBe(VirtualDomElements.Img)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Br)).toBe(VirtualDomElements.Br)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Abbr)).toBe(VirtualDomElements.Abbr)
 })
 
-test('div', () => {
-  expect(GetVirtualDomTag.getVirtualDomTag('div')).toBe(VirtualDomElements.Div)
+test('list tags', () => {
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Ol)).toBe(VirtualDomElements.Ol)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Li)).toBe(VirtualDomElements.Li)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Dl)).toBe(VirtualDomElements.Dl)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Dd)).toBe(VirtualDomElements.Dd)
 })
 
-test('article', () => {
-  expect(GetVirtualDomTag.getVirtualDomTag('article')).toBe(VirtualDomElements.Article)
+test('text content tags', () => {
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.P)).toBe(VirtualDomElements.P)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Pre)).toBe(VirtualDomElements.Pre)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Cite)).toBe(VirtualDomElements.Cite)
 })
 
-test('img', () => {
-  expect(GetVirtualDomTag.getVirtualDomTag('img')).toBe(VirtualDomElements.Img)
+test('figure tags', () => {
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Figure)).toBe(VirtualDomElements.Figure)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Figcaption)).toBe(VirtualDomElements.Figcaption)
 })
 
-test('span', () => {
-  expect(GetVirtualDomTag.getVirtualDomTag('span')).toBe(VirtualDomElements.Span)
+test('data tags', () => {
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Time)).toBe(VirtualDomElements.Time)
+  expect(GetVirtualDomTag.getVirtualDomTag(ElementTags.Data)).toBe(VirtualDomElements.Data)
 })
 
 test('unknown tag defaults to div', () => {
