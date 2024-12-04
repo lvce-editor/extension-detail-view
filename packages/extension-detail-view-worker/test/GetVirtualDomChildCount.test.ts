200de58decee6428c68b7fa35c2cfab5b821b606
Le Vivilet <levivlet@pm.me>
2024-12-04 21:40:18 +0100

    feature: add dom child count test

---
+++ packages/extension-detail-view-worker/test/GetVirtualDomChildCount.test.ts
@@ -0,0 +1,71 @@
+import { expect, test } from '@jest/globals'
+import * as GetVirtualDomChildCount from '../src/parts/GetVirtualDomChildCount/GetVirtualDomChildCount.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+
+test('empty array', () => {
+  expect(GetVirtualDomChildCount.getVirtualDomChildCount([])).toBe(0)
+})
+
+test('single element without children', () => {
+  const dom = [
+    {
+      type: VirtualDomElements.Div,
+      childCount: 0,
+    },
+  ]
+  expect(GetVirtualDomChildCount.getVirtualDomChildCount(dom)).toBe(1)
+})
+
+test('element with one child', () => {
+  const dom = [
+    {
+      type: VirtualDomElements.Div,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Span,
+      childCount: 0,
+    },
+  ]
+  expect(GetVirtualDomChildCount.getVirtualDomChildCount(dom)).toBe(2)
+})
+
+test('element with two children', () => {
+  const dom = [
+    {
+      type: VirtualDomElements.Div,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Span,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Span,
+      childCount: 0,
+    },
+  ]
+  expect(GetVirtualDomChildCount.getVirtualDomChildCount(dom)).toBe(3)
+})
+
+test('nested elements', () => {
+  const dom = [
+    {
+      type: VirtualDomElements.Div,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Div,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Span,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Span,
+      childCount: 0,
+    },
+  ]
+  expect(GetVirtualDomChildCount.getVirtualDomChildCount(dom)).toBe(4)
+})
