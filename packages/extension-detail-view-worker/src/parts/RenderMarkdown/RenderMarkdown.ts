0b16f4b549b33f20d334739693ac305765a285ec
Le Vivilet <levivlet@pm.me>
2024-12-04 22:36:53 +0100

    feature: use explicit return types

--- packages/extension-detail-view-worker/src/parts/RenderMarkdown/RenderMarkdown.ts
+++ packages/extension-detail-view-worker/src/parts/RenderMarkdown/RenderMarkdown.ts
@@ -1,6 +1,6 @@
 import * as marked from 'marked'
 
-export const renderMarkdown = async (markdown: string, options: { baseUrl?: string } = {}) => {
+export const renderMarkdown = async (markdown: string, options: { baseUrl?: string } = {}): Promise<string> => {
   const html = await marked.marked(markdown, {})
   return html
 }
