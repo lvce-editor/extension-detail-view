28f836084276921fd0ecaaa3d554308b36e19f1c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-04 21:50:36 +0100

    feature: add isEnoentError tests (#26)
    * feature: add isEnoentError tests
    
    * test

---
+++ packages/extension-detail-view-worker/test/IsEnoentError.test.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@jest/globals'
+import * as IsEnoentError from '../src/parts/IsEnoentError/IsEnoentError.ts'
+import * as ErrorCodes from '../src/parts/ErrorCodes/ErrorCodes.ts'
+
+test('returns true for ENOENT error', () => {
+  const error = new Error('file not found')
+  // @ts-ignore
+  error.code = ErrorCodes.ENOENT
+  expect(IsEnoentError.isEnoentError(error)).toBe(true)
+})
+
+test('returns false for non-ENOENT error', () => {
+  const error = new Error('some other error')
+  // @ts-ignore
+  error.code = 'OTHER_ERROR'
+  expect(IsEnoentError.isEnoentError(error)).toBe(false)
+})
+
+test('returns false for error without code', () => {
+  const error = new Error('error without code')
+  expect(IsEnoentError.isEnoentError(error)).toBe(false)
+})
+
+test('returns false for null', () => {
+  expect(IsEnoentError.isEnoentError(null)).toBeFalsy()
+})
+
+test('returns false for undefined', () => {
+  expect(IsEnoentError.isEnoentError(undefined)).toBeFalsy()
+})
