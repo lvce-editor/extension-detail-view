0c0fa019fb6b57cacfdbbb616529fef82522b63a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-04 21:55:13 +0100

    feature: add extension detail header dom test (#29)

---
+++ packages/extension-detail-view-worker/test/GetExtensionDetailHeaderVirtualDom.test.ts
@@ -0,0 +1,85 @@
+import { expect, test } from '@jest/globals'
+import * as GetExtensionDetailHeaderVirtualDom from '../src/parts/GetExtensionDetailHeaderVirtualDom/GetExtensionDetailHeaderVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
+
+test('extension detail header virtual dom', () => {
+  const extensionDetail = {
+    name: 'Test Extension',
+    iconSrc: './test-icon.png',
+    description: 'Test Description',
+  }
+  expect(GetExtensionDetailHeaderVirtualDom.getExtensionDetailHeaderVirtualDom(extensionDetail)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ExtensionDetailHeader,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Img,
+      className: ClassNames.ExtensionDetailIcon,
+      alt: '',
+      draggable: false,
+      childCount: 0,
+      src: './test-icon.png',
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ExtensionDetailHeaderDetails,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ExtensionDetailName,
+      childCount: 1,
+    },
+    text('Test Extension'),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ExtensionDetailDescription,
+      childCount: 1,
+    },
+    text('Test Description'),
+  ])
+})
+
+test('handles missing extension details', () => {
+  const extensionDetail = {
+    name: '',
+    iconSrc: '',
+    description: '',
+  }
+  expect(GetExtensionDetailHeaderVirtualDom.getExtensionDetailHeaderVirtualDom(extensionDetail)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ExtensionDetailHeader,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Img,
+      className: ClassNames.ExtensionDetailIcon,
+      alt: '',
+      draggable: false,
+      childCount: 0,
+      src: '',
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ExtensionDetailHeaderDetails,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ExtensionDetailName,
+      childCount: 1,
+    },
+    text(''),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ExtensionDetailDescription,
+      childCount: 1,
+    },
+    text(''),
+  ])
+})
