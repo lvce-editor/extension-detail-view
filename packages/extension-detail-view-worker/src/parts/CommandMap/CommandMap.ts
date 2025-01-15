9b4b6d32761f10c5c6a91b793130112e6b1537b0
Le Vivilet <levivlet@pm.me>
2025-01-15 22:17:13 +0100

    feature: add terminate command

--- packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
@@ -3,11 +3,13 @@ import * as GetMenuEntries from '../GetMenuEntries/GetMenuEntries.ts'
 import * as HandleIconError from '../HandleIconError/HandleIconError.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
+import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
-  'RenderMarkdown.renderMarkdown': RenderMarkdown.renderMarkdown,
-  'HandleIconError.handleIconError': HandleIconError.handleIconError,
-  'ExtensionDetail.loadContent': LoadContent.loadContent,
   'ExtensionDetail.getMenuEntries': GetMenuEntries.getMenuEntries,
   'ExtensionDetail.getVirtualDom': GetExtensionDetailVirtualDom.getExtensionDetailVirtualDom,
+  'ExtensionDetail.loadContent': LoadContent.loadContent,
+  'ExtensionDetail.terminate': Terminate.terminate,
+  'HandleIconError.handleIconError': HandleIconError.handleIconError,
+  'RenderMarkdown.renderMarkdown': RenderMarkdown.renderMarkdown,
 }
