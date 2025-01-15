30e73d686465ec3165c1e6f56f140611ad4ed666
Le Vivilet <levivlet@pm.me>
2025-01-15 22:26:24 +0100

    feature: move uistrings into separate file

--- packages/extension-detail-view-worker/src/parts/ExtensionDetailStrings/ExtensionDetailStrings.ts
+++ packages/extension-detail-view-worker/src/parts/ExtensionDetailStrings/ExtensionDetailStrings.ts
@@ -1,11 +1,5 @@
 import * as I18nString from '../I18nString/I18nString.ts'
-
-const UiStrings = {
-  Copy: 'Copy',
-  OpenInNewTab: 'Open in New Tab',
-  OpenImageInNewTab: 'Open Image in New Tab',
-  SaveImageAs: 'Save Image as',
-}
+import * as UiStrings from '../UiStrings/UiStrings.ts'
 
 export const copy = (): string => {
   return I18nString.i18nString(UiStrings.Copy)
