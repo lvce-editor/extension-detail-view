82c42b92f96b6e601564ff9bac16f1f2b099a314
Le Vivilet <levivlet@pm.me>
2025-01-15 22:39:05 +0100

    feature: move markdown into separate file

--- packages/extension-detail-view-worker/src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts
@@ -1,32 +1,19 @@
-import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as GetExtensionDetailContentVirtualDom from '../GetExtensionDetailContentVirtualDom/GetExtensionDetailContentVirtualDom.ts'
 import * as GetExtensionDetailHeaderVirtualDom from '../GetExtensionDetailHeaderVirtualDom/GetExtensionDetailHeaderVirtualDom.ts'
-import * as GetMarkdownVirtualDom from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
-import * as GetVirtualDomChildCount from '../GetVirtualDomChildCount/GetVirtualDomChildCount.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getExtensionDetailVirtualDom = (extensionDetail: any, sanitizedReadmeHtml: string): readonly VirtualDomNode[] => {
-  const markdownDom = GetMarkdownVirtualDom.getMarkdownVirtualDom(sanitizedReadmeHtml)
-  const childCount = GetVirtualDomChildCount.getVirtualDomChildCount(markdownDom)
   const dom = [
     {
       type: VirtualDomElements.Div,
       className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.ExtensionDetail),
-      childCount: childCount + 1,
+      childCount: 2,
     },
-
     ...GetExtensionDetailHeaderVirtualDom.getExtensionDetailHeaderVirtualDom(extensionDetail),
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.Markdown,
-      role: AriaRoles.Document,
-      onContextMenu: DomEventListenerFunctions.HandleReadmeContextMenu,
-      childCount,
-    },
-    ...markdownDom,
+    ...GetExtensionDetailContentVirtualDom.getExtensionDetailContentVirtualDom(sanitizedReadmeHtml),
   ]
   return dom
 }
