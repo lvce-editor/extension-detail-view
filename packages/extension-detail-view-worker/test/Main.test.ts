75914af6a7c2d0b467bbaee56d8f2e554a51469a
Le Vivilet <levivlet@pm.me>
2025-01-18 19:45:59 +0100

    feature: add main test

--- packages/extension-detail-view-worker/test/Main.test.ts
+++ packages/extension-detail-view-worker/test/Main.test.ts
@@ -1,6 +1,29 @@
-import { expect, test } from '@jest/globals'
-import * as Main from '../src/parts/Main/Main.ts'
+import { expect, jest, test } from '@jest/globals'
+
+jest.unstable_mockModule('../src/parts/Listen/Listen.ts', () => ({
+  listen: jest.fn(),
+}))
+
+const Listen = await import('../src/parts/Listen/Listen.ts')
+const Main = await import('../src/parts/Main/Main.ts')
 
 test('main', () => {
   expect(typeof Main.main).toBe('function')
+})
+
+test('main calls listen', async () => {
+  await Main.main()
+  // @ts-ignore
+  expect(Listen.listen).toHaveBeenCalledTimes(1)
+})
+
+test('main handles errors', async () => {
+  // @ts-ignore
+  Listen.listen.mockRejectedValue(new Error('test error'))
+  await expect(Main.main()).rejects.toThrow('test error')
+})
+
+test('main can only be called once', async () => {
+  await Main.main()
+  await expect(Main.main()).rejects.toThrow('already started')
 })
