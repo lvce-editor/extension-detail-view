0f4587fe8d7f76c681f5f598e946cb969f74c34a
Le Vivilet <levivlet@pm.me>
2025-01-18 19:46:47 +0100

    feature: add description test

---
+++ packages/extension-detail-view-worker/test/GetDescription.test.ts
@@ -0,0 +1,37 @@
+import { expect, test } from '@jest/globals'
+import * as GetDescription from '../src/parts/GetDescription/GetDescription.ts'
+
+test('gets description from extension detail', () => {
+  const extensionDetail = {
+    name: 'Test Extension',
+    description: 'Test Description',
+    iconSrc: './test-icon.png',
+  }
+  expect(GetDescription.getDescription(extensionDetail)).toBe('Test Description')
+})
+
+test('handles missing description', () => {
+  const extensionDetail = {
+    name: 'Test Extension',
+    iconSrc: './test-icon.png',
+  }
+  expect(GetDescription.getDescription(extensionDetail)).toBe('')
+})
+
+test('handles empty description', () => {
+  const extensionDetail = {
+    name: 'Test Extension',
+    description: '',
+    iconSrc: './test-icon.png',
+  }
+  expect(GetDescription.getDescription(extensionDetail)).toBe('')
+})
+
+test('handles null description', () => {
+  const extensionDetail = {
+    name: 'Test Extension',
+    description: null,
+    iconSrc: './test-icon.png',
+  }
+  expect(GetDescription.getDescription(extensionDetail)).toBe('')
+})
