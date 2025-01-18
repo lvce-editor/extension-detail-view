a5513cb64f1ab8c526ddbc9784cff9afb7c2018e
Le Vivilet <levivlet@pm.me>
2025-01-18 23:21:57 +0100

    test

--- packages/extension-detail-view-worker/test/GetThemeMarkdown.test.ts
+++ packages/extension-detail-view-worker/test/GetThemeMarkdown.test.ts
@@ -1,20 +1,31 @@
+import { expect, test } from '@jest/globals'
 import * as GetThemeMarkdown from '../src/parts/GetThemeMarkdown/GetThemeMarkdown.ts'
+import { IconTheme } from '../src/parts/IconTheme/IconTheme.ts'
+import { ProductIconTheme } from '../src/parts/ProductIconTheme/ProductIconTheme.ts'
+import { Theme } from '../src/parts/Theme/Theme.ts'
 
 test('getThemeMarkdown', () => {
-  const themes = [
+  const themes: readonly Theme[] = [
     {
       label: 'A',
     },
   ]
-  const iconThemes = [
+  const iconThemes: readonly IconTheme[] = [
     {
       label: 'B',
     },
   ]
-  const productIconThemes = [
+  const productIconThemes: readonly ProductIconTheme[] = [
     {
       label: 'C',
     },
   ]
-  const markdown = GetThemeMarkdown.getThemeMarkdown(themes, iconsThems, productIconThemes)
+  const markdown = GetThemeMarkdown.getThemeMarkdown(themes, iconThemes, productIconThemes)
+  expect(markdown).toBe(`### Color Themes·
+- A
+### File Icon Themes·
+- B
+### Product Icon Themes·
+- C
+`)
 })
