46ee5b0356391d4c46bee3ede06abf6ac54957cd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-18 23:45:22 +0100

    feature: render features markdown when tab is selected (#128)
    * feature: render features markdown when tab is selected
    
    * test
    
    * coverage

--- packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
@@ -1,10 +1,18 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import * as GetThemeMarkdown from '../GetThemeMarkdown/GetThemeMarkdown.ts'
 import * as InputName from '../InputName/InputName.ts'
+import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const selectTab = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  // TODO get features markdown here
+  const { extension, baseUrl } = state
+  const { colorThemes, iconThemes, productIconThemes } = extension
+  const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
+  const rendered = await RenderMarkdown.renderMarkdown(markdown, {
+    baseUrl,
+  })
   return {
     ...state,
     selectedTab: InputName.Features,
+    selectedFeatureMarkdownDom: rendered,
   }
 }
