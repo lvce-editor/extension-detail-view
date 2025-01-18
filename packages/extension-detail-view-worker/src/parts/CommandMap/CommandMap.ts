732ee0aef5fc78f2bcdb61a2a1c4a5beb7c5b3f8
Le Vivilet <levivlet@pm.me>
2025-01-18 20:50:48 +0100

    feature: prepare to store state in extension detail worker

--- packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,3 +1,4 @@
+import * as Create from '../Create/Create.ts'
 import * as GetExtensionDetailVirtualDom from '../GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts'
 import * as GetMenuEntries from '../GetMenuEntries/GetMenuEntries.ts'
 import * as HandleIconError from '../HandleIconError/HandleIconError.ts'
@@ -7,6 +8,7 @@ import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
+  'ExtensionDetail.create': Create.create,
   'ExtensionDetail.getMenuEntries': GetMenuEntries.getMenuEntries,
   'ExtensionDetail.getVirtualDom': GetExtensionDetailVirtualDom.getExtensionDetailVirtualDom,
   'ExtensionDetail.loadContent': LoadContent.loadContent,
