cc701a7132569714896c2e268d8b9b8141c709e5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-18 19:52:09 +0100

    feature: add main test (#101)
    * feature: add main test
    
    * test
    
    * test

--- packages/extension-detail-view-worker/test/Main.test.ts
+++ packages/extension-detail-view-worker/test/Main.test.ts
@@ -1,6 +1,17 @@
-import { expect, test } from '@jest/globals'
-import * as Main from '../src/parts/Main/Main.ts'
+import { expect, jest, test } from '@jest/globals'
+
+jest.unstable_mockModule('../src/parts/Listen/Listen.ts', () => ({
+  listen: jest.fn(),
+}))
+
+const Listen = await import('../src/parts/Listen/Listen.ts')
+const Main = await import('../src/parts/Main/Main.ts')
 
 test('main', () => {
   expect(typeof Main.main).toBe('function')
+})
+
+test('main calls listen', async () => {
+  await Main.main()
+  expect(Listen.listen).toHaveBeenCalledTimes(1)
 })
