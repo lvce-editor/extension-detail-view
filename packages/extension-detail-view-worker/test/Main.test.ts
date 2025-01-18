d3f7aa2c5c5bd39c9961cd044a9e00301981df93
Le Vivilet <levivlet@pm.me>
2025-01-18 19:50:15 +0100

    test

--- packages/extension-detail-view-worker/test/Main.test.ts
+++ packages/extension-detail-view-worker/test/Main.test.ts
@@ -16,14 +16,3 @@ test('main calls listen', async () => {
   // @ts-ignore
   expect(Listen.listen).toHaveBeenCalledTimes(1)
 })
-
-test('main handles errors', async () => {
-  // @ts-ignore
-  Listen.listen.mockRejectedValue(new Error('test error'))
-  await expect(Main.main()).rejects.toThrow('test error')
-})
-
-test('main can only be called once', async () => {
-  await Main.main()
-  await expect(Main.main()).rejects.toThrow('already started')
-})
