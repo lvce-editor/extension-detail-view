69e791f74d22a26a6544a53da732cffe4dc0984f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-18 22:17:11 +0100

    feature: add selectTab command (#113)

--- packages/extension-detail-view-worker/src/parts/HandleTabsClick/HandleTabsClick.ts
+++ packages/extension-detail-view-worker/src/parts/HandleTabsClick/HandleTabsClick.ts
@@ -1,9 +1,6 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import * as SelectTab from '../SelectTab/SelectTab.ts'
 
 export const handleTabsClick = (state: ExtensionDetailState, name: string): ExtensionDetailState => {
-  // TODO load the tabs content if needed
-  return {
-    ...state,
-    selectedTab: name,
-  }
+  return SelectTab.selectTab(state, name)
 }
