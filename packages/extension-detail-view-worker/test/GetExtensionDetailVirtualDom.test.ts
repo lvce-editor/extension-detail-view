1fe04f40f554993ee588afdd627608acaa12303c
Le Vivilet <levivlet@pm.me>
2025-01-18 18:00:04 +0100

    test

--- packages/extension-detail-view-worker/test/GetExtensionDetailVirtualDom.test.ts
+++ packages/extension-detail-view-worker/test/GetExtensionDetailVirtualDom.test.ts
@@ -98,6 +98,12 @@ test('extension detail virtual dom with content', () => {
       type: 12,
     },
     {
+      childCount: 1,
+      className: 'ExtensionDetailPanel',
+      role: 'panel',
+      type: 4,
+    },
+    {
       type: VirtualDomElements.Div,
       className: ClassNames.Markdown,
       role: AriaRoles.Document,
