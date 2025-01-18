b719f57c381f8f1b1d68856765a89ae18849d444
Le Vivilet <levivlet@pm.me>
2025-01-18 22:42:37 +0100

    test

--- packages/extension-detail-view-worker/test/GetFeaturesVirtualDom.test.ts
+++ packages/extension-detail-view-worker/test/GetFeaturesVirtualDom.test.ts
@@ -1,15 +1,28 @@
 import { expect, test } from '@jest/globals'
-import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
 import * as GetFeaturesVirtualDom from '../src/parts/GetFeaturesVirtualDom/GetFeaturesVirtualDom.ts'
-import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
 
 test('features virtual dom', () => {
   expect(GetFeaturesVirtualDom.getFeaturesVirtualDom()).toEqual([
     {
-      type: VirtualDomElements.Div,
-      className: ClassNames.Features,
+      childCount: 2,
+      className: 'Features',
+      type: 4,
+    },
+    {
       childCount: 1,
+      className: 'FeaturesList',
+      type: 4,
+    },
+    {
+      childCount: 1,
+      className: 'Feature',
+      type: 4,
+    },
+    {
+      childCount: 0,
+      text: 'Theme',
+      type: 12,
     },
     text('Not Implemented'),
   ])
