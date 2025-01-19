72ef8f5fca715d37717fad35c09620d51548fb9c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-19 10:40:55 +0100

    feature: simplify details dom (#131)

--- packages/extension-detail-view-worker/src/parts/GetAdditionalDetailsVirtualDom/GetAdditionalDetailsVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetAdditionalDetailsVirtualDom/GetAdditionalDetailsVirtualDom.ts
@@ -3,11 +3,11 @@ import type { MoreInfoEntry } from '../MoreInfoEntry/MoreInfoEntry.ts'
 import type { Resource } from '../Resource/Resource.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetAdditionalDetailsEntryVirtualDom from '../GetAdditionalDetailsEntryVirtualDom/GetAdditionalDetailsEntryVirtualDom.ts'
 import * as GetCategoriesDom from '../GetCategoriesDom/GetCategoriesDom.ts'
 import * as GetMoreInfoVirtualDom from '../GetMoreInfoVirtualDom/GetMoreInfoVirtualDom.ts'
 import * as GetResourcesVirtualDom from '../GetResourcesVirtualDom/GetResourcesVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getAdditionalDetailsVirtualDom = (
   firstHeading: string,
@@ -25,54 +25,17 @@ export const getAdditionalDetailsVirtualDom = (
       className: ClassNames.AdditionalDetails,
       childCount: 4,
     },
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.AdditionalDetailsEntry,
-      childCount: 2,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.AdditionalDetailsTitle,
-      childCount: 1,
-    },
-    text(firstHeading),
-    ...GetMoreInfoVirtualDom.getMoreInfoVirtualDom(entries),
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.AdditionalDetailsEntry,
-      childCount: 2,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.AdditionalDetailsTitle,
-      childCount: 1,
-    },
-    text(secondHeading),
-    ...GetMoreInfoVirtualDom.getMoreInfoVirtualDom(secondEntries),
-
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.AdditionalDetailsEntry,
-      childCount: 2,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.AdditionalDetailsTitle,
-      childCount: 1,
-    },
-    text(thirdHeading),
-    ...GetCategoriesDom.getCategoriesDom(categories),
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.AdditionalDetailsEntry,
-      childCount: 2,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.AdditionalDetailsTitle,
-      childCount: 1,
-    },
-    text(fourthHeading),
-    ...GetResourcesVirtualDom.getResourcesVirtualDom(resources),
+    ...GetAdditionalDetailsEntryVirtualDom.getAdditionalDetailsEntryVirtualDom(firstHeading, entries, GetMoreInfoVirtualDom.getMoreInfoVirtualDom),
+    ...GetAdditionalDetailsEntryVirtualDom.getAdditionalDetailsEntryVirtualDom(
+      secondHeading,
+      secondEntries,
+      GetMoreInfoVirtualDom.getMoreInfoVirtualDom,
+    ),
+    ...GetAdditionalDetailsEntryVirtualDom.getAdditionalDetailsEntryVirtualDom(thirdHeading, categories, GetCategoriesDom.getCategoriesDom),
+    ...GetAdditionalDetailsEntryVirtualDom.getAdditionalDetailsEntryVirtualDom(
+      fourthHeading,
+      resources,
+      GetResourcesVirtualDom.getResourcesVirtualDom,
+    ),
   ]
 }
