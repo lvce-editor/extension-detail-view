1852d1e81e0cebbc3c5bef940dafc208a11eef16
Le Vivilet <levivlet@pm.me>
2025-01-19 21:19:29 +0100

    feature: update e2e tests to use mock extensions

--- packages/e2e/src/extension-detail.features.ts
+++ packages/e2e/src/extension-detail.features.ts
@@ -2,14 +2,16 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'extension-detail.features'
 
-export const test: Test = async ({ Main, Locator, expect }) => {
+export const test: Test = async ({ Main, Locator, expect, Extension }) => {
+  const extensionUri = import.meta.resolve('../fixtures/extension-detail-theme')
+  await Extension.addWebExtension(extensionUri)
+  console.log({ extensionUri })
   // arrange
-  await Main.openUri('extension-detail://builtin.theme-ayu')
+  await Main.openUri('extension-detail://ayu')
   const tabFeatures = Locator('.ExtensionDetailTab[name="Features"]')
 
   // act
   await tabFeatures.click()
-  // await tabChangelog.click()
 
   // assert
   const content = Locator('.FeatureTheme')
