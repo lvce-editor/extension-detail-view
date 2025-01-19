5842d3b0f521bd28788416ac6c3ed7fe6e5d276e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-19 23:28:11 +0100

    feature: simplify commands table (#170)

--- packages/extension-detail-view-worker/src/parts/GetCommandTableEntries/GetCommandTableEntries.ts
+++ packages/extension-detail-view-worker/src/parts/GetCommandTableEntries/GetCommandTableEntries.ts
@@ -2,22 +2,24 @@ import type { Row } from '../Row/Row.ts'
 import type { TableInfo } from '../TableInfo/TableInfo.ts'
 import * as TableCellType from '../TableCellType/TableCellType.ts'
 
+const getCommandTableEntry = (command: any): Row => {
+  // TODO watch out for command being null/undefined/number/string/array
+  const { id, label } = command
+  return [
+    {
+      type: TableCellType.Code,
+      value: id,
+    },
+    {
+      type: TableCellType.Text,
+      value: label,
+    },
+  ]
+}
+
 export const getCommandTableEntries = (extension: any): TableInfo => {
-  const rows: Row[] = []
   const commands = extension.commands || []
-  for (const command of commands) {
-    // TODO watch out for command being null/undefined/number/string/array
-    rows.push([
-      {
-        type: TableCellType.Code,
-        value: command.id,
-      },
-      {
-        type: TableCellType.Text,
-        value: command.label,
-      },
-    ])
-  }
+  const rows: readonly Row[] = commands.map(getCommandTableEntry)
   return {
     headings: ['ID', 'Label'],
     rows,
