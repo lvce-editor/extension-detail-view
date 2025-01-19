892360d831a79e549e204952dda1605e6a168e3f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-19 21:57:53 +0100

    feature: add commands e2e test (#155)

---
+++ packages/e2e/src/extension-detail.feature-commands.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'extension-detail.feature-commands'
+
+export const skip = 1
+
+export const test: Test = async ({ Main, Locator, expect, Extension }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-commands')
+  await Extension.addWebExtension(extensionUri)
+  await Main.openUri('extension-detail://test.commands-test')
+  const tabFeatures = Locator('.ExtensionDetailTab[name="Features"]')
+  const featureCommands = Locator('.Feature[name="Commands"]')
+  await tabFeatures.click()
+
+  // act
+  await featureCommands.click()
+
+  // assert
+  // TODO
+}
