ed15599df7429297f69048983596d18d6e9c0d89
Le Vivilet <levivlet@pm.me>
2025-01-19 21:50:55 +0100

    skip for now

--- packages/e2e/src/extension-detail.feature-product-icon-theme.ts
+++ packages/e2e/src/extension-detail.feature-product-icon-theme.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'extension-detail.feature-product-icon-theme'
 
+export const skip = 1
+
 export const test: Test = async ({ Main, Locator, expect, Extension }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/extension-detail-product-icon-theme')
