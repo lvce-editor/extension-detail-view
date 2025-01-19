9a288b2d98badcc05a57cd12a2e45e07fee98b1c
Le Vivilet <levivlet@pm.me>
2025-01-19 21:18:47 +0100

    feature: add new command to query single extension

---
+++ packages/extension-detail-view-worker/src/parts/GetExtensionFallback/GetExtensionFallback.ts
@@ -0,0 +1,20 @@
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import * as PlatformType from '../PlatformType/PlatformType.ts'
+
+const getAllExtensions = async (platform: number): Promise<any[]> => {
+  if (platform === PlatformType.Web) {
+    return []
+  }
+  return ParentRpc.invoke('ExtensionManagement.getAllExtensions')
+}
+
+export const getExtension = async (id: string, platform: number): Promise<any> => {
+  // TODO only ask one extension from renderer worker instead of all
+  const allExtensions = await getAllExtensions(platform)
+  for (const extension of allExtensions) {
+    if (extension.id === id) {
+      return extension
+    }
+  }
+  return undefined
+}
