aaae15f897d99e48f5711d48ae9d437201f83eb9
Le Vivilet <levivlet@pm.me>
2025-01-19 22:20:13 +0100

    feature: add settings e2e test

---
+++ packages/e2e/src/extension-detail.feature-settings.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'extension-detail.feature-settings'
+
+export const skip = 1
+
+export const test: Test = async ({ Main, Locator, expect, Extension }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-settings')
+  await Extension.addWebExtension(extensionUri)
+  await Main.openUri('extension-detail://test.settings-test')
+  const tabFeatures = Locator('.ExtensionDetailTab[name="Features"]')
+  const featureSettings = Locator('.Feature[name="Settings"]')
+  await tabFeatures.click()
+
+  // act
+  await featureSettings.click()
+
+  // assert
+  const heading = Locator('.FeatureSettings h1')
+  await expect(heading).toBeVisible()
+  await expect(heading).toHaveText('Settings')
+}
