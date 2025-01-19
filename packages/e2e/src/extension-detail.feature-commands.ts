7e9d240305c2c62c17fadecae6f11d65d6a89d27
Le Vivilet <levivlet@pm.me>
2025-01-19 22:37:27 +0100

    feature: enable commands e2e test

--- packages/e2e/src/extension-detail.feature-commands.ts
+++ packages/e2e/src/extension-detail.feature-commands.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'extension-detail.feature-commands'
 
-export const skip = 1
-
 export const test: Test = async ({ Main, Locator, expect, Extension }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/extension-commands')
@@ -20,4 +18,14 @@ export const test: Test = async ({ Main, Locator, expect, Extension }) => {
   const heading = Locator('.FeatureCommands h1')
   await expect(heading).toBeVisible()
   await expect(heading).toHaveText('Commands')
+  const commandsTable = Locator('.FeatureCommands .Table')
+  await expect(commandsTable).toBeVisible()
+  const heading1 = commandsTable.locator('th').nth(0)
+  await expect(heading1).toHaveText('ID')
+  const heading2 = commandsTable.locator('th').nth(1)
+  await expect(heading2).toHaveText('Label')
+  const cell1 = commandsTable.locator('tbody td').nth(0)
+  await expect(cell1).toHaveText('test')
+  const cell2 = commandsTable.locator('tbody td').nth(1)
+  await expect(cell2).toHaveText('Test')
 }
