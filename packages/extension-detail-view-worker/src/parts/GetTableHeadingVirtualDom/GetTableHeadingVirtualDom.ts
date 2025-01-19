83ac028d8af06489b1ddcc9ff4d905f6b2c368fb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-19 23:34:04 +0100

    feature: move table heading into separate file (#169)

---
+++ packages/extension-detail-view-worker/src/parts/GetTableHeadingVirtualDom/GetTableHeadingVirtualDom.ts
@@ -0,0 +1,13 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+
+export const getTableHeadingVirtualDom = (heading: string): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Th,
+      childCount: 1,
+    },
+    text(heading),
+  ]
+}
--- packages/extension-detail-view-worker/src/parts/GetTableVirtualDom/GetTableVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetTableVirtualDom/GetTableVirtualDom.ts
@@ -2,27 +2,17 @@ import type { Cell } from '../Cell/Cell.ts'
 import type { Row } from '../Row/Row.ts'
 import type { TableInfo } from '../TableInfo/TableInfo.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as GetTableHeadingVirtualDom from '../GetTableHeadingVirtualDom/GetTableHeadingVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
-const getTableHeadingVirtualDom = (heading: string): readonly VirtualDomNode[] => {
-  return [
-    {
-      type: VirtualDomElements.Th,
-      childCount: 1,
-    },
-    text(heading),
-  ]
-}
-
 const getCellVirtualDom = (entry: Cell): readonly VirtualDomNode[] => {
-  const { value } = entry
   return [
     {
       type: VirtualDomElements.Td,
       childCount: 1,
     },
-    text(value),
+    text(entry.value),
   ]
 }
 
@@ -52,7 +42,7 @@ export const getTableVirtualDom = (tableInfo: TableInfo): readonly VirtualDomNod
       type: VirtualDomElements.Tr,
       childCount: headings.length,
     },
-    ...headings.flatMap(getTableHeadingVirtualDom),
+    ...headings.flatMap(GetTableHeadingVirtualDom.getTableHeadingVirtualDom),
     {
       type: VirtualDomElements.TBody,
       childCount: rows.length,
