ad15841af564789a3b9409e66ca940a50e1c3849
Le Vivilet <levivlet@pm.me>
2025-01-22 00:11:09 +0100

    feature: use more i18n strings

--- packages/extension-detail-view-worker/src/parts/GetFeatureSettingsVirtualDom/GetFeatureSettingsVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetFeatureSettingsVirtualDom/GetFeatureSettingsVirtualDom.ts
@@ -1,11 +1,12 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
 import * as GetSettingsTableEntries from '../GetSettingsTableEntries/GetSettingsTableEntries.ts'
 import * as GetTableVirtualDom from '../GetTableVirtualDom/GetTableVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getFeatureSettingsVirtualDom = (extension: any): readonly VirtualDomNode[] => {
-  const heading = 'Settings'
+  const heading = ExtensionDetailStrings.settings()
   const tableInfo = GetSettingsTableEntries.getSettingsTableEntries(extension)
   return [
     {
