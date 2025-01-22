4c16c5e5ef70c5c11d80f3559236455158634113
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-22 20:40:28 +0100

    feature: render basic webview info (#193)

--- packages/extension-detail-view-worker/src/parts/GetFeatureWebViewsVirtualDom/GetFeatureWebViewsVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetFeatureWebViewsVirtualDom/GetFeatureWebViewsVirtualDom.ts
@@ -4,7 +4,47 @@ import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDeta
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
+const getWebViewVirtualDom = (webView: any): readonly VirtualDomNode[] => {
+  const { id, selector, contentSecurityPolicy, elements } = webView
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'FeatureWebView',
+      childCount: 4,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'DefinitionListItem',
+      childCount: 2,
+    },
+    text('ID'),
+    text(id),
+    {
+      type: VirtualDomElements.Pre,
+      className: 'DefinitionListItem',
+      childCount: 2,
+    },
+    text('Selector'),
+    text(JSON.stringify(selector)),
+    {
+      type: VirtualDomElements.Pre,
+      className: 'DefinitionListItem',
+      childCount: 1,
+    },
+    text('Content Security Policy'),
+    text(JSON.stringify(contentSecurityPolicy)),
+    {
+      type: VirtualDomElements.Pre,
+      className: 'DefinitionListItem',
+      childCount: 1,
+    },
+    text('Elements'),
+    text(JSON.stringify(elements)),
+  ]
+}
+
 export const getFeatureWebViewsVirtualDom = (extension: any): readonly VirtualDomNode[] => {
+  const webViews = extension.webViews || []
   const heading = ExtensionDetailStrings.webViews()
   return [
     {
@@ -17,6 +57,10 @@ export const getFeatureWebViewsVirtualDom = (extension: any): readonly VirtualDo
       childCount: 1,
     },
     text(heading),
-    text('Not implemented'),
+    {
+      type: VirtualDomElements.Div,
+      childCount: webViews.length,
+    },
+    ...webViews.flatMap(getWebViewVirtualDom),
   ]
 }
--- packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
@@ -6,6 +6,7 @@ import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 export const selectTab = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
   const { extension, baseUrl } = state
   const { colorThemes, iconThemes, productIconThemes } = extension
+  // TODO do this depending on featured tab content
   const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
   const rendered = await RenderMarkdown.renderMarkdown(markdown, {
     baseUrl,
