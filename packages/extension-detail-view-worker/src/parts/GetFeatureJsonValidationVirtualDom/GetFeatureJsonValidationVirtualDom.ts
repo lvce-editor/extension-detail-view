45de4f9a1b39683b17d5800c4ccef54e62a3117f
Le Vivilet <levivlet@pm.me>
2025-01-22 21:15:12 +0100

    simplify

--- packages/extension-detail-view-worker/src/parts/GetFeatureJsonValidationVirtualDom/GetFeatureJsonValidationVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetFeatureJsonValidationVirtualDom/GetFeatureJsonValidationVirtualDom.ts
@@ -1,10 +1,10 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
+import * as GetFeatureContentHeadingVirtualDom from '../GetFeatureContentHeadingVirtualDom/GetFeatureContentHeadingVirtualDom.ts'
 import * as GetJsonValidationTableEntries from '../GetJsonValidationTableEntries/GetJsonValidationTableEntries.ts'
 import * as GetTableVirtualDom from '../GetTableVirtualDom/GetTableVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getFeatureJsonValidationVirtualDom = (extension: any): readonly VirtualDomNode[] => {
   const heading = ExtensionDetailStrings.jsonValidation()
@@ -15,11 +15,7 @@ export const getFeatureJsonValidationVirtualDom = (extension: any): readonly Vir
       className: ClassNames.FeatureContent,
       childCount: 2,
     },
-    {
-      type: VirtualDomElements.H1,
-      childCount: 1,
-    },
-    text(heading),
+    ...GetFeatureContentHeadingVirtualDom.getFeatureContentHeadingVirtualDom(heading),
     ...GetTableVirtualDom.getTableVirtualDom(tableInfo),
   ]
 }
