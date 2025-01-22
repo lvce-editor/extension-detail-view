94957555fec15bc0b24087fd13a22d491b285171
Le Vivilet <levivlet@pm.me>
2025-01-22 20:49:28 +0100

    feature: improve webview display

--- packages/extension-detail-view-worker/src/parts/GetFeatureWebViewsVirtualDom/GetFeatureWebViewsVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetFeatureWebViewsVirtualDom/GetFeatureWebViewsVirtualDom.ts
@@ -4,41 +4,60 @@ import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDeta
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
+const heading: VirtualDomNode = {
+  type: VirtualDomElements.H2,
+  className: 'DefinitionListItemHeading',
+  childCount: 1,
+}
+
+const pre: VirtualDomNode = {
+  type: VirtualDomElements.Pre,
+  childCount: 1,
+}
+
 const getWebViewVirtualDom = (webView: any): readonly VirtualDomNode[] => {
   const { id, selector, contentSecurityPolicy, elements } = webView
   return [
     {
       type: VirtualDomElements.Div,
       className: 'FeatureWebView',
-      childCount: 4,
+      childCount: 5,
     },
     {
       type: VirtualDomElements.Div,
       className: 'DefinitionListItem',
       childCount: 2,
     },
+    heading,
     text('ID'),
+    pre,
     text(id),
     {
-      type: VirtualDomElements.Pre,
+      type: VirtualDomElements.Div,
       className: 'DefinitionListItem',
       childCount: 2,
     },
+    heading,
     text('Selector'),
+    pre,
     text(JSON.stringify(selector)),
     {
-      type: VirtualDomElements.Pre,
+      type: VirtualDomElements.Div,
       className: 'DefinitionListItem',
       childCount: 1,
     },
+    heading,
     text('Content Security Policy'),
+    pre,
     text(JSON.stringify(contentSecurityPolicy)),
     {
-      type: VirtualDomElements.Pre,
+      type: VirtualDomElements.Div,
       className: 'DefinitionListItem',
       childCount: 1,
     },
+    heading,
     text('Elements'),
+    pre,
     text(JSON.stringify(elements)),
   ]
 }
