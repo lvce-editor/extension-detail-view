935b3f9b38edfbce6f6f1832416b5f80415372bc
Le Vivilet <levivlet@pm.me>
2025-01-22 20:05:07 +0100

    lint

--- packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,8 +1,10 @@
 import * as Create from '../Create/Create.ts'
 import * as GetExtensionDetailVirtualDom from '../GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts'
 import * as GetMenuEntries from '../GetMenuEntries/GetMenuEntries.ts'
+import * as HandleClickDisable from '../HandleClickDisable/HandleClickDisable.ts'
 import * as HandleClickFeatures from '../HandleClickFeatures/HandleClickFeatures.ts'
 import * as HandleClickSize from '../HandleClickSize/HandleClickSize.ts'
+import * as HandleClickUninstall from '../HandleClickUninstall/HandleClickUninstall.ts'
 import * as HandleIconError from '../HandleIconError/HandleIconError.ts'
 import * as HandleTabsClick from '../HandleTabsClick/HandleTabsClick.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
@@ -10,8 +12,6 @@ import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
 import * as SelectTab from '../SelectTab/SelectTab.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
-import * as HandleClickDisable from '../HandleClickDisable/HandleClickDisable.ts'
-import * as HandleClickUninstall from '../HandleClickUninstall/HandleClickUninstall.ts'
 
 export const commandMap = {
   'ExtensionDetail.create': Create.create,
