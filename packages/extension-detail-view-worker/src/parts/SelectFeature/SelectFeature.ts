24a67bf1c06721b736801adade48cf0a92e6759f
Le Vivilet <levivlet@pm.me>
2025-01-22 19:29:07 +0100

    fix: ignore click outside for features

--- packages/extension-detail-view-worker/src/parts/SelectFeature/SelectFeature.ts
+++ packages/extension-detail-view-worker/src/parts/SelectFeature/SelectFeature.ts
@@ -1,6 +1,9 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 
 export const selectFeature = async (state: ExtensionDetailState, name: string): Promise<ExtensionDetailState> => {
+  if (!name) {
+    return state
+  }
   const { features } = state
   const newFeatures = features.map((feature) => {
     if (feature.id === name) {
