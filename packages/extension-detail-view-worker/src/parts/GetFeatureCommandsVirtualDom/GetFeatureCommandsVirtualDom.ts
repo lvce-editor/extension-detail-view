f3149f02e7273c13295075dc424570a727ab3a73
Le Vivilet <levivlet@pm.me>
2025-01-22 21:17:02 +0100

    use helper

--- packages/extension-detail-view-worker/src/parts/GetFeatureCommandsVirtualDom/GetFeatureCommandsVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetFeatureCommandsVirtualDom/GetFeatureCommandsVirtualDom.ts
@@ -2,9 +2,9 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
 import * as GetCommandTableEntries from '../GetCommandTableEntries/GetCommandTableEntries.ts'
+import * as GetFeatureContentHeadingVirtualDom from '../GetFeatureContentHeadingVirtualDom/GetFeatureContentHeadingVirtualDom.ts'
 import * as GetTableVirtualDom from '../GetTableVirtualDom/GetTableVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getFeatureCommandsVirtualDom = (extension: any): readonly VirtualDomNode[] => {
   const heading = ExtensionDetailStrings.commands()
@@ -15,11 +15,7 @@ export const getFeatureCommandsVirtualDom = (extension: any): readonly VirtualDo
       className: ClassNames.FeatureContent,
       childCount: 2,
     },
-    {
-      type: VirtualDomElements.H1,
-      childCount: 1,
-    },
-    text(heading),
+    ...GetFeatureContentHeadingVirtualDom.getFeatureContentHeadingVirtualDom(heading),
     ...GetTableVirtualDom.getTableVirtualDom(tableInfo),
   ]
 }
