cbff89b936b94145ee8b3fb4236afbcddf1cf5d7
Le Vivilet <levivlet@pm.me>
2025-01-23 21:25:15 +0100

    fix: typo

--- packages/extension-detail-view-worker/src/parts/Create/Create.ts
+++ packages/extension-detail-view-worker/src/parts/Create/Create.ts
@@ -1,5 +1,5 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
-import * as ExtensionDetailStates from '../ExensionDetailStates/ExtensionDetailStates.ts'
+import * as ExtensionDetailStates from '../ExtensionDetailStates/ExtensionDetailStates.ts'
 
 export const create = (uid: number, uri: string, width: number, height: number, platform: number): void => {
   const state: ExtensionDetailState = {
--- packages/extension-detail-view-worker/src/parts/ExensionDetailStates/ExtensionDetailStates.ts
+++
@@ -1,4 +0,0 @@
-import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
-import * as ViewletRegistry from '../ViewletRegistry/ViewletRegistry.ts'
-
-export const { get, set } = ViewletRegistry.create<ExtensionDetailState>()
