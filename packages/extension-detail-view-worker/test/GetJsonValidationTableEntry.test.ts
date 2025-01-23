51030c68e86d12fba1ddf5111d1ed125ed8a9e85
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-23 22:55:18 +0100

    feature: add json validation test (#231)
    * feature: add json validation test
    
    * test
    
    * fix
    
    * fix

---
+++ packages/extension-detail-view-worker/test/GetJsonValidationTableEntry.test.ts
@@ -0,0 +1,54 @@
+import { expect, test } from '@jest/globals'
+import * as GetJsonValidationTableEntry from '../src/parts/GetJsonValidationTableEntry/GetJsonValidationTableEntry.ts'
+import * as TableCellType from '../src/parts/TableCellType/TableCellType.ts'
+
+test('get json validation table entry with fileMatch and schema', () => {
+  const jsonValidation = {
+    fileMatch: ['package.json'],
+    schema: 'https://example.com/schema.json',
+  }
+  expect(GetJsonValidationTableEntry.getJsonValidationTableEntry(jsonValidation)).toEqual([
+    {
+      type: TableCellType.Code,
+      value: ['package.json'],
+    },
+    {
+      type: TableCellType.Code,
+      value: 'https://example.com/schema.json',
+    },
+  ])
+})
+
+test('get json validation table entry with multiple fileMatches', () => {
+  const jsonValidation = {
+    fileMatch: ['package.json', 'composer.json'],
+    schema: 'https://example.com/schema.json',
+  }
+  expect(GetJsonValidationTableEntry.getJsonValidationTableEntry(jsonValidation)).toEqual([
+    {
+      type: TableCellType.Code,
+      value: ['package.json', 'composer.json'],
+    },
+    {
+      type: TableCellType.Code,
+      value: 'https://example.com/schema.json',
+    },
+  ])
+})
+
+test('get json validation table entry with empty values', () => {
+  const jsonValidation = {
+    fileMatch: [],
+    schema: '',
+  }
+  expect(GetJsonValidationTableEntry.getJsonValidationTableEntry(jsonValidation)).toEqual([
+    {
+      type: TableCellType.Code,
+      value: [],
+    },
+    {
+      type: TableCellType.Code,
+      value: '',
+    },
+  ])
+})
