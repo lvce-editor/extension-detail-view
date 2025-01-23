102bd1a4f6c4f9214eb4b0ea34421664ca014ed7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-23 22:34:26 +0100

    feature: add savestate test (#226)

---
+++ packages/extension-detail-view-worker/test/SaveState.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from '@jest/globals'
+import * as Create from '../src/parts/Create/Create.ts'
+import * as SaveState from '../src/parts/SaveState/SaveState.ts'
+
+test('saves state with selected tab and feature', () => {
+  Create.create(1, 'test-uri', 800, 600, 0)
+  const savedState = SaveState.saveState(1)
+  expect(savedState).toEqual({
+    selectedTab: '',
+    selectedFeature: '',
+  })
+})
