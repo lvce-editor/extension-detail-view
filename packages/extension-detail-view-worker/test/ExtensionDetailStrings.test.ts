d2b6bd4ebcad93340806b67f910c6dfaf2825676
Le Vivilet <levivlet@pm.me>
2025-01-23 22:27:57 +0100

    feature: add more strings tests

--- packages/extension-detail-view-worker/test/ExtensionDetailStrings.test.ts
+++ packages/extension-detail-view-worker/test/ExtensionDetailStrings.test.ts
@@ -16,3 +16,51 @@ test('openImageInNewTab', () => {
 test('saveImageAs', () => {
   expect(ExtensionDetailStrings.saveImageAs()).toBe('Save Image as')
 })
+
+test('fileMatch', () => {
+  expect(ExtensionDetailStrings.fileMatch()).toBe('File Match')
+})
+
+test('schema', () => {
+  expect(ExtensionDetailStrings.schema()).toBe('Schema')
+})
+
+test('theme', () => {
+  expect(ExtensionDetailStrings.theme()).toBe('Theme')
+})
+
+test('commands', () => {
+  expect(ExtensionDetailStrings.commands()).toBe('Commands')
+})
+
+test('webViews', () => {
+  expect(ExtensionDetailStrings.webViews()).toBe('Web Views')
+})
+
+test('jsonValidation', () => {
+  expect(ExtensionDetailStrings.jsonValidation()).toBe('Json Validation')
+})
+
+test('programmingLanguages', () => {
+  expect(ExtensionDetailStrings.programmingLanguages()).toBe('Programming Languages')
+})
+
+test('settings', () => {
+  expect(ExtensionDetailStrings.settings()).toBe('Settings')
+})
+
+test('id', () => {
+  expect(ExtensionDetailStrings.id()).toBe('Id')
+})
+
+test('selector', () => {
+  expect(ExtensionDetailStrings.selector()).toBe('Selector')
+})
+
+test('contentSecurityPolicy', () => {
+  expect(ExtensionDetailStrings.contentSecurityPolicy()).toBe('Content Security Policy')
+})
+
+test('elements', () => {
+  expect(ExtensionDetailStrings.elements()).toBe('Elements')
+})
