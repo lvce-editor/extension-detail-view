3a27619aa7a39c00237a6d30f615d32f640ecdfe
Le Vivilet <levivlet@pm.me>
2025-01-23 21:43:37 +0100

    lint

--- packages/extension-detail-view-worker/src/parts/GetExtensionDetailVirtualDom2/GetExtensionDetailVirtualDom2.ts
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailVirtualDom2/GetExtensionDetailVirtualDom2.ts
@@ -1,6 +1,6 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-import * as GetExtensionDetailVirtualDom from '../GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts'
 import * as ExtensionDetailStates from '../ExtensionDetailStates/ExtensionDetailStates.ts'
+import * as GetExtensionDetailVirtualDom from '../GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts'
 
 export const getExtensionDetailVirtualDom2 = (uid: number): readonly VirtualDomNode[] => {
   const { newState } = ExtensionDetailStates.get(uid)
