984bbfba23bc5016a26421fd4663bbacf3d22a00
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-23 22:17:48 +0100

    feature: add tab test (#218)

---
+++ packages/extension-detail-view-worker/test/GetTabVirtualDom.test.ts
@@ -0,0 +1,66 @@
+import { expect, test } from '@jest/globals'
+import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetTabVirtualDom from '../src/parts/GetTabVirtualDom/GetTabVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
+
+test('selected tab', () => {
+  const tab = {
+    label: 'Details',
+    name: 'Details',
+    selected: true,
+  }
+  expect(GetTabVirtualDom.getTabVirtualDom(tab)).toEqual([
+    {
+      type: VirtualDomElements.Button,
+      role: AriaRoles.Tab,
+      name: 'Details',
+      className: `${ClassNames.ExtensionDetailTab} ${ClassNames.ExtensionDetailTabSelected}`,
+      childCount: 1,
+      tabIndex: -1,
+      ariaSelected: 'true',
+    },
+    text('Details'),
+  ])
+})
+
+test('unselected tab', () => {
+  const tab = {
+    label: 'Features',
+    name: 'Features',
+    selected: false,
+  }
+  expect(GetTabVirtualDom.getTabVirtualDom(tab)).toEqual([
+    {
+      type: VirtualDomElements.Button,
+      role: AriaRoles.Tab,
+      name: 'Features',
+      className: ClassNames.ExtensionDetailTab,
+      childCount: 1,
+      tabIndex: -1,
+      ariaSelected: 'false',
+    },
+    text('Features'),
+  ])
+})
+
+test('tab with different label and name', () => {
+  const tab = {
+    label: 'Display Label',
+    name: 'internal-name',
+    selected: false,
+  }
+  expect(GetTabVirtualDom.getTabVirtualDom(tab)).toEqual([
+    {
+      type: VirtualDomElements.Button,
+      role: AriaRoles.Tab,
+      name: 'internal-name',
+      className: ClassNames.ExtensionDetailTab,
+      childCount: 1,
+      tabIndex: -1,
+      ariaSelected: 'false',
+    },
+    text('Display Label'),
+  ])
+})
