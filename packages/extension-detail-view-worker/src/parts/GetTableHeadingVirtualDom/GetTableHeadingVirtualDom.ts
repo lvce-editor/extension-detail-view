c6a853da6d46bbac36ded189664c849f51f0e79e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-23 22:12:46 +0100

    feature: use classnames more often (#216)

--- packages/extension-detail-view-worker/src/parts/GetTableHeadingVirtualDom/GetTableHeadingVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetTableHeadingVirtualDom/GetTableHeadingVirtualDom.ts
@@ -1,4 +1,5 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
@@ -6,7 +7,7 @@ export const getTableHeadingVirtualDom = (heading: string): readonly VirtualDomN
   return [
     {
       type: VirtualDomElements.Th,
-      className: 'TableHeading',
+      className: ClassNames.TableHeading,
       childCount: 1,
     },
     text(heading),
