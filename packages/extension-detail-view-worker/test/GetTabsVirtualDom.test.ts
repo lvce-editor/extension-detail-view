55c43080f5a9b1a524b66ebf2d8e977119051c52
Le Vivilet <levivlet@pm.me>
2025-01-24 20:22:02 +0100

    feature: add tabs dom test

---
+++ packages/extension-detail-view-worker/test/GetTabsVirtualDom.test.ts
@@ -0,0 +1,54 @@
+import { expect, test } from '@jest/globals'
+import * as GetTabsVirtualDom from '../src/parts/GetTabsVirtualDom/GetTabsVirtualDom.ts'
+import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import { Tab } from '../src/parts/Tab/Tab.ts'
+
+test('getTabsVirtualDom - returns correct virtual dom structure', () => {
+  const tabs: readonly Tab[] = [
+    {
+      label: 'Tab 1',
+      name: 'tab1',
+      selected: true,
+    },
+    {
+      label: 'Tab 2',
+      name: 'tab2',
+      selected: false,
+    },
+  ]
+
+  const virtualDom = GetTabsVirtualDom.getTabsVirtualDom(tabs)
+
+  expect(virtualDom[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.ExtensionDetailTabs,
+    childCount: tabs.length,
+    role: AriaRoles.TabList,
+    onClick: DomEventListenerFunctions.HandleTabsClick,
+    tabIndex: 0,
+  })
+
+  // Since the actual tab rendering is delegated to GetTabVirtualDom,
+  // we don't need to test the specific tab content here
+  expect(virtualDom.length).toBeGreaterThan(1)
+})
+
+test('getTabsVirtualDom - handles empty tabs array', () => {
+  const tabs: readonly Tab[] = []
+
+  const virtualDom = GetTabsVirtualDom.getTabsVirtualDom(tabs)
+
+  expect(virtualDom[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.ExtensionDetailTabs,
+    childCount: 0,
+    role: AriaRoles.TabList,
+    onClick: DomEventListenerFunctions.HandleTabsClick,
+    tabIndex: 0,
+  })
+
+  expect(virtualDom.length).toBe(1)
+})
