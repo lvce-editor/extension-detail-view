7081082a9a30af9da5c9373b4b772ef9c8579d6c
Le Vivilet <levivlet@pm.me>
2025-01-24 21:04:58 +0100

    lint

--- packages/extension-detail-view-worker/test/GetCellCodeVirtualDom.test.ts
+++ packages/extension-detail-view-worker/test/GetCellCodeVirtualDom.test.ts
@@ -1,6 +1,5 @@
 import { expect, test } from '@jest/globals'
 import * as GetCellCodeVirtualDom from '../src/parts/GetCellCodeVirtualDom/GetCellCodeVirtualDom.ts'
-import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
 
 test('get cell code virtual dom with string value', () => {
