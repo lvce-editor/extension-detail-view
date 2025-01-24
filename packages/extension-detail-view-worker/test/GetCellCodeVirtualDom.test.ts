94bffb93645b157a600570aee1d409067553f979
Le Vivilet <levivlet@pm.me>
2025-01-24 20:59:19 +0100

    feature: add code cell test

---
+++ packages/extension-detail-view-worker/test/GetCellCodeVirtualDom.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@jest/globals'
+import * as GetCellCodeVirtualDom from '../src/parts/GetCellCodeVirtualDom/GetCellCodeVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
+
+test('get cell code virtual dom with string value', () => {
+  const value = 'npm install'
+  expect(GetCellCodeVirtualDom.getCellCodeVirtualDom(value)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: 'TableCell',
+      childCount: 1,
+    },
+    text('npm install'),
+  ])
+})
