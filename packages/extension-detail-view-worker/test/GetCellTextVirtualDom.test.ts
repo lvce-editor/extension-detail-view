e2d6ac284273792fa34e3db79d550664a64a816d
Le Vivilet <levivlet@pm.me>
2025-01-24 21:08:02 +0100

    test

--- packages/extension-detail-view-worker/test/GetCellTextVirtualDom.test.ts
+++ packages/extension-detail-view-worker/test/GetCellTextVirtualDom.test.ts
@@ -1,15 +1,14 @@
 import { expect, test } from '@jest/globals'
 import * as GetCellTextVirtualDom from '../src/parts/GetCellTextVirtualDom/GetCellTextVirtualDom.ts'
-import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
 
 test('get cell text virtual dom with string value', () => {
   const value = 'Test Value'
   expect(GetCellTextVirtualDom.getCellTextVirtualDom(value)).toEqual([
     {
-      type: VirtualDomElements.Div,
+      type: 11,
       className: 'TableCell',
-      childCount: 11,
+      childCount: 1,
     },
     text('Test Value'),
   ])
