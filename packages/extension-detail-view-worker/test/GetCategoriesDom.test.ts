642a402914bcb8355fb84028938bc49c4abf496a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-24 21:41:50 +0100

    feature: add categories test (#256)
    * feature: add categories test
    
    * lint
    
    * test

---
+++ packages/extension-detail-view-worker/test/GetCategoriesDom.test.ts
@@ -0,0 +1,49 @@
+import { expect, test } from '@jest/globals'
+import type { Category } from '../src/parts/Category/Category.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetCategoriesDom from '../src/parts/GetCategoriesDom/GetCategoriesDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
+
+test('categories dom with categories', () => {
+  const categories: readonly Category[] = [
+    {
+      id: 'programming-languages',
+      label: 'Programming Languages',
+    },
+    {
+      id: 'snippets',
+      label: 'Snippets',
+    },
+  ]
+  expect(GetCategoriesDom.getCategoriesDom(categories)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Categories,
+      childCount: categories.length,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Category,
+      childCount: 1,
+    },
+    text('Programming Languages'),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Category,
+      childCount: 1,
+    },
+    text('Snippets'),
+  ])
+})
+
+test('categories dom with empty categories', () => {
+  const categories: readonly Category[] = []
+  expect(GetCategoriesDom.getCategoriesDom(categories)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Categories,
+      childCount: 0,
+    },
+  ])
+})
