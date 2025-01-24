864ef4c97e4f8af56a43a947e81644ca7cc5bbd9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-24 20:32:00 +0100

    feature: add more info entry test (#240)

---
+++ packages/extension-detail-view-worker/test/GetMoreInfoEntryValueVirtualDom.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@jest/globals'
+import type { MoreInfoEntry } from '../src/parts/MoreInfoEntry/MoreInfoEntry.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetMoreInfoEntryValueVirtualDom from '../src/parts/GetMoreInfoEntryValueVirtualDom/GetMoreInfoEntryValueVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
+
+test('string value', () => {
+  const entry: MoreInfoEntry = {
+    key: 'License',
+    value: 'MIT',
+  }
+  expect(GetMoreInfoEntryValueVirtualDom.getMoreInfoEntryValueVirtualDom(entry)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.MoreInfoEntryValue,
+      childCount: 1,
+    },
+    text('MIT'),
+  ])
+})
