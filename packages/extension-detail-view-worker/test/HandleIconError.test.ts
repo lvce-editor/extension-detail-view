3140ed52e8aef635647f5cc4bcab1e62fbc31e5f
Le Vivilet <levivlet@pm.me>
2025-01-30 20:08:50 +0100

    test

--- packages/extension-detail-view-worker/test/HandleIconError.test.ts
+++ packages/extension-detail-view-worker/test/HandleIconError.test.ts
@@ -6,6 +6,7 @@ test('returns same state if icon is already default icon', () => {
   const state = {
     iconSrc: Icon.extensionDefaultIcon(''),
     name: 'test',
+    assetDir: '',
   } as any
   expect(HandleIconError.handleIconError(state)).toBe(state)
 })
@@ -14,6 +15,7 @@ test('replaces icon with default icon when error occurs', () => {
   const state = {
     iconSrc: '/some/invalid/path.png',
     name: 'test',
+    assetDir: '',
   } as any
   expect(HandleIconError.handleIconError(state)).toEqual({
     iconSrc: Icon.extensionDefaultIcon(''),
@@ -27,6 +29,7 @@ test('preserves other state properties when replacing icon', () => {
     name: 'test',
     description: 'test description',
     version: '1.0.0',
+    assetDir: '',
   } as any
   expect(HandleIconError.handleIconError(state)).toEqual({
     iconSrc: Icon.extensionDefaultIcon(''),
