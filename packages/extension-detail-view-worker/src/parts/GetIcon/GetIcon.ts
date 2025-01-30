ff91b28bd54026ad6db2eb659c3755ed70680b17
Le Vivilet <levivlet@pm.me>
2025-01-30 19:59:27 +0100

    feature: make assetDir more dynamic

--- packages/extension-detail-view-worker/src/parts/GetIcon/GetIcon.ts
+++ packages/extension-detail-view-worker/src/parts/GetIcon/GetIcon.ts
@@ -5,21 +5,23 @@ import * as IsThemeExtension from '../IsThemeExtension/IsThemeExtension.ts'
 import * as PlatformType from '../PlatformType/PlatformType.ts'
 
 export const getIcon = (extension: any, platform: number): string => {
+  // TODO make it dynamic
+  const {assetDir} = AssetDir
   if (!extension) {
     return Icon.ExtensionDefaultIcon
   }
   if (!extension.path || !extension.icon) {
     if (IsLanguageBasicsExtension.isLanguageBasicsExtension(extension)) {
-      return Icon.ExtensionLanguageBasics
+      return Icon.extensionLanguageBasics(AssetDir.assetDir)
     }
     if (IsThemeExtension.isThemeExtension(extension)) {
-      return Icon.ExtensionTheme
+      return Icon.extensionTheme(AssetDir.assetDir)
     }
-    return Icon.ExtensionDefaultIcon
+    return Icon.extensionDefaultIcon(AssetDir.assetDir)
   }
   if (platform === PlatformType.Remote || platform === PlatformType.Electron) {
     if (extension.builtin) {
-      return `${AssetDir.assetDir}/extensions/${extension.id}/${extension.icon}`
+      return `${assetDir}/extensions/${extension.id}/${extension.icon}`
     }
     return `/remote/${extension.path}/${extension.icon}` // TODO support windows paths
   }
