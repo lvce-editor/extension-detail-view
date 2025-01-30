c3e362ada9260feb4846ede2833fb3cfbde4a1da
Le Vivilet <levivlet@pm.me>
2025-01-30 19:57:56 +0100

    test

--- packages/extension-detail-view-worker/test/SaveState.test.ts
+++ packages/extension-detail-view-worker/test/SaveState.test.ts
@@ -3,7 +3,7 @@ import * as Create from '../src/parts/Create/Create.ts'
 import * as SaveState from '../src/parts/SaveState/SaveState.ts'
 
 test('saves state with selected tab and feature', () => {
-  Create.create(1, 'test-uri', 800, 600, 0)
+  Create.create(1, 'test-uri', 800, 600, 0, '')
   const savedState = SaveState.saveState(1)
   expect(savedState).toEqual({
     selectedTab: '',
