bb2023bb763d1e222e79956db768e8fb8a4377dc
Le Vivilet <levivlet@pm.me>
2025-03-30 18:01:55 +0200

    feature: add createDefaultState function

---
+++ packages/extension-detail-view-worker/src/parts/CreateDefaultState/CreateDefaultState.ts
@@ -0,0 +1,26 @@
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+
+export const createDefaultState = (overrides: Partial<ExtensionDetailState> = {}): ExtensionDetailState => {
+  return {
+    selectedTab: '',
+    selectedFeature: '',
+    sanitizedReadmeHtml: '',
+    iconSrc: '',
+    name: '',
+    description: '',
+    sizeOnDisk: 0,
+    width: 500,
+    uri: 'extension-detail://test-extension',
+    selectedFeatureMarkdownDom: '',
+    entries: [],
+    secondEntries: [],
+    categories: [],
+    resources: [],
+    extension: {},
+    baseUrl: '',
+    features: [],
+    folderSize: 0,
+    assetDir: '/test/asset/dir',
+    ...overrides,
+  }
+}
