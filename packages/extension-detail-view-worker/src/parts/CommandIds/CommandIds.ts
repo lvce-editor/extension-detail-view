dc667ae26dd05c6027b4e7bb3f04114ed729831a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-30 18:47:40 +0200

    feature: add getCommandIds (#307)

---
+++ packages/extension-detail-view-worker/src/parts/CommandIds/CommandIds.ts
@@ -0,0 +1,14 @@
+export const commandIds = [
+  'getMenuEntries',
+  'handleClickDisable',
+  'handleClickSize',
+  'handleClickUninstall',
+  'handleFeaturesClick',
+  'handleIconError',
+  'handleTabsClick',
+  'renderEventListeners',
+  'resize',
+  'saveState',
+  'selectTab',
+  'terminate',
+]
