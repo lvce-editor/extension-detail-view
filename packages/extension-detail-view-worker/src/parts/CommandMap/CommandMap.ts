f71cb780e089198974e7fc7bd83437fa7452513d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-30 18:42:34 +0200

    feature: register new commands (#306)

--- packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-detail-view-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,6 @@
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
+import * as Dispose from '../Dispose/Dispose.ts'
 import * as GetExtensionDetailVirtualDom2 from '../GetExtensionDetailVirtualDom2/GetExtensionDetailVirtualDom2.ts'
 import * as GetMenuEntries from '../GetMenuEntries/GetMenuEntries.ts'
 import * as HandleClickDisable from '../HandleClickDisable/HandleClickDisable.ts'
@@ -8,7 +9,9 @@ import * as HandleClickSize from '../HandleClickSize/HandleClickSize.ts'
 import * as HandleClickUninstall from '../HandleClickUninstall/HandleClickUninstall.ts'
 import * as HandleIconError from '../HandleIconError/HandleIconError.ts'
 import * as HandleTabsClick from '../HandleTabsClick/HandleTabsClick.ts'
+import * as LoadContent2 from '../LoadContent2/LoadContent2.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
+import * as Render2 from '../Render2/Render2.ts'
 import * as RenderEventListeners from '../RenderEventListeners/RenderEventListeners.ts'
 import * as Resize from '../Resize/Resize.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
@@ -18,19 +21,24 @@ import * as WrapCommand from '../WrapCommand/WrapCommand.ts'
 
 export const commandMap = {
   'ExtensionDetail.create': Create.create,
+  'ExtensionDetail.diff2': Diff2.diff2,
+  'ExtensionDetail.dispose': Dispose.dispose,
   'ExtensionDetail.getMenuEntries': GetMenuEntries.getMenuEntries,
-  'ExtensionDetail.getVirtualDom2': GetExtensionDetailVirtualDom2.getExtensionDetailVirtualDom2,
   'ExtensionDetail.handleClickDisable': WrapCommand.wrapCommand(HandleClickDisable.handleClickDisable),
   'ExtensionDetail.handleClickSize': WrapCommand.wrapCommand(HandleClickSize.handleClickSize),
   'ExtensionDetail.handleClickUninstall': WrapCommand.wrapCommand(HandleClickUninstall.handleClickUninstall),
   'ExtensionDetail.handleFeaturesClick': WrapCommand.wrapCommand(HandleClickFeatures.handleClickFeatures),
   'ExtensionDetail.handleIconError': WrapCommand.wrapCommand(HandleIconError.handleIconError),
   'ExtensionDetail.handleTabsClick': WrapCommand.wrapCommand(HandleTabsClick.handleTabsClick),
-  'ExtensionDetail.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
+  'ExtensionDetail.render2': WrapCommand.wrapCommand(Render2.render2),
+  'ExtensionDetail.loadContent2': WrapCommand.wrapCommand(LoadContent2.loadContent2),
   'ExtensionDetail.renderEventListeners': RenderEventListeners.renderEventListeners,
-  'ExtensionDetail.diff2': Diff2.diff2,
   'ExtensionDetail.resize': Resize.resize,
   'ExtensionDetail.saveState': SaveState.saveState,
   'ExtensionDetail.selectTab': WrapCommand.wrapCommand(SelectTab.selectTab),
   'ExtensionDetail.terminate': Terminate.terminate,
+
+  // deprecated
+  'ExtensionDetail.getVirtualDom2': GetExtensionDetailVirtualDom2.getExtensionDetailVirtualDom2,
+  'ExtensionDetail.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
 }
