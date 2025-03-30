306f497b0c378ba5043718eed4cc12164d6e5ab0
Le Vivilet <levivlet@pm.me>
2025-03-30 18:17:41 +0200

    feature: add renderDom function

---
+++ packages/extension-detail-view-worker/src/parts/RenderDom/RenderDom.ts
@@ -0,0 +1,7 @@
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import * as GetExtensionDetailVirtualDom from '../GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts'
+
+export const renderDom = async (oldState: ExtensionDetailState, newState: ExtensionDetailState): Promise<readonly any[]> => {
+  const dom = await GetExtensionDetailVirtualDom.getExtensionDetailVirtualDom(newState, newState.sanitizedReadmeHtml, newState.selectedTab, newState)
+  return ['Viewlet.setDom2', dom]
+}
