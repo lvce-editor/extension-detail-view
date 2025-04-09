0d16ee47cd68e4e10fb6a9633d156d85b47a6d03
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-09 19:50:36 +0200

    feature: use i18n string (#325)

--- packages/extension-detail-view-worker/src/parts/GetFeaturesVirtualDom/GetFeaturesVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetFeaturesVirtualDom/GetFeaturesVirtualDom.ts
@@ -1,6 +1,7 @@
 import type { Feature } from '../Feature/Feature.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
 import * as GetFeatureContentVirtualDom from '../GetFeatureContentVirtualDom/GetFeatureContentVirtualDom.ts'
 import * as GetFeatureListVirtualDom from '../GetFeatureListVirtualDom/GetFeatureListVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
@@ -14,13 +15,14 @@ export const getFeaturesVirtualDom = async (
   extension: any,
 ): Promise<readonly VirtualDomNode[]> => {
   if (features.length === 0) {
+    const none = ExtensionDetailStrings.none()
     return [
       {
         type: VirtualDomElements.Div,
         className: ClassNames.Features,
         childCount: 3,
       },
-      text('None'),
+      text(none),
     ]
   }
   return [
