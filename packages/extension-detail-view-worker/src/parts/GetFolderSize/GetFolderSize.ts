953dfcc1c7032e4a72e842c834c3266cb24d449f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-24 22:32:51 +0200

    fix: throw error when passing empty uri to getFolderSize (#340)

--- packages/extension-detail-view-worker/src/parts/GetFolderSize/GetFolderSize.ts
+++ packages/extension-detail-view-worker/src/parts/GetFolderSize/GetFolderSize.ts
@@ -1,6 +1,10 @@
+import { VError } from '@lvce-editor/verror'
 import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
 export const getFolderSize = async (uri: string): Promise<number> => {
+  if (!uri) {
+    throw new VError(`uri is required`)
+  }
   try {
     return await ParentRpc.invoke('FileSystem.getFolderSize', uri)
   } catch {
