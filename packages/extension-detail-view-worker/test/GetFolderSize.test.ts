31fe9d3870ca008acbd48c06b2fc70caf50ca5f2
Le Vivilet <levivlet@pm.me>
2025-07-06 22:14:44 +0200

    lint

--- packages/extension-detail-view-worker/test/GetFolderSize.test.ts
+++ packages/extension-detail-view-worker/test/GetFolderSize.test.ts
@@ -1,11 +1,10 @@
 import { expect, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
 import * as GetFolderSize from '../src/parts/GetFolderSize/GetFolderSize.ts'
 import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
-import { MockRpc } from '@lvce-editor/rpc'
-
 
 test('get folder size', async () => {
-  const mockRpc = await MockRpc.create({
+  const mockRpc = MockRpc.create({
     commandMap: {},
     invoke: (method: string) => {
       if (method === 'FileSystem.getFolderSize') {
@@ -19,7 +18,7 @@ test('get folder size', async () => {
 })
 
 test('get folder size - error case', async () => {
-  const mockRpc = await MockRpc.create({
+  const mockRpc = MockRpc.create({
     commandMap: {},
     invoke: (method: string) => {
       throw new Error('access denied')
