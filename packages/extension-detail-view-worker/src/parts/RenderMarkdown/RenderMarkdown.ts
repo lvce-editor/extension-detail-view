0e0156ba56c246427ade895245ac80e0c1785517
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-06 22:28:48 +0200

    feature: simplify markdown code (#380)

--- packages/extension-detail-view-worker/src/parts/RenderMarkdown/RenderMarkdown.ts
+++ packages/extension-detail-view-worker/src/parts/RenderMarkdown/RenderMarkdown.ts
@@ -1,7 +1,6 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const renderMarkdown = async (markdown: string, options: { readonly baseUrl?: string } = {}): Promise<string> => {
-  // @ts-ignore todo
-  const html = await RendererWorker.invoke('Markdown.renderMarkdown', markdown, options)
+  const html = await RendererWorker.renderMarkdown(markdown, options)
   return html
 }
