fdeee33037ee057131fe0df24324a927871f5746
Le Vivilet <levivlet@pm.me>
2025-07-06 20:37:45 +0200

    feature: add color theme button

--- packages/extension-detail-view-worker/src/parts/GetExtensionDetailHeaderActionsVirtualDom/GetExtensionDetailHeaderActionsVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailHeaderActionsVirtualDom/GetExtensionDetailHeaderActionsVirtualDom.ts
@@ -3,16 +3,25 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetButtonVirtualDom from '../GetButtonVirtualDom/GetButtonVirtualDom.ts'
+import * as HasColorThemes from '../HasColorThemes/HasColorThemes.ts'
 
-export const getExtensionDetailHeaderActionsVirtualDom = (): readonly VirtualDomNode[] => {
+export const getExtensionDetailHeaderActionsVirtualDom = (extension: any): readonly VirtualDomNode[] => {
+  const buttons: VirtualDomNode[] = [
+    ...GetButtonVirtualDom.getButtonVirtualDom('Disable', DomEventListenerFunctions.HandleClickDisable),
+    ...GetButtonVirtualDom.getButtonVirtualDom('Uninstall', DomEventListenerFunctions.HandleClickUninstall),
+  ]
+
+  if (HasColorThemes.hasColorThemes(extension)) {
+    buttons.push(...GetButtonVirtualDom.getButtonVirtualDom('Set Color Theme', DomEventListenerFunctions.HandleClickSetColorTheme))
+  }
+
   const dom: readonly VirtualDomNode[] = [
     {
       type: VirtualDomElements.Div,
       className: ClassNames.ExtensionDetailHeaderActions,
-      childCount: 2,
+      childCount: buttons.length,
     },
-    ...GetButtonVirtualDom.getButtonVirtualDom('Disable', DomEventListenerFunctions.HandleClickDisable),
-    ...GetButtonVirtualDom.getButtonVirtualDom('Uninstall', DomEventListenerFunctions.HandleClickUninstall),
+    ...buttons,
   ]
   return dom
 }
