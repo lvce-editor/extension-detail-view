ceedfa60fe26a74a123d2c72f748d6309a6230d7
Le Vivilet <levivlet@pm.me>
2025-07-06 22:56:48 +0200

    feature: add settings button dom

---
+++ packages/extension-detail-view-worker/src/parts/GetSettingsButtonVirtualDom/GetSettingsButtonVirtualDom.ts
@@ -0,0 +1,22 @@
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+
+export const getSettingsButtonVirtualDom = (): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Button,
+      className: ClassNames.SettingsButton,
+      onClick: DomEventListenerFunctions.HandleClickSettings,
+      childCount: 1,
+      title: 'Settings',
+    },
+    {
+      type: VirtualDomElements.Span,
+      className: ClassNames.SettingsIcon,
+      childCount: 0,
+      text: '⚙️',
+    },
+  ]
+}
