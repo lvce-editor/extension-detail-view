ae424f0cf63473d62c86dcc943837612cc5327af
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-06 22:32:19 +0200

    feature: simplify renderer worker api (#381)

--- packages/extension-detail-view-worker/src/parts/DisableExtension/DisableExtension.ts
+++ packages/extension-detail-view-worker/src/parts/DisableExtension/DisableExtension.ts
@@ -1,6 +1,5 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const disableExtension = (id: string): Promise<void> => {
-  // @ts-ignore todo
-  return RendererWorker.invoke('ExtensionManagement.disable', id)
+  return RendererWorker.disableExtension(id)
 }
