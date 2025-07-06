e594dd25ec8bd1facf8e8b7c50c7acc77151d305
Le Vivilet <levivlet@pm.me>
2025-07-06 23:39:04 +0200

    fix

--- packages/extension-detail-view-worker/test/GetExtensionDetailVirtualDom.test.ts
+++ packages/extension-detail-view-worker/test/GetExtensionDetailVirtualDom.test.ts
@@ -5,16 +5,19 @@ import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetExtensionDetailVirtualDom from '../src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts'
 import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
+import { ExtensionDetailState } from '../src/parts/ExtensionDetailState/ExtensionDetailState.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 
 test.skip('extension detail virtual dom with content', async () => {
-  const extensionDetail = {
+  const extensionDetail: ExtensionDetailState = {
+    ...createDefaultState(),
     name: 'Test Extension',
     iconSrc: './test-icon.png',
     description: 'Test Description',
   }
   const sanitizedReadmeHtml = '<h1>Test Header</h1>'
   const selectedTab = 'Details'
-  const state = {}
+  const state = extensionDetail
   expect(await GetExtensionDetailVirtualDom.getExtensionDetailVirtualDom(extensionDetail, sanitizedReadmeHtml, selectedTab, state)).toEqual([
     {
       type: VirtualDomElements.Div,
@@ -120,14 +123,15 @@ test.skip('extension detail virtual dom with content', async () => {
 })
 
 test.skip('extension detail virtual dom with empty content', async () => {
-  const extensionDetail = {
+  const extensionDetail: ExtensionDetailState = {
+    ...createDefaultState(),
     name: '',
     iconSrc: '',
     description: '',
   }
   const sanitizedReadmeHtml = ''
   const selectedTab = 'Details'
-  const state = {}
+  const state = extensionDetail
   expect(await GetExtensionDetailVirtualDom.getExtensionDetailVirtualDom(extensionDetail, sanitizedReadmeHtml, selectedTab, state)).toEqual([
     {
       type: VirtualDomElements.Div,
