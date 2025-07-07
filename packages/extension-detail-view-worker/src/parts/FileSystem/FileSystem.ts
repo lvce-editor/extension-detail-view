773253d2215dc8ed67203b9e19ca0c693c4d40f0
Le Vivilet <levivlet@pm.me>
2025-07-07 13:31:04 +0200

    feature: ask renderer worker to read file

--- packages/extension-detail-view-worker/src/parts/FileSystem/FileSystem.ts
+++ packages/extension-detail-view-worker/src/parts/FileSystem/FileSystem.ts
@@ -1,13 +1,5 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const readFile = async (uri: string): Promise<string> => {
-  if (uri.startsWith('http://') || uri.startsWith('https://')) {
-    const response = await fetch(uri)
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    const result = await response.text()
-    return result
-  }
   return RendererWorker.readFile(uri)
 }
