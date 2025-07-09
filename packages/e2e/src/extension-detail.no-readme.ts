6b7dd76376c53761f7bb37b23e0de400548fa798
Le Vivilet <levivlet@pm.me>
2025-07-09 23:40:06 +0200

    feature: add readme not found e2e test

---
+++ packages/e2e/src/extension-detail.no-readme.ts
@@ -0,0 +1,17 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-no-readme')
+  await Extension.addWebExtension(extensionUri)
+
+  // act
+  await ExtensionDetail.open('test.extension-basics')
+
+  // assert
+  const detailView = Locator('.ExtensionDetail')
+  await expect(detailView).toBeVisible()
+  const icon = Locator('.Markdown')
+  await expect(icon).toBeVisible()
+  await expect(icon).toHaveText('Error: Failed to get text: Not Found')
+}
