3bcd1417a15b05286a0219e2fff333cfae17b8b2
Le Vivilet <levivlet@pm.me>
2025-07-09 23:28:43 +0200

    tsc

--- packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
@@ -1,7 +1,7 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 import * as InputName from '../InputName/InputName.ts'
 
-export const selectTab = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
+export const selectTabFeatures = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
   return {
     ...state,
     selectedTab: InputName.Features,
