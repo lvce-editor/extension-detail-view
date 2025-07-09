da2e221ba2d5faa3cf4d417f2f8b6b5d53de160b
Le Vivilet <levivlet@pm.me>
2025-07-09 23:59:00 +0200

    feature: enable changelog e2e test

--- packages/e2e/src/extension-detail.changelog.ts
+++ packages/e2e/src/extension-detail.changelog.ts
@@ -1,19 +1,15 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const skip = 1
-
 export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/extension-changelog')
   await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.extension-changelog')
+  const tabChangelog = Locator('.ExtensionDetailTab[name="Changelog"]') // TODO use page object model
 
   // act
-  await ExtensionDetail.open('test.extension-changelog')
-
-  // assert
-  const detailView = Locator('.ExtensionDetail')
-  await expect(detailView).toBeVisible()
-  const icon = Locator('.Markdown')
-  await expect(icon).toBeVisible()
-  await expect(icon).toHaveText('Error: Failed to get text: Not Found\n')
+  await tabChangelog.click()
+  const changelog = Locator('.Changelog')
+  await expect(changelog).toBeVisible()
+  await expect(changelog).toHaveText('Not Implemented')
 }
