426fa0902cc866ee70b28f91ae29e51eeae526dc
Le Vivilet <levivlet@pm.me>
2025-07-09 23:26:00 +0200

    undo

--- packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
@@ -1,14 +1,7 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 import * as InputName from '../InputName/InputName.ts'
 
-export const selectTabFeatures = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  const { extension, baseUrl } = state
-  const { colorThemes, iconThemes, productIconThemes } = extension
-  // TODO do this depending on featured tab content
-  const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
-  const rendered = await RenderMarkdown.renderMarkdown(markdown, {
-    baseUrl,
-  })
+export const selectTab = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
   return {
     ...state,
     selectedTab: InputName.Features,
