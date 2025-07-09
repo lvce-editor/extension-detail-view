938607902fa694c8f5a72a114e3f69f05d072081
Le Vivilet <levivlet@pm.me>
2025-07-09 23:03:16 +0200

    tsc

--- packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
@@ -1,19 +1,9 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
-import * as GetThemeMarkdown from '../GetThemeMarkdown/GetThemeMarkdown.ts'
 import * as InputName from '../InputName/InputName.ts'
-import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const selectTab = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  const { extension, baseUrl } = state
-  const { colorThemes, iconThemes, productIconThemes } = extension
-  // TODO do this depending on featured tab content
-  const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
-  const rendered = await RenderMarkdown.renderMarkdown(markdown, {
-    baseUrl,
-  })
   return {
     ...state,
     selectedTab: InputName.Features,
-    selectedFeatureMarkdownDom: rendered,
   }
 }
