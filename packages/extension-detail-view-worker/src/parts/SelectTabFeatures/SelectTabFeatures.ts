edf42c39a085dbecfcaa3184faeab8dc7e292dfe
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-09 23:54:48 +0200

    feature: query themes markdown when themes tab is selected (#457)

--- packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
@@ -1,3 +1,4 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 import { getMarkdownVirtualDom } from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
 import * as GetThemeMarkdown from '../GetThemeMarkdown/GetThemeMarkdown.ts'
@@ -5,17 +6,22 @@ import * as InputName from '../InputName/InputName.ts'
 import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const selectTabFeatures = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  const { extension, baseUrl } = state
-  const { colorThemes, iconThemes, productIconThemes } = extension
-  // TODO do this depending on featured tab content
-  const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
-  const rendered = await RenderMarkdown.renderMarkdown(markdown, {
-    baseUrl,
-  })
-  const dom = await getMarkdownVirtualDom(rendered)
+  const { extension, baseUrl, selectedFeature } = state
+
+  // Only generate theme markdown when the selected feature is actually "Theme"
+  let themesMarkdownDom: readonly VirtualDomNode[] = []
+  if (!selectedFeature || selectedFeature === InputName.Theme) {
+    const { colorThemes, iconThemes, productIconThemes } = extension
+    const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
+    const rendered = await RenderMarkdown.renderMarkdown(markdown, {
+      baseUrl,
+    })
+    themesMarkdownDom = await getMarkdownVirtualDom(rendered)
+  }
+
   return {
     ...state,
     selectedTab: InputName.Features,
-    themesMarkdownDom: dom,
+    themesMarkdownDom: themesMarkdownDom,
   }
 }
