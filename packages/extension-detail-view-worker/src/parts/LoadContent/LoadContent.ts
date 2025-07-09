bfc83f9efb7f553a4bcab676a8f36f3d0d0c02ea
Le Vivilet <levivlet@pm.me>
2025-07-10 00:58:06 +0200

    feature: remove unused html property

--- packages/extension-detail-view-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/extension-detail-view-worker/src/parts/LoadContent/LoadContent.ts
@@ -31,8 +31,6 @@ export const loadContent = async (state: ExtensionDetailState, platform: number,
   const readmeHtml = await RenderMarkdown.renderMarkdown(readmeContent, {
     baseUrl,
   })
-  const sanitizedReadmeHtml = readmeHtml
-  const normalizedReadmeHtml = sanitizedReadmeHtml
   const detailsVirtualDom = await getMarkdownVirtualDom(readmeHtml)
   const iconSrc = ExtensionDisplay.getIcon(extension, platform, assetDir)
   const description = ExtensionDisplay.getDescription(extension)
@@ -51,7 +49,6 @@ export const loadContent = async (state: ExtensionDetailState, platform: number,
   return {
     ...state,
     selectedTab,
-    sanitizedReadmeHtml: normalizedReadmeHtml,
     iconSrc,
     detailsVirtualDom,
     name,
