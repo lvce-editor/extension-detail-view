47197ef81fbb81e672bbeb38bc098ee9862d0faa
Le Vivilet <levivlet@pm.me>
2025-07-10 01:04:56 +0200

    tsc

--- packages/extension-detail-view-worker/src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailVirtualDom/GetExtensionDetailVirtualDom.ts
@@ -10,7 +10,6 @@ import * as GetExtensionDetailHeaderVirtualDom from '../GetExtensionDetailHeader
 import * as GetFeatures from '../GetFeatures/GetFeatures.ts'
 import * as GetTabs from '../GetTabs/GetTabs.ts'
 import * as GetTabsVirtualDom from '../GetTabsVirtualDom/GetTabsVirtualDom.ts'
-import * as GetViewletSize from '../GetViewletSize/GetViewletSize.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as ViewletSizeMap from '../ViewletSizeMap/ViewletSizeMap.ts'
 
