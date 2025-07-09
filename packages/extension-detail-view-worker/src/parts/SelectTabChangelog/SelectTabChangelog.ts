8e5883abd40ff423a01e09dbb492026f8a585877
Le Vivilet <levivlet@pm.me>
2025-07-09 22:22:06 +0200

    feature: request changelog dom when clicking changelog tab

--- packages/extension-detail-view-worker/src/parts/SelectTabChangelog/SelectTabChangelog.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabChangelog/SelectTabChangelog.ts
@@ -1,9 +1,20 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import { getMarkdownVirtualDom } from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
 import * as InputName from '../InputName/InputName.ts'
+import * as LoadChangelogContent from '../LoadChangelogContent/LoadChangelogContent.ts'
+import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const selectTabChangelog = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
+  const { extension, baseUrl } = state
+  const changelogContent = await LoadChangelogContent.loadChangelogContent(extension.path) // TODO use uri
+  const changelogMarkdownHtml = await RenderMarkdown.renderMarkdown(changelogContent, {
+    baseUrl,
+  })
+
+  const changelogDom = await getMarkdownVirtualDom(changelogMarkdownHtml)
   return {
     ...state,
     selectedTab: InputName.Changelog,
+    changelogVirtualDom: changelogDom,
   }
 }
