aca6f6e27a98d26b2b094292680e0917813b4eb7
Le Vivilet <levivlet@pm.me>
2025-07-09 23:40:55 +0200

    lint

--- packages/extension-detail-view-worker/test/GetFeaturesVirtualDom.test.ts
+++ packages/extension-detail-view-worker/test/GetFeaturesVirtualDom.test.ts
@@ -1,9 +1,10 @@
 import { expect, test } from '@jest/globals'
 import type { Feature } from '../src/parts/Feature/Feature.ts'
 import * as GetFeaturesVirtualDom from '../src/parts/GetFeaturesVirtualDom/GetFeaturesVirtualDom.ts'
+import { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 
 test.skip('features virtual dom', async () => {
-  const themesHtml = ''
+  const themesHtml: readonly VirtualDomNode[] = []
   const features: readonly Feature[] = [
     {
       id: 'Theme',
