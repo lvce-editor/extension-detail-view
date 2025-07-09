5c823a6cbc76375fc9a404d08a47cd6e9c761b58
Le Vivilet <levivlet@pm.me>
2025-07-09 23:37:28 +0200

    lint

--- packages/extension-detail-view-worker/src/parts/GetFeatureThemesVirtualDom/GetFeatureThemesVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetFeatureThemesVirtualDom/GetFeatureThemesVirtualDom.ts
@@ -1,9 +1,9 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
 import * as GetFeatureContentHeadingVirtualDom from '../GetFeatureContentHeadingVirtualDom/GetFeatureContentHeadingVirtualDom.ts'
 import * as GetVirtualDomChildCount from '../GetVirtualDomChildCount/GetVirtualDomChildCount.ts'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getFeatureThemesVirtualDom = async (themesDom: readonly VirtualDomNode[]): Promise<readonly VirtualDomNode[]> => {
   const childCount = GetVirtualDomChildCount.getVirtualDomChildCount(themesDom)
