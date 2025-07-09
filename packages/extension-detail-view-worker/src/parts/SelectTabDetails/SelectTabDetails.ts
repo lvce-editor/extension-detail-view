4c2711c81ddb92add00b606a7d215832da68b105
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-09 22:31:30 +0200

    feature: query readme dom when selecting details (#455)
    * feature: query readme dom when selecting details
    
    * test

--- packages/extension-detail-view-worker/src/parts/SelectTabDetails/SelectTabDetails.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabDetails/SelectTabDetails.ts
@@ -1,10 +1,21 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import * as GetBaseUrl from '../GetBaseUrl/GetBaseUrl.ts'
+import { getMarkdownVirtualDom } from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
 import * as InputName from '../InputName/InputName.ts'
+import * as GetExtensionReadme from '../LoadReadmeContent/LoadReadmeContent.ts'
+import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const selectTabDetails = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  // TODO load readmo markdown here
+  const { extension, platform } = state
+  const readmeContent = await GetExtensionReadme.loadReadmeContent(extension.path)
+  const baseUrl = GetBaseUrl.getBaseUrl(extension.path, platform)
+  const readmeHtml = await RenderMarkdown.renderMarkdown(readmeContent, {
+    baseUrl,
+  })
+  const detailsDom = await getMarkdownVirtualDom(readmeHtml)
   return {
     ...state,
     selectedTab: InputName.Details,
+    detailsVirtualDom: detailsDom,
   }
 }
