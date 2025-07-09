4a4531735fb66fde4a7119a786f1101a2a1c3214
Le Vivilet <levivlet@pm.me>
2025-07-09 17:09:01 +0200

    feature: use test exension for testing extension basics

--- packages/e2e/src/extension-detail.basics.ts
+++ packages/e2e/src/extension-detail.basics.ts
@@ -1,10 +1,12 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const test: Test = async ({ Main, Locator, expect }) => {
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
   // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-basics')
+  await Extension.addWebExtension(extensionUri)
 
   // act
-  await Main.openUri('extension-detail://builtin.theme-ayu')
+  await ExtensionDetail.open('test.extension-basics')
 
   // assert
   const detailView = Locator('.ExtensionDetail')
@@ -13,8 +15,20 @@ export const test: Test = async ({ Main, Locator, expect }) => {
   await expect(icon).toBeVisible()
   const name = Locator('.ExtensionDetailName')
   await expect(name).toBeVisible()
+  await expect(name).toHaveText('Test')
   const description = Locator('.ExtensionDetailDescription')
   await expect(description).toBeVisible()
+  await expect(description).toHaveText('Test Description')
+  const disableButton = Locator('[name="Disable"]')
+  await expect(disableButton).toBeVisible()
+  const uninstallButton = Locator('[name="Uninstall"]')
+  await expect(uninstallButton).toBeVisible()
   const tabs = Locator('.ExtensionDetailTabs')
   await expect(tabs).toBeVisible()
+  const tab1 = Locator('.ExtensionDetailTab').nth(0)
+  await expect(tab1).toHaveText('Details')
+  const tab2 = Locator('.ExtensionDetailTab').nth(1)
+  await expect(tab2).toHaveText('Features')
+  const tab3 = Locator('.ExtensionDetailTab').nth(2)
+  await expect(tab3).toHaveText('Changelog')
 }
