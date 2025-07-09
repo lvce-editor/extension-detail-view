8deeed40e605a37f993dffa74cabd348033d09bb
Le Vivilet <levivlet@pm.me>
2025-07-09 23:02:35 +0200

    feature: query thems markdown when themes tab is selected

--- packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
@@ -1,19 +1,27 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as GetThemeMarkdown from '../GetThemeMarkdown/GetThemeMarkdown.ts'
 import * as InputName from '../InputName/InputName.ts'
 import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
+import { getMarkdownVirtualDom } from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
 
-export const selectTab = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  const { extension, baseUrl } = state
-  const { colorThemes, iconThemes, productIconThemes } = extension
-  // TODO do this depending on featured tab content
-  const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
-  const rendered = await RenderMarkdown.renderMarkdown(markdown, {
-    baseUrl,
-  })
+export const selectTabFeatures = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
+  const { extension, baseUrl, selectedFeature } = state
+
+  // Only generate theme markdown when the selected feature is actually "Theme"
+  let themesMarkdownDom: readonly VirtualDomNode[] = []
+  if (selectedFeature === InputName.Theme) {
+    const { colorThemes, iconThemes, productIconThemes } = extension
+    const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
+    const rendered = await RenderMarkdown.renderMarkdown(markdown, {
+      baseUrl,
+    })
+    themesMarkdownDom = await getMarkdownVirtualDom(rendered)
+  }
+
   return {
     ...state,
     selectedTab: InputName.Features,
-    selectedFeatureMarkdownDom: rendered,
+    themesMarkdownDom,
   }
 }
