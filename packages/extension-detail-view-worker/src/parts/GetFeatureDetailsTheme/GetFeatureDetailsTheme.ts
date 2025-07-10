dff3e18d3b92a3624c84a5e4ff7e4f1e563deedc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-10 14:17:21 +0200

    feature: add getFeatureDetailsCommand (#486)

---
+++ packages/extension-detail-view-worker/src/parts/GetFeatureDetailsTheme/GetFeatureDetailsTheme.ts
@@ -0,0 +1,19 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import { getMarkdownVirtualDom } from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
+import * as GetThemeMarkdown from '../GetThemeMarkdown/GetThemeMarkdown.ts'
+import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
+
+export const getFeatureDetailsTheme = async (extension: any, baseUrl: string): Promise<Partial<ExtensionDetailState>> => {
+  // Only generate theme markdown when the selected feature is actually "Theme"
+  let themesMarkdownDom: readonly VirtualDomNode[] = []
+  const { colorThemes, iconThemes, productIconThemes } = extension
+  const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
+  const rendered = await RenderMarkdown.renderMarkdown(markdown, {
+    baseUrl,
+  })
+  themesMarkdownDom = await getMarkdownVirtualDom(rendered)
+  return {
+    themesMarkdownDom,
+  }
+}
