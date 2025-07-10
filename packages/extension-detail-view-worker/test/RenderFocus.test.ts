329431a0df4101fb39f6504980d61fd94471bd6e
Le Vivilet <levivlet@pm.me>
2025-07-10 19:57:25 +0200

    feature: add focus test

---
+++ packages/extension-detail-view-worker/test/RenderFocus.test.ts
@@ -0,0 +1,55 @@
+import { expect, test } from '@jest/globals'
+import * as RenderFocus from '../src/parts/RenderFocus/RenderFocus.ts'
+import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+
+test('renderFocus - returns focus command', () => {
+  const oldState = CreateDefaultState.createDefaultState()
+  const newState = CreateDefaultState.createDefaultState()
+
+  const result = RenderFocus.renderFocus(oldState, newState)
+
+  expect(result).toEqual(['Viewlet.focusElementByName', ''])
+})
+
+test('renderFocus - with different states', () => {
+  const oldState = CreateDefaultState.createDefaultState({
+    selectedTab: 'Features',
+    selectedFeature: 'Commands',
+  })
+  const newState = CreateDefaultState.createDefaultState({
+    selectedTab: 'Settings',
+    selectedFeature: 'General',
+  })
+
+  const result = RenderFocus.renderFocus(oldState, newState)
+
+  expect(result).toEqual(['Viewlet.focusElementByName', ''])
+})
+
+test('renderFocus - with empty states', () => {
+  const oldState = CreateDefaultState.createDefaultState()
+  const newState = CreateDefaultState.createDefaultState()
+
+  const result = RenderFocus.renderFocus(oldState, newState)
+
+  expect(result).toEqual(['Viewlet.focusElementByName', ''])
+})
+
+test('renderFocus - with complex states', () => {
+  const oldState = CreateDefaultState.createDefaultState({
+    extensionId: 'test-extension',
+    name: 'Test Extension',
+    selectedTab: 'Details',
+    selectedFeature: 'Features',
+  })
+  const newState = CreateDefaultState.createDefaultState({
+    extensionId: 'test-extension',
+    name: 'Test Extension',
+    selectedTab: 'Features',
+    selectedFeature: 'Commands',
+  })
+
+  const result = RenderFocus.renderFocus(oldState, newState)
+
+  expect(result).toEqual(['Viewlet.focusElementByName', ''])
+})
