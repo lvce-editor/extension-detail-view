59737eeb26862f75aca9975e887666fdbd9f7753
Le Vivilet <levivlet@pm.me>
2025-07-10 23:44:40 +0200

    feature: add copyImage command

--- packages/extension-detail-view-worker/test/CopyImage.test.ts
+++ packages/extension-detail-view-worker/test/CopyImage.test.ts
@@ -1,50 +1,9 @@
 import { expect, test } from '@jest/globals'
-import * as Create from '../src/parts/Create/Create.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as CopyImage from '../src/parts/CopyImage/CopyImage.ts'
 
-test('returns current state', () => {
-  Create.create(1, 'test-uri', 0, 0, 800, 600, 0, '')
-  const state = CopyImage.copyImage(1)
-  expect(state).toEqual({
-    activationEvents: [],
-    assetDir: '',
-    baseUrl: '',
-    builtinExtensionsBadgeEnabled: true,
-    categories: [],
-    changelogVirtualDom: [],
-    commands: [],
-    description: '',
-    detailsVirtualDom: [],
-    displaySize: '',
-    entries: [],
-    extension: {},
-    extensionId: '',
-    extensionVersion: '',
-    features: [],
-    featuresVirtualDom: [],
-    folderSize: 0,
-    hasColorTheme: false,
-    iconSrc: '',
-    isBuiltin: false,
-    jsonValidation: [],
-    name: '',
-    platform: 0,
-    programmingLanguages: [],
-    readmeScrollTop: 0,
-    resources: [],
-    scrollSource: 0,
-    scrollToTopButtonEnabled: false,
-    secondEntries: [],
-    selectedFeature: '',
-    selectedTab: '',
-    settings: [],
-    settingsButtonEnabled: false,
-    showAdditionalDetailsBreakpoint: 600,
-    sizeOnDisk: 0,
-    sizeValue: 0,
-    themesMarkdownDom: [],
-    uri: 'test-uri',
-    webViews: [],
-    width: 800,
-  })
+test('returns state as is', () => {
+  const state = createDefaultState({ uri: 'test-uri', width: 800 })
+  const result = CopyImage.copyImage(state)
+  expect(result).toBe(state)
 })
