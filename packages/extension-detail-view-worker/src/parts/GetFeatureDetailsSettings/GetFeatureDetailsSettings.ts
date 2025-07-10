a1314301edc76c1064b7e7ffe74ec9ccf4815079
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-10 14:27:50 +0200

    feature: add setting details (#490)

---
+++ packages/extension-detail-view-worker/src/parts/GetFeatureDetailsSettings/GetFeatureDetailsSettings.ts
@@ -0,0 +1,11 @@
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import type { Row } from '../Row/Row.ts'
+import * as GetSettingsTableEntry from '../GetSettingsTableEntry/GetSettingsTableEntry.ts'
+
+export const getFeatureDetailsCommand = (extension: any): Partial<ExtensionDetailState> => {
+  const settings = extension.settings || []
+  const rows: readonly Row[] = settings.map(GetSettingsTableEntry.getSettingsTableEntry)
+  return {
+    settings: rows,
+  }
+}
