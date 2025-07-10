f44986c9a3b6557f73415860456fc005b3f1b6b4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-10 23:50:59 +0200

    feature: set inputsource in handleWheel function (#548)

--- packages/extension-detail-view-worker/src/parts/HandleWheel/HandleWheel.ts
+++ packages/extension-detail-view-worker/src/parts/HandleWheel/HandleWheel.ts
@@ -1,9 +1,11 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
 export const handleWheel = (state: ExtensionDetailState, deltaX: number, deltaY: number): ExtensionDetailState => {
   const newScrollTop = Math.max(0, state.readmeScrollTop + deltaY)
   return {
     ...state,
     readmeScrollTop: newScrollTop,
+    scrollSource: InputSource.User,
   }
 }
