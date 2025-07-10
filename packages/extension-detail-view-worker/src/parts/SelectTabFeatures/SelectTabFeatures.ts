66941678ddacf2c6f0f5288e29d8f9440b0538be
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-10 14:12:18 +0200

    feature: use details handler when selecting feature (#485)

--- packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabFeatures/SelectTabFeatures.ts
@@ -1,27 +1,15 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
-import { getMarkdownVirtualDom } from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
-import * as GetThemeMarkdown from '../GetThemeMarkdown/GetThemeMarkdown.ts'
+import { getFeatureDetailsHandler } from '../GetFeatureDetailsHandler/GetFeatureDetailsHandler.ts'
 import * as InputName from '../InputName/InputName.ts'
-import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const selectTabFeatures = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
   const { extension, baseUrl, selectedFeature } = state
-
-  // Only generate theme markdown when the selected feature is actually "Theme"
-  let themesMarkdownDom: readonly VirtualDomNode[] = []
-  if (!selectedFeature || selectedFeature === InputName.Theme) {
-    const { colorThemes, iconThemes, productIconThemes } = extension
-    const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
-    const rendered = await RenderMarkdown.renderMarkdown(markdown, {
-      baseUrl,
-    })
-    themesMarkdownDom = await getMarkdownVirtualDom(rendered)
-  }
-
+  const actualSelectedFeature = selectedFeature || InputName.Theme
+  const fn = getFeatureDetailsHandler(actualSelectedFeature)
+  const partialNewState = await fn(extension, baseUrl)
   return {
     ...state,
     selectedTab: InputName.Features,
-    themesMarkdownDom: themesMarkdownDom,
+    ...partialNewState,
   }
 }
