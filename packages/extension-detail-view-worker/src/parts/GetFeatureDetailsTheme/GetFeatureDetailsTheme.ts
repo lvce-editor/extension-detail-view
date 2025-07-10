ddf2a3de638fdaf7b0733d67afe4d39ab004260d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-10 14:24:07 +0200

    feature: simplify theme details (#488)

--- packages/extension-detail-view-worker/src/parts/GetFeatureDetailsTheme/GetFeatureDetailsTheme.ts
+++ packages/extension-detail-view-worker/src/parts/GetFeatureDetailsTheme/GetFeatureDetailsTheme.ts
@@ -1,4 +1,3 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 import { getMarkdownVirtualDom } from '../GetMarkdownVirtualDom/GetMarkdownVirtualDom.ts'
 import * as GetThemeMarkdown from '../GetThemeMarkdown/GetThemeMarkdown.ts'
@@ -6,13 +5,12 @@ import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const getFeatureDetailsTheme = async (extension: any, baseUrl: string): Promise<Partial<ExtensionDetailState>> => {
   // Only generate theme markdown when the selected feature is actually "Theme"
-  let themesMarkdownDom: readonly VirtualDomNode[] = []
   const { colorThemes, iconThemes, productIconThemes } = extension
   const markdown = GetThemeMarkdown.getThemeMarkdown(colorThemes || [], iconThemes || [], productIconThemes || [])
   const rendered = await RenderMarkdown.renderMarkdown(markdown, {
     baseUrl,
   })
-  themesMarkdownDom = await getMarkdownVirtualDom(rendered)
+  const themesMarkdownDom = await getMarkdownVirtualDom(rendered)
   return {
     themesMarkdownDom,
   }
