ffae53b86d9ad241903d5fb8dba2f91992681b1b
Le Vivilet <levivlet@pm.me>
2025-07-10 14:28:35 +0200

    feature: add programmingLanguages details

---
+++ packages/extension-detail-view-worker/src/parts/GetFeatureDetailsProgrammingLanguages/GetFeatureDetailsProgrammingLanguages.ts
@@ -0,0 +1,7 @@
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+
+export const getFeatureDetailsProgrammingLanguages = (extension: any): Partial<ExtensionDetailState> => {
+  return {
+    programmingLanguages: [],
+  }
+}
