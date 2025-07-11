5ddd81359f064dd50daf086cdb036c95e6289f76
Le Vivilet <levivlet@pm.me>
2025-07-11 10:01:03 +0200

    feature: add scroll diffing

--- packages/extension-detail-view-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/extension-detail-view-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,6 +1,7 @@
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
+import * as DiffScrollTop from '../DiffScrollTop/DiffScrollTop.ts'
 
-export const modules = [DiffItems.isEqual, DiffFocus.isEqual]
+export const modules = [DiffItems.isEqual, DiffFocus.isEqual, DiffScrollTop.isEqual]
 
-export const numbers = [DiffItems.diffType, DiffFocus.diffType]
+export const numbers = [DiffItems.diffType, DiffFocus.diffType, DiffScrollTop.diffType]
