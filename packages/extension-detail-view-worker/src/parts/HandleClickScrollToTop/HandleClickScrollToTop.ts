838f01ab3ba48464961b54c78da917d73eccd316
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-11 10:04:14 +0200

    feature: don't scroll to top if already at top (#555)

--- packages/extension-detail-view-worker/src/parts/HandleClickScrollToTop/HandleClickScrollToTop.ts
+++ packages/extension-detail-view-worker/src/parts/HandleClickScrollToTop/HandleClickScrollToTop.ts
@@ -1,6 +1,10 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 
 export const handleClickScrollToTop = (state: ExtensionDetailState): ExtensionDetailState => {
+  const { readmeScrollTop } = state
+  if (readmeScrollTop === 0) {
+    return state
+  }
   return {
     ...state,
     readmeScrollTop: 0,
