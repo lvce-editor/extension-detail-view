4cc8d94d3446d6486b09dae5e453d562c8400430
Le Vivilet <levivlet@pm.me>
2025-07-13 23:08:12 +0200

    feature: move badge to header data

---
+++ packages/extension-detail-view-worker/src/parts/HeaderData/HeaderData.ts
@@ -0,0 +1,11 @@
+export interface HeaderData {
+  extensionId: string
+  name: string
+  extensionUri: string
+  iconSrc: string
+  extensionVersion: string
+  description: string
+  extension: any
+  hasColorTheme: boolean
+  badge: string
+}
--- packages/extension-detail-view-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/extension-detail-view-worker/src/parts/LoadContent/LoadContent.ts
@@ -4,7 +4,6 @@ import type { MoreInfoEntry } from '../MoreInfoEntry/MoreInfoEntry.ts'
 import type { Resource } from '../Resource/Resource.ts'
 import * as ExtensionManagement from '../ExtensionManagement/ExtensionManagement.ts'
 import * as FeatureRegistry from '../FeatureRegistry/FeatureRegistry.ts'
-import * as GetBadge from '../GetBadge/GetBadge.ts'
 import * as GetBaseUrl from '../GetBaseUrl/GetBaseUrl.ts'
 import * as GetCategories from '../GetCategories/GetCategories.ts'
 import * as GetDisplaySize from '../GetDisplaySize/GetDisplaySize.ts'
@@ -15,22 +14,22 @@ import { getMarkdownVirtualDom } from '../GetMarkdownVirtualDom/GetMarkdownVirtu
 import * as GetResources from '../GetResources/GetResources.ts'
 import * as GetSecondEntries from '../GetSecondEntries/GetSecondEntries.ts'
 import * as GetViewletSize from '../GetViewletSize/GetViewletSize.ts'
-import * as HasColorThemes from '../HasColorThemes/HasColorThemes.ts'
 import * as InputName from '../InputName/InputName.ts'
 import * as LoadHeaderContent from '../LoadHeaderContent/LoadHeaderContent.ts'
 import * as GetExtensionReadme from '../LoadReadmeContent/LoadReadmeContent.ts'
 import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 import * as RestoreState from '../RestoreState/RestoreState.ts'
+import { HeaderData } from '../HeaderData/HeaderData.ts'
 
 export const loadContent = async (state: ExtensionDetailState, platform: number, savedState: unknown): Promise<ExtensionDetailState> => {
-  const { width, builtinExtensionsBadgeEnabled, uri } = state
+  const { width, uri } = state
   const id = getExtensionIdFromUri(uri)
   const extension = await ExtensionManagement.getExtension(id, platform)
   if (!extension) {
     throw new Error(`extension not found: ${id}`)
   }
-  const headerData = LoadHeaderContent.loadHeaderContent(state, platform, extension)
-  const { extensionId, name, extensionUri, iconSrc, extensionVersion, description } = headerData
+  const headerData: HeaderData = LoadHeaderContent.loadHeaderContent(state, platform, extension)
+  const { extensionId, name, extensionUri, iconSrc, extensionVersion, description, hasColorTheme, badge } = headerData
   const readmeContent = await GetExtensionReadme.loadReadmeContent(extension.path)
   const baseUrl = GetBaseUrl.getBaseUrl(extension.path, platform)
   const readmeHtml = await RenderMarkdown.renderMarkdown(readmeContent, {
@@ -50,8 +49,6 @@ export const loadContent = async (state: ExtensionDetailState, platform: number,
   const resources: readonly Resource[] = GetResources.getResources()
   const sizeValue = GetViewletSize.getViewletSize(width || 0)
   const isBuiltin = extension?.builtin
-  const hasColorTheme = HasColorThemes.hasColorThemes(extension)
-  const badge = GetBadge.getBadge(isBuiltin, builtinExtensionsBadgeEnabled)
   return {
     ...state,
     badge,
