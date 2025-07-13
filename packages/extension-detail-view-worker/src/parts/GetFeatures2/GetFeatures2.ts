6f4b751f9455875c84884c481fde4688082e3481
Le Vivilet <levivlet@pm.me>
2025-07-13 19:43:54 +0200

    feature: add getFeatures2

---
+++ packages/extension-detail-view-worker/src/parts/GetFeatures2/GetFeatures2.ts
@@ -0,0 +1,10 @@
+import type { Feature } from '../Feature/Feature.ts'
+import * as FeatureRegistry from '../FeatureRegistry/FeatureRegistry.ts'
+import * as InputName from '../InputName/InputName.ts'
+
+export const getFeatures2 = (selectedFeature: string, extension: any): readonly Feature[] => {
+  if (!selectedFeature) {
+    selectedFeature = InputName.Theme
+  }
+  return FeatureRegistry.getFeatures(selectedFeature, extension)
+}
