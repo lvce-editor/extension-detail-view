8cd3c34d205cfe1b3cfd382f18b5fea331ca901f
Le Vivilet <levivlet@pm.me>
2025-07-13 21:18:49 +0200

    fix

--- packages/extension-detail-view-worker/src/parts/FeatureCommandsEnabled/FeatureCommandsEnabled.ts
+++ packages/extension-detail-view-worker/src/parts/FeatureCommandsEnabled/FeatureCommandsEnabled.ts
@@ -1,10 +1,6 @@
 export const featureCommandsEnabled = (extension: unknown): boolean => {
-  if (!extension || typeof extension !== 'object') {
+  if (!extension || typeof extension !== 'object'|| (!('commands' in extension)) {
     return false
   }
-  if (!('commands' in extension)) {
-    return false
-  }
-  const {commands} = extension
-  return Array.isArray(commands)
+  return Array.isArray(extension.commands)
 }
