f925d7415c9f329914f6f2a418dbaadf672a9d1b
Le Vivilet <levivlet@pm.me>
2025-07-13 21:27:11 +0200

    undo rename

--- packages/extension-detail-view-worker/src/parts/GetFeatures2/GetFeatures2.ts
+++ packages/extension-detail-view-worker/src/parts/GetFeatures2/GetFeatures2.ts
@@ -2,7 +2,7 @@ import type { Feature } from '../Feature/Feature.ts'
 import * as FeatureRegistry from '../FeatureRegistry/FeatureRegistry.ts'
 import * as InputName from '../InputName/InputName.ts'
 
-export const getFeatures = (selectedFeature: string, extension: any): readonly Feature[] => {
+export const getFeatures2 = (selectedFeature: string, extension: any): readonly Feature[] => {
   if (!selectedFeature) {
     selectedFeature = InputName.Theme
   }
