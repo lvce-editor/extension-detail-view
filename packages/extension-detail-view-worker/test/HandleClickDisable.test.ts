b23b711826affa4edf209773cadd9597ecc51ff2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-13 22:08:31 +0200

    feature: add disable test (#594)

---
+++ packages/extension-detail-view-worker/test/HandleClickDisable.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { handleClickDisable } from '../src/parts/HandleClickDisable/HandleClickDisable.ts'
+
+test('handleClickDisable returns the same state', async () => {
+  const state = createDefaultState()
+  const result = await handleClickDisable(state)
+  expect(result).toBe(state)
+})
