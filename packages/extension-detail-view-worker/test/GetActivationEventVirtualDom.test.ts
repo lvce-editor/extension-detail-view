e0d44ecc9f30d5505cb194bfae7f8f95a7c93d0c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-13 21:49:27 +0200

    feature: add dom tests (#588)

---
+++ packages/extension-detail-view-worker/test/GetActivationEventVirtualDom.test.ts
@@ -0,0 +1,37 @@
+import { expect, test } from '@jest/globals'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as GetActivationEventVirtualDom from '../src/parts/GetActivationEventVirtualDom/GetActivationEventVirtualDom.ts'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
+
+test('activation event virtual dom', () => {
+  const event = 'onCommand:workbench.action.openFile'
+  expect(GetActivationEventVirtualDom.getActivationEventVirtualDom(event)).toEqual([
+    {
+      type: VirtualDomElements.Li,
+      childCount: 1,
+    },
+    text('onCommand:workbench.action.openFile'),
+  ])
+})
+
+test('activation event virtual dom with different event', () => {
+  const event = 'onLanguage:typescript'
+  expect(GetActivationEventVirtualDom.getActivationEventVirtualDom(event)).toEqual([
+    {
+      type: VirtualDomElements.Li,
+      childCount: 1,
+    },
+    text('onLanguage:typescript'),
+  ])
+})
+
+test('activation event virtual dom with empty event', () => {
+  const event = ''
+  expect(GetActivationEventVirtualDom.getActivationEventVirtualDom(event)).toEqual([
+    {
+      type: VirtualDomElements.Li,
+      childCount: 1,
+    },
+    text(''),
+  ])
+})
---
+++ packages/extension-detail-view-worker/test/GetFeatureActivationEventsVirtualDom.test.ts
@@ -0,0 +1,64 @@
+import { expect, test } from '@jest/globals'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetFeatureActivationEventsVirtualDom from '../src/parts/GetFeatureActivationEventsVirtualDom/GetFeatureActivationEventsVirtualDom.ts'
+import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
+
+test('feature activation events virtual dom with events', () => {
+  const activationEvents: readonly string[] = [
+    'onCommand:workbench.action.openFile',
+    'onLanguage:typescript',
+    'onView:explorer',
+  ]
+  expect(GetFeatureActivationEventsVirtualDom.getFeatureActivationEventsVirtualDom(activationEvents)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.FeatureContent,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.H1,
+      childCount: 1,
+    },
+    text('Activation Events'),
+    {
+      type: VirtualDomElements.Ul,
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.Li,
+      childCount: 1,
+    },
+    text('onCommand:workbench.action.openFile'),
+    {
+      type: VirtualDomElements.Li,
+      childCount: 1,
+    },
+    text('onLanguage:typescript'),
+    {
+      type: VirtualDomElements.Li,
+      childCount: 1,
+    },
+    text('onView:explorer'),
+  ])
+})
+
+test('feature activation events virtual dom with empty events', () => {
+  const activationEvents: readonly string[] = []
+  expect(GetFeatureActivationEventsVirtualDom.getFeatureActivationEventsVirtualDom(activationEvents)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.FeatureContent,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.H1,
+      childCount: 1,
+    },
+    text('Activation Events'),
+    {
+      type: VirtualDomElements.Ul,
+      childCount: 0,
+    },
+  ])
+})
