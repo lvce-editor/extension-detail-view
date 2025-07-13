cd28f140cb108aa77393b3b45e48642a6222c2bd
Le Vivilet <levivlet@pm.me>
2025-07-13 22:09:08 +0200

    feature: add resize test

---
+++ packages/extension-detail-view-worker/test/Resize.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { resize } from '../src/parts/Resize/Resize.ts'
+
+test('resize merges dimensions into state', () => {
+  const state = createDefaultState()
+  const dimensions = { x: 0, y: 0, width: 800, height: 600 }
+  const result = resize(state, dimensions)
+
+  expect(result.width).toBe(800)
+  expect(result).toEqual({ ...state, ...dimensions })
+})
