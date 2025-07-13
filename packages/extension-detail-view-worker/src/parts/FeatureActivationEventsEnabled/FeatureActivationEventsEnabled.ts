47a1b910b8213cbf8a51cee7d804db3cbacaf855
Le Vivilet <levivlet@pm.me>
2025-07-13 21:18:10 +0200

    format code

--- packages/extension-detail-view-worker/src/parts/FeatureActivationEventsEnabled/FeatureActivationEventsEnabled.ts
+++ packages/extension-detail-view-worker/src/parts/FeatureActivationEventsEnabled/FeatureActivationEventsEnabled.ts
@@ -1,10 +1,7 @@
 export const featureActivationEventsEnabled = (extension: unknown): boolean => {
-  if (!extension || typeof extension !== 'object') {
+  if (!extension || typeof extension !== 'object' || !('activation' in extension)) {
     return false
   }
-  if (!('activation' in extension)) {
-    return false
-  }
-  const {activation} = extension
+  const { activation } = extension
   return typeof activation === 'object' && activation !== null
 }
