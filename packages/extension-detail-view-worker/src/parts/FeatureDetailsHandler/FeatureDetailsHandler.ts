5185157fda679e07a45c1b68a285fa0e7d1a6914
Le Vivilet <levivlet@pm.me>
2025-07-13 19:57:12 +0200

    feature: add features dom handler

--- packages/extension-detail-view-worker/src/parts/FeatureDetailsHandler/FeatureDetailsHandler.ts
+++ packages/extension-detail-view-worker/src/parts/FeatureDetailsHandler/FeatureDetailsHandler.ts
@@ -1,5 +1,10 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 
 export interface FeatureDetailsHandler {
   (extension: any, baseUrl: string): Partial<ExtensionDetailState> | Promise<Partial<ExtensionDetailState>>
+}
+
+export interface FeatureDomHandler {
+  (state: ExtensionDetailState): readonly VirtualDomNode[]
 }
---
+++ packages/extension-detail-view-worker/src/parts/GetFeaturesVirtualDom2/GetFeaturesVirtualDom2.ts
@@ -0,0 +1,46 @@
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import type { Feature } from '../Feature/Feature.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
+import { getFeatureVirtualDomHandler } from '../FeatureRegistry/FeatureRegistry.ts'
+import * as GetFeatureListVirtualDom from '../GetFeatureListVirtualDom/GetFeatureListVirtualDom.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+
+export const getFeaturesVirtualDom2 = (
+  features: readonly Feature[],
+  selectedFeature: string,
+  state: ExtensionDetailState,
+): readonly VirtualDomNode[] => {
+  if (features.length === 0) {
+    const none = ExtensionDetailStrings.none()
+    return [
+      {
+        type: VirtualDomElements.Div,
+        className: ClassNames.Features,
+        childCount: 1,
+      },
+      text(none),
+    ]
+  }
+
+  const fn = getFeatureVirtualDomHandler(selectedFeature)
+  const featureVirtualDom = fn(state)
+
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Features,
+      childCount: 3,
+    },
+    ...GetFeatureListVirtualDom.getFeatureListVirtualDom(features),
+    {
+      type: VirtualDomElements.Div,
+      className: MergeClassNames.mergeClassNames(ClassNames.Sash, ClassNames.SashVertical),
+      childCount: 0,
+    },
+    ...featureVirtualDom,
+  ]
+}
