3c2a8cf0830c71f1412118e204211113a0c60da6
Le Vivilet <levivlet@pm.me>
2025-07-13 22:27:21 +0200

    lint

--- packages/extension-detail-view-worker/test/SelectTabFeatures.test.ts
+++ packages/extension-detail-view-worker/test/SelectTabFeatures.test.ts
@@ -2,10 +2,10 @@ import { test, expect } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
 import type { ExtensionDetailState } from '../src/parts/ExtensionDetailState/ExtensionDetailState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
-import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
-import { selectTabFeatures } from '../src/parts/SelectTabFeatures/SelectTabFeatures.ts'
 import { register } from '../src/parts/FeatureRegistry/FeatureRegistry.ts'
 import * as InputName from '../src/parts/InputName/InputName.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
+import { selectTabFeatures } from '../src/parts/SelectTabFeatures/SelectTabFeatures.ts'
 
 test('should select features tab and first feature when selectedFeature is null', async () => {
   const mockRpc = MockRpc.create({
