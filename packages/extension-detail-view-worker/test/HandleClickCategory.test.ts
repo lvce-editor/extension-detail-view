ba5268ebd975f12fec47aaec1d603a7affe4db64
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-13 22:28:08 +0200

    feature: add category test (#602)

---
+++ packages/extension-detail-view-worker/test/HandleClickCategory.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { handleClickCategory } from '../src/parts/HandleClickCategory/HandleClickCategory.ts'
+
+test('handleClickCategory returns the same state', async () => {
+  const state = createDefaultState()
+  const result = await handleClickCategory(state, 'category-1')
+  expect(result).toBe(state)
+})
