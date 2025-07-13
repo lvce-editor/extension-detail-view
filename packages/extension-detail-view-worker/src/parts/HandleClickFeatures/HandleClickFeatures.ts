edafe966a47dbd76b29d1e8069245ef9ad78eb15
Le Vivilet <levivlet@pm.me>
2025-07-13 22:16:10 +0200

    feature: remove duplicate code

--- packages/extension-detail-view-worker/src/parts/HandleClickFeatures/HandleClickFeatures.ts
+++ packages/extension-detail-view-worker/src/parts/HandleClickFeatures/HandleClickFeatures.ts
@@ -1,6 +1,6 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
-import { selectFeature2 } from '../SelectFeature2/SelectFeature2.ts'
+import { selectFeature } from '../SelectFeature/SelectFeature.ts'
 
 export const handleClickFeatures = async (state: ExtensionDetailState, name: string): Promise<ExtensionDetailState> => {
-  return selectFeature2(state, name)
+  return selectFeature(state, name)
 }
