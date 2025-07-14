9abfa7e567b007dc78d2aca6ce95d0b051209bf5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-14 10:54:37 +0200

    feature: improve scrolltop render (#622)
    * feature: improve scrolltop render
    
    * test

--- packages/extension-detail-view-worker/src/parts/RenderScrollTop/RenderScrollTop.ts
+++ packages/extension-detail-view-worker/src/parts/RenderScrollTop/RenderScrollTop.ts
@@ -1,5 +1,7 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 
 export const renderScrollTop = (oldState: ExtensionDetailState, newState: ExtensionDetailState): readonly any[] => {
-  return ['Viewlet.setScrollTop', '', newState.readmeScrollTop]
+  const selector = '.ExtensionDetailPanel .Markdown'
+  const property = 'scrollTop'
+  return ['Viewlet.setProperty', selector, property, newState.readmeScrollTop]
 }
