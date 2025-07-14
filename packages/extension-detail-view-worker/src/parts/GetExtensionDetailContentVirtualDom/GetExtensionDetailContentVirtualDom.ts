c9bbf091c08a5532459ce3d524d898e062bebe47
Le Vivilet <levivlet@pm.me>
2025-07-14 13:39:00 +0200

    lint

--- packages/extension-detail-view-worker/src/parts/GetExtensionDetailContentVirtualDom/GetExtensionDetailContentVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailContentVirtualDom/GetExtensionDetailContentVirtualDom.ts
@@ -1,11 +1,11 @@
 import type { Category } from '../Category/Category.ts'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import type { Resource } from '../Resource/Resource.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as GetChangelogVirtualDom from '../GetChangelogVirtualDom/GetChangelogVirtualDom.ts'
 import * as GetDetailsVirtualDom from '../GetDetailsVirtualDom/GetDetailsVirtualDom.ts'
 import { getFeaturesVirtualDom } from '../GetFeaturesVirtualDom/GetFeaturesVirtualDom.ts'
 import * as InputName from '../InputName/InputName.ts'
-import type { Resource } from '../Resource/Resource.ts'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getExtensionDetailContentVirtualDom = (
   sanitizedReadmeHtml: readonly VirtualDomNode[],
