bd2568eb644661d037686cc68ed9bbc04e1877d6
Le Vivilet <levivlet@pm.me>
2025-07-15 20:32:41 +0200

    rename

--- packages/extension-detail-view-worker/src/parts/SendMessagePortToFileSystemWorker/SendMessagePortToFileSystemWorker.ts
+++ packages/extension-detail-view-worker/src/parts/SendMessagePortToFileSystemWorker/SendMessagePortToFileSystemWorker.ts
@@ -1,5 +1,5 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
-export const sendMessagePortToMarkdownWorker = async (port: any): Promise<void> => {
+export const sendMessagePortToFileSystemWorker = async (port: any): Promise<void> => {
   await RendererWorker.sendMessagePortToFileSystemWorker(port, 0)
 }
