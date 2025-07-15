b81037127fe076e58e23fc794e90e95ba90070e6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-15 20:37:34 +0200

    fix: highlighting selected tab (#655)

--- packages/extension-detail-view-worker/src/parts/SelectTabChangelog/SelectTabChangelog.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabChangelog/SelectTabChangelog.ts
@@ -5,16 +5,22 @@ import * as LoadChangelogContent from '../LoadChangelogContent/LoadChangelogCont
 import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const selectTabChangelog = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  const { extension, baseUrl } = state
+  const { extension, baseUrl, tabs } = state
   const changelogContent = await LoadChangelogContent.loadChangelogContent(extension.path) // TODO use uri
   const changelogMarkdownHtml = await RenderMarkdown.renderMarkdown(changelogContent, {
     baseUrl,
   })
-
   const changelogDom = await getMarkdownVirtualDom(changelogMarkdownHtml)
+  const newTabs = tabs.map((tab) => {
+    return {
+      ...tab,
+      selected: tab.name === InputName.Changelog,
+    }
+  })
   return {
     ...state,
     selectedTab: InputName.Changelog,
     changelogVirtualDom: changelogDom,
+    tabs: newTabs,
   }
 }
--- packages/extension-detail-view-worker/src/parts/SelectTabDetails/SelectTabDetails.ts
+++ packages/extension-detail-view-worker/src/parts/SelectTabDetails/SelectTabDetails.ts
@@ -5,15 +5,22 @@ import * as GetExtensionReadme from '../LoadReadmeContent/LoadReadmeContent.ts'
 import * as RenderMarkdown from '../RenderMarkdown/RenderMarkdown.ts'
 
 export const selectTabDetails = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  const { baseUrl, readmeUrl } = state
+  const { baseUrl, readmeUrl, tabs } = state
   const readmeContent = await GetExtensionReadme.loadReadmeContent(readmeUrl)
   const readmeHtml = await RenderMarkdown.renderMarkdown(readmeContent, {
     baseUrl,
   })
   const detailsDom = await getMarkdownVirtualDom(readmeHtml)
+  const newTabs = tabs.map((tab) => {
+    return {
+      ...tab,
+      selected: tab.name === InputName.Details,
+    }
+  })
   return {
     ...state,
     selectedTab: InputName.Details,
     detailsVirtualDom: detailsDom,
+    tabs: newTabs,
   }
 }
