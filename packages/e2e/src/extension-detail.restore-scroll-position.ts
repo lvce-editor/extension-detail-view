b8db34167591572f4fadd53a9074d93687baf0ef
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-17 18:21:20 +0200

    feature: improve scroll restore e2e test (#664)

--- packages/e2e/src/extension-detail.restore-scroll-position.ts
+++ packages/e2e/src/extension-detail.restore-scroll-position.ts
@@ -1,16 +1,21 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail, Command }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/extension-restore-scroll-position')
   await Extension.addWebExtension(extensionUri)
-
-  // act
   await ExtensionDetail.open('test.restore-scroll-position')
-
-  // assert
   const detailView = Locator('.ExtensionDetail')
   await expect(detailView).toBeVisible()
   const icon = Locator('.Markdown')
   await expect(icon).toBeVisible()
+  await Command.execute('ExtensionDetail.handleScroll', 10)
+  await ExtensionDetail.selectTab('Features')
+
+  // act
+  await ExtensionDetail.selectTab('Details')
+
+  // assert
+
+  // TODO
 }
