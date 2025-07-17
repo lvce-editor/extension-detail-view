06be6f1bdf75aa4e031a84b3c64109f8d73e3005
Le Vivilet <levivlet@pm.me>
2025-07-17 18:12:31 +0200

    feature: add inputSource property to handleScroll

--- packages/extension-detail-view-worker/src/parts/HandleScroll/HandleScroll.ts
+++ packages/extension-detail-view-worker/src/parts/HandleScroll/HandleScroll.ts
@@ -1,11 +1,11 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
-export const handleScroll = (state: ExtensionDetailState, scrollTop: number): ExtensionDetailState => {
+export const handleScroll = (state: ExtensionDetailState, scrollTop: number, scrollSource = InputSource.Script): ExtensionDetailState => {
   const newScrollTop = Math.max(0, scrollTop)
   return {
     ...state,
     readmeScrollTop: newScrollTop,
-    scrollSource: InputSource.User,
+    scrollSource,
   }
 }
