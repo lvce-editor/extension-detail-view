3f9d9c59af8d8b57ca4e5b15af43fe354e15f969
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-18 22:41:15 +0200

    feature: simplify saveState command (#688)

--- packages/extension-detail-view-worker/test/SaveState.test.ts
+++ packages/extension-detail-view-worker/test/SaveState.test.ts
@@ -1,10 +1,12 @@
 import { expect, test } from '@jest/globals'
 import * as Create from '../src/parts/Create/Create.ts'
+import { get } from '../src/parts/ExtensionDetailStates/ExtensionDetailStates.ts'
 import * as SaveState from '../src/parts/SaveState/SaveState.ts'
 
 test('saves state with selected tab and feature', () => {
   Create.create(1, 'test-uri', 0, 0, 800, 600, 0, '')
-  const savedState = SaveState.saveState(1)
+  const { newState } = get(1)
+  const savedState = SaveState.saveState(newState)
   expect(savedState).toEqual({
     selectedTab: '',
     selectedFeature: '',
