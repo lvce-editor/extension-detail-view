0042e75175628467da64ea28a5409ae68f08107e
Le Vivilet <levivlet@pm.me>
2025-07-18 19:45:04 +0200

    feature: remove unused integration tests

--- packages/test-integration/test/render-markdown.test.js
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@jest/globals'
-import { setup } from '../src/test.js'
-
-test.skip('render-markdown', async () => {
-  const rpc = await setup()
-  const content = '# Test'
-  const result = await rpc.invoke('RenderMarkdown.renderMarkdown', content)
-  expect(result).toBe('<h1>Test</h1>\n')
-})
