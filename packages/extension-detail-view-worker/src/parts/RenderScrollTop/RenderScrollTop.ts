945e0b600ee57bb9bd38962aac975375ff9d402c
Le Vivilet <levivlet@pm.me>
2025-07-18 15:16:18 +0200

    feature: render readme or changelog scrolltop depending on opened tab

--- packages/extension-detail-view-worker/src/parts/RenderScrollTop/RenderScrollTop.ts
+++ packages/extension-detail-view-worker/src/parts/RenderScrollTop/RenderScrollTop.ts
@@ -1,7 +1,23 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import * as InputName from '../InputName/InputName.ts'
+
+const getScrollTop = (selectedTab: string, readmeScrollTop: number, changelogScrollTop: number): number => {
+  if (selectedTab === InputName.Details) {
+    return readmeScrollTop
+  }
+  if (selectedTab === InputName.Changelog) {
+    return changelogScrollTop
+  }
+  return -1
+}
 
 export const renderScrollTop = (oldState: ExtensionDetailState, newState: ExtensionDetailState): readonly any[] => {
   const selector = '.ExtensionDetailPanel .Markdown'
   const property = 'scrollTop'
-  return ['Viewlet.setProperty', newState.uid, selector, property, newState.readmeScrollTop]
+  const { uid, readmeScrollTop, changelogScrollTop, selectedTab } = newState
+  const scrollTop = getScrollTop(selectedTab, readmeScrollTop, changelogScrollTop)
+  if (scrollTop === -1) {
+    return []
+  }
+  return ['Viewlet.setProperty', uid, selector, property, scrollTop]
 }
