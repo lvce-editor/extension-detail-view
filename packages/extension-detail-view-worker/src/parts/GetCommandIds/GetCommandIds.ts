167fa4f1cda2d65eb561f74af17e184e1f767dcb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-21 12:25:22 +0200

    feature: simplify command ids (#710)

--- packages/extension-detail-view-worker/src/parts/GetCommandIds/GetCommandIds.ts
+++ packages/extension-detail-view-worker/src/parts/GetCommandIds/GetCommandIds.ts
@@ -1,5 +1,5 @@
-import { commandIds } from '../CommandIds/CommandIds.ts'
 
-export const getCommandIds = (): readonly string[] => {
-  return commandIds
-}
+
+
+
+export {getCommandIds} from '../ExtensionDetailStates/ExtensionDetailStates.ts'
