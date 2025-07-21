c426bb0ff0273e01f6fb4a09cda821e6a40dcc7e
Le Vivilet <levivlet@pm.me>
2025-07-21 12:55:10 +0200

    tsc

--- packages/extension-detail-view-worker/src/parts/FileSystemWorker/FileSystemWorker.ts
+++ packages/extension-detail-view-worker/src/parts/FileSystemWorker/FileSystemWorker.ts
@@ -2,7 +2,7 @@ import { FileSystemWorker } from '@lvce-editor/rpc-registry'
 
 export const { set, exists, readDirWithFileTypes, readFile, invoke } = FileSystemWorker
 
-export const readFileAsBlob = async (uri: string) => {
+export const readFileAsBlob = async (uri: string): Promise<any> => {
   // TODO maybe readAsObjectUrl?
   // @ts-ignore
   return invoke('FileSystem.readFileAsBlob', uri)
