bda4b3d7859972e5dcf2a17183fa6b6e13305838
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-21 16:08:10 +0200

    feature: initialize extension host worker rpc on start (#730)

--- packages/extension-detail-view-worker/src/parts/Initialize/Initialize.ts
+++ packages/extension-detail-view-worker/src/parts/Initialize/Initialize.ts
@@ -1,6 +1,7 @@
+import { initializeExtensionHostWorker } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
 import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 import { initializeMarkdownWorker } from '../InitializeMarkdownWorker/InitializeMarkdownWorker.ts'
 
 export const initialize = async (): Promise<void> => {
-  await Promise.all([initializeMarkdownWorker(), initializeFileSystemWorker()])
+  await Promise.all([initializeMarkdownWorker(), initializeFileSystemWorker(), initializeExtensionHostWorker()])
 }
