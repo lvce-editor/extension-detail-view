cbd830ece5dc4d4f13fd9fc6b1bcd2926c327be4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-21 13:18:06 +0200

    feature: add openImageInNewTab (#718)

---
+++ packages/extension-detail-view-worker/src/parts/OpenImageInNewTab/OpenImageInNewTab.ts
@@ -0,0 +1,5 @@
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+
+export const openImageInNewTab = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
+  return state
+}
