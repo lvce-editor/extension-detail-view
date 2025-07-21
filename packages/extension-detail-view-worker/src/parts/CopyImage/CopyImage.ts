f8f513e29cdb460c1ff53707e90c66a5382a9604
Le Vivilet <levivlet@pm.me>
2025-07-21 13:14:30 +0200

    feature: write image to clipboard

--- packages/extension-detail-view-worker/src/parts/CopyImage/CopyImage.ts
+++ packages/extension-detail-view-worker/src/parts/CopyImage/CopyImage.ts
@@ -1,5 +1,14 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import * as ClipBoard from '../Clipboard/Clipboard.ts'
+import * as FileSystemWorker from '../FileSystemWorker/FileSystemWorker.ts'
 
-export const copyImage = (state: ExtensionDetailState): ExtensionDetailState => {
+export const copyImage = async (state: ExtensionDetailState): Promise<Promise<ExtensionDetailState>> => {
+  const { iconSrc } = state
+  // TODO support gitpod url prefixes
+  // @ts-ignore
+  const prefix = `${location.protocol}//${location.host}`
+  const absoluteIconSrc = `${prefix}${iconSrc}`
+  const blob = await FileSystemWorker.readFileAsBlob(absoluteIconSrc)
+  await ClipBoard.writeClipboardImage(blob)
   return state
 }
