a0222b6a1636badd5df91ce4345295aeb807b7a5
Le Vivilet <levivlet@pm.me>
2025-07-21 16:17:00 +0200

    simplify test

--- packages/extension-detail-view-worker/test/OpenExtensionSearch.test.ts
+++ packages/extension-detail-view-worker/test/OpenExtensionSearch.test.ts
@@ -4,29 +4,21 @@ import * as OpenExtensionSearch from '../src/parts/OpenExtensionSearch/OpenExten
 import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 
 test('openExtensionSearch calls RendererWorker.openExtensionSearch and setExtensionsSearchValue', async () => {
-  const setExtensionsSearchValueMock = jest.fn()
-  const openExtensionSearchMock = jest.fn()
-  const openViewletMock = jest.fn()
-
-  const mockInvoke = (method: string, ...args: readonly any[]) => {
+  const mockInvoke = jest.fn((method: string, ...args: readonly any[]) => {
     if (method === 'openExtensionSearch') {
-      openExtensionSearchMock(...args)
       return undefined
     }
     if (method === 'setExtensionsSearchValue') {
-      setExtensionsSearchValueMock(...args)
       return undefined
     }
     if (method === 'Extensions.handleInput') {
-      setExtensionsSearchValueMock(...args)
       return undefined
     }
     if (method === 'SideBar.openViewlet') {
-      openViewletMock(...args)
       return undefined
     }
     throw new Error(`unexpected method ${method}`)
-  }
+  })
 
   const mockRpc = MockRpc.create({
     commandMap: {},
@@ -37,6 +29,6 @@ test('openExtensionSearch calls RendererWorker.openExtensionSearch and setExtens
   const searchValue = 'test-search'
   await OpenExtensionSearch.openExtensionSearch(searchValue)
 
-  expect(setExtensionsSearchValueMock).toHaveBeenCalledWith(searchValue)
-  expect(openViewletMock).toHaveBeenCalled()
+  expect(mockInvoke).toHaveBeenCalledWith('setExtensionsSearchValue', searchValue)
+  expect(mockInvoke).toHaveBeenCalledWith('SideBar.openViewlet')
 })
