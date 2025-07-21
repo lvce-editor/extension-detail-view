24f395e86c42b15741378941816e34d0057880cb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-21 12:45:19 +0200

    fix: order of context menu parameters (#712)

--- packages/extension-detail-view-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/extension-detail-view-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -1,5 +1,5 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const show = async (menuId: any, x: number, y: number): Promise<void> => {
-  await RendererWorker.showContextMenu(menuId, x, y)
+  await RendererWorker.showContextMenu(x, y, menuId)
 }
