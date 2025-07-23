e536f80f93872a882b439d40b82e097369fd7ace
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-23 13:03:22 +0200

    feature: add category test (#763)

--- packages/extension-detail-view-worker/test/HandleClickCategory.test.ts
+++ packages/extension-detail-view-worker/test/HandleClickCategory.test.ts
@@ -1,9 +1,29 @@
 import { test, expect } from '@jest/globals'
-import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
-import { handleClickCategory } from '../src/parts/HandleClickCategory/HandleClickCategory.ts'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as createDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as HandleClickCategory from '../src/parts/HandleClickCategory/HandleClickCategory.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 
-test.skip('handleClickCategory returns the same state', async () => {
-  const state = createDefaultState()
-  const result = await handleClickCategory(state, 'category-1')
+test('handleClickCategory should call openExtensionSearch with category search', async () => {
+  const state = createDefaultState.createDefaultState()
+  const categoryId = 'programming-languages'
+
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: () => undefined,
+  })
+  RendererWorker.set(mockRpc)
+
+  const result = await HandleClickCategory.handleClickCategory(state, categoryId)
+
+  expect(result).toBe(state)
+})
+
+test('handleClickCategory should return state unchanged when categoryId is empty', async () => {
+  const state = createDefaultState.createDefaultState()
+  const categoryId = ''
+
+  const result = await HandleClickCategory.handleClickCategory(state, categoryId)
+
   expect(result).toBe(state)
 })
