ef0ba3c94cb83a5d61ae766720bc8784667c149b
Le Vivilet <levivlet@pm.me>
2025-07-23 22:24:06 +0200

    feature: improve runtime status rendering

--- packages/extension-detail-view-worker/src/parts/FeatureRuntimeStatusVirtualDom/FeatureRuntimeStatusVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/FeatureRuntimeStatusVirtualDom/FeatureRuntimeStatusVirtualDom.ts
@@ -1,7 +1,25 @@
-import { text } from '@lvce-editor/virtual-dom-worker'
+import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
+import * as GetFeatureContentHeadingVirtualDom from '../GetFeatureContentHeadingVirtualDom/GetFeatureContentHeadingVirtualDom.ts'
 
 export const getRuntimeStatusVirtualDom = (state: ExtensionDetailState): readonly VirtualDomNode[] => {
-  return [text('runtime status')]
+  const { status } = state
+  const heading = ExtensionDetailStrings.runtimeStatus()
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.FeatureContent,
+      childCount: 2,
+    },
+    ...GetFeatureContentHeadingVirtualDom.getFeatureContentHeadingVirtualDom(heading),
+    {
+      type: VirtualDomElements.P,
+      childCount: 2,
+    },
+    text(`Status: `),
+    text(`${status}`),
+  ]
 }
