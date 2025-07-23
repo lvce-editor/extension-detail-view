6d25c15e0ba21164b5d3dd865f6a0537f2b6a86d
Le Vivilet <levivlet@pm.me>
2025-07-23 12:42:11 +0200

    feature: add renderer test

--- packages/extension-detail-view-worker/test/GetRenderer.test.ts
+++ packages/extension-detail-view-worker/test/GetRenderer.test.ts
@@ -3,6 +3,7 @@ import * as DiffType from '../src/parts/DiffType/DiffType.ts'
 import * as GetRenderer from '../src/parts/GetRenderer/GetRenderer.ts'
 import * as RenderDom from '../src/parts/RenderDom/RenderDom.ts'
 import * as RenderFocus from '../src/parts/RenderFocus/RenderFocus.ts'
+import * as RenderScrollTop from '../src/parts/RenderScrollTop/RenderScrollTop.ts'
 
 test('getRenderer - RenderItems', () => {
   const renderer = GetRenderer.getRenderer(DiffType.RenderItems)
@@ -12,6 +13,11 @@ test('getRenderer - RenderItems', () => {
 test('getRenderer - RenderFocus', () => {
   const renderer = GetRenderer.getRenderer(DiffType.RenderFocus)
   expect(renderer).toBe(RenderFocus.renderFocus)
+})
+
+test('getRenderer - RenderScrollTop', () => {
+  const renderer = GetRenderer.getRenderer(DiffType.RenderScrollTop)
+  expect(renderer).toBe(RenderScrollTop.renderScrollTop)
 })
 
 test('getRenderer - unknown type throws', () => {
