7b461b562d2618cf1b880d39c50d6c03116535fc
Le Vivilet <levivlet@pm.me>
2025-07-23 22:38:05 +0200

    feature: make runtime status a number and display activation time

--- packages/extension-detail-view-worker/src/parts/FeatureRuntimeStatusDetails/FeatureRuntimeStatusDetails.ts
+++ packages/extension-detail-view-worker/src/parts/FeatureRuntimeStatusDetails/FeatureRuntimeStatusDetails.ts
@@ -2,9 +2,10 @@ import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDeta
 import { getRuntimeStatus } from '../GetRuntimeStatus/GetRuntimeStatus.ts'
 
 export const getRuntimeStatusDetails = async (extension: any): Promise<Partial<ExtensionDetailState>> => {
-  const { activationEvent, status } = await getRuntimeStatus(extension.id)
+  const { activationEvent, status, activationTime } = await getRuntimeStatus(extension.id)
   return {
     wasActivatedByEvent: activationEvent,
+    activationTime,
     status,
   }
 }
