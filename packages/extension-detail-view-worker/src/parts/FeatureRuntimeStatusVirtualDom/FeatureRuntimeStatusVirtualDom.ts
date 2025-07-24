aa790919dab3dbed39868c64dc4cb58507bc2439
Le Vivilet <levivlet@pm.me>
2025-07-24 13:25:58 +0200

    feature: move runtime status into separate file

--- packages/extension-detail-view-worker/src/parts/FeatureRuntimeStatusVirtualDom/FeatureRuntimeStatusVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/FeatureRuntimeStatusVirtualDom/FeatureRuntimeStatusVirtualDom.ts
@@ -1,41 +1,11 @@
-import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
+import { getActivationTimeVirtualDom } from '../GetActivationTimeVirtualDom/GetActivationTimeVirtualDom.ts'
 import * as GetFeatureContentHeadingVirtualDom from '../GetFeatureContentHeadingVirtualDom/GetFeatureContentHeadingVirtualDom.ts'
-import { getStatusMessage } from '../GetStatusMessage/GetStatusMessage.ts'
-
-const formatTime = (time: number): string => {
-  return time.toFixed(2) + 'ms'
-}
-
-const getActivationTimeVirtualDom = (activationTime: number): readonly VirtualDomNode[] => {
-  if (!activationTime) {
-    return []
-  }
-  const formattedTime = formatTime(activationTime)
-  return [
-    {
-      type: VirtualDomElements.P,
-      childCount: 2,
-    },
-    text('Activation Time: '), // i18n
-    text(formattedTime),
-  ]
-}
-
-const getStatusVirtualDom = (status: number): readonly VirtualDomNode[] => {
-  const statusString = getStatusMessage(status)
-  return [
-    {
-      type: VirtualDomElements.P,
-      childCount: 2,
-    },
-    text(`Status: `), // i18n
-    text(`${statusString}`),
-  ]
-}
+import { getStatusVirtualDom } from '../GetStatusVirtualDom/GetStatusVirtualDom.ts'
 
 const getChildCount = (status: number, activationTime: number): number => {
   let childCount = 1 // heading
