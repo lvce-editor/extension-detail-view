d81e77a11fcb7917ff41e72be8475ac810e1159c
Le Vivilet <levivlet@pm.me>
2025-07-24 21:57:45 +0200

    feature: add todo

--- packages/extension-detail-view-worker/src/parts/HandleClickDisable/HandleClickDisable.ts
+++ packages/extension-detail-view-worker/src/parts/HandleClickDisable/HandleClickDisable.ts
@@ -3,5 +3,6 @@ import { disableExtension } from '../DisableExtension/DisableExtension.ts'
 
 export const handleClickDisable = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
   await disableExtension(state.extensionId)
+  // TODO when it fails, show dialog / alert?
   return state
 }
