35057f0f8ccee81307e22e8eab63d776a0b22d8b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-24 14:28:33 +0200

    fix: don't restore state when in test (#802)
    * fix: don't restore state when in test
    
    * test

--- packages/extension-detail-view-worker/src/parts/LoadContent2/LoadContent2.ts
+++ packages/extension-detail-view-worker/src/parts/LoadContent2/LoadContent2.ts
@@ -1,6 +1,6 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 
-export const loadContent2 = async (state: ExtensionDetailState, savedState: unknown): Promise<ExtensionDetailState> => {
-  return LoadContent.loadContent(state, state.platform, savedState)
+export const loadContent2 = async (state: ExtensionDetailState, savedState: unknown, isTest: boolean = false): Promise<ExtensionDetailState> => {
+  return LoadContent.loadContent(state, state.platform, savedState, isTest)
 }
