7b8fc347f1d6a2f697578ce69cf551189538f124
Le Vivilet <levivlet@pm.me>
2025-07-24 10:21:17 +0200

    feature: add extension status update function

---
+++ packages/extension-detail-view-worker/src/parts/HandleExtensionsStatusUpdate/HandleExtensionsStatusUpdate.ts
@@ -0,0 +1,6 @@
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+
+export const handleExtensionsStatusUpdate = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
+  // TODO query extensions status again
+  return state
+}
