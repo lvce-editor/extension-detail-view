130b0c49587d22b14f1112959d94736e626b6487
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-24 14:20:46 +0200

    feature: add runtime status e2e test (#800)

---
+++ packages/e2e/src/extension-detail.feature-runtime-status.ts
@@ -0,0 +1,34 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'extension-detail.feature-runtime-status'
+
+export const skip = 1
+
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-commands')
+  await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.commands-test')
+  const tabFeatures = Locator('.ExtensionDetailTab[name="Features"]')
+  const featureCommands = Locator('.Feature[name="Commands"]')
+  await tabFeatures.click() // TODO use page object model
+  await ExtensionDetail.selectFeature('')
+
+  // act
+  await featureCommands.click()
+
+  // assert
+  const heading = Locator('.FeatureContent h1')
+  await expect(heading).toBeVisible()
+  await expect(heading).toHaveText('Commands')
+  const commandsTable = Locator('.FeatureContent .Table')
+  await expect(commandsTable).toBeVisible()
+  const heading1 = commandsTable.locator('th').nth(0)
+  await expect(heading1).toHaveText('ID')
+  const heading2 = commandsTable.locator('th').nth(1)
+  await expect(heading2).toHaveText('Label')
+  const cell1 = commandsTable.locator('tbody td').nth(0)
+  await expect(cell1).toHaveText('test')
+  const cell2 = commandsTable.locator('tbody td').nth(1)
+  await expect(cell2).toHaveText('Test')
+}
