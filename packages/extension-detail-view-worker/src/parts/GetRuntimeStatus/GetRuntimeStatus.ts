2e7ecf0093dcff97b134f9d30ece8cfa069f8a59
Le Vivilet <levivlet@pm.me>
2025-07-24 23:08:18 +0200

    feature: simplify runtime status

--- packages/extension-detail-view-worker/src/parts/GetRuntimeStatus/GetRuntimeStatus.ts
+++ packages/extension-detail-view-worker/src/parts/GetRuntimeStatus/GetRuntimeStatus.ts
@@ -2,9 +2,8 @@ import type { RuntimeStatus } from '../RuntimeStatus/RuntimeStatus.ts'
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 
 export const getRuntimeStatus = async (extensionId: string): Promise<RuntimeStatus> => {
-  // TODO simplify api
   // @ts-ignore
-  const status = await ExtensionHostWorker.invoke('ExtensionHost.getRuntimeStatus', extensionId)
+  const status = await ExtensionHostWorker.getRuntimeStatus(extensionId)
   // @ts-ignore
   return status
 }
