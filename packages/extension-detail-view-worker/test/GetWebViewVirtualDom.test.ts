91bd4b50ca3064213205e90acac2599359a99860
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-29 10:21:10 +0200

    Discard changes to packages/extension-detail-view-worker/test/GetWebViewVirtualDom.test.ts

--- packages/extension-detail-view-worker/test/GetWebViewVirtualDom.test.ts
+++ packages/extension-detail-view-worker/test/GetWebViewVirtualDom.test.ts
@@ -18,47 +18,79 @@ test('webview virtual dom', () => {
       2,
     ),
   }
-
-  const result = GetWebViewVirtualDom.getWebViewVirtualDom(webView)
-
-  // Test the structure without checking every token
-  expect(result[0]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.FeatureWebView,
-    childCount: 5,
-  })
-
-  // Test that JSON strings are tokenized (not plain text)
-  // First pre is for ID (plain text), second is for Selector (JSON), third is for CSP (JSON), fourth is for Elements (JSON)
-  const selectorPreIndex = 9 // Second pre element
-  const cspPreIndex = 23 // Third pre element
-  const elementsPreIndex = 37 // Fourth pre element
-
-  expect(result[selectorPreIndex]).toEqual({
-    type: VirtualDomElements.Pre,
-    className: ClassNames.DefinitionListItemValue,
-    childCount: 10, // Should have multiple tokens
-  })
-
-  expect(result[cspPreIndex]).toEqual({
-    type: VirtualDomElements.Pre,
-    className: ClassNames.DefinitionListItemValue,
-    childCount: 10, // Should have multiple tokens
-  })
-
-  expect(result[elementsPreIndex]).toEqual({
-    type: VirtualDomElements.Pre,
-    className: ClassNames.DefinitionListItemValue,
-    childCount: 26, // Should have multiple tokens for complex JSON
-  })
-
-  // Test that tokens are present
-  const hasTokenSpans = result.some(
-    (node) =>
-      node.type === VirtualDomElements.Span &&
-      (node.className === ClassNames.TokenJsonString ||
-        node.className === ClassNames.TokenJsonPunctuation ||
-        node.className === ClassNames.TokenJsonNumber),
-  )
-  expect(hasTokenSpans).toBe(true)
+  expect(GetWebViewVirtualDom.getWebViewVirtualDom(webView)).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.FeatureWebView,
+      childCount: 5,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.DefinitionListItem,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.H2,
+      className: ClassNames.DefinitionListItemHeading,
+      childCount: 1,
+    },
+    text('ID'),
+    {
+      type: VirtualDomElements.Pre,
+      className: ClassNames.DefinitionListItemValue,
+      childCount: 1,
+    },
+    text('test-webview'),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.DefinitionListItem,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.H2,
+      className: ClassNames.DefinitionListItemHeading,
+      childCount: 1,
+    },
+    text('Selector'),
+    {
+      type: VirtualDomElements.Pre,
+      className: ClassNames.DefinitionListItemValue,
+      childCount: 1,
+    },
+    text('{"viewType":"test.view"}'),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.DefinitionListItem,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.H2,
+      className: ClassNames.DefinitionListItemHeading,
+      childCount: 1,
+    },
+    text('ContentSecurityPolicy'),
+    {
+      type: VirtualDomElements.Pre,
+      className: ClassNames.DefinitionListItemValue,
+      childCount: 1,
+    },
+    text('{"default-src":"\'none\'"}'),
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.DefinitionListItem,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.H2,
+      className: ClassNames.DefinitionListItemHeading,
+      childCount: 1,
+    },
+    text('Elements'),
+    {
+      type: VirtualDomElements.Pre,
+      className: ClassNames.DefinitionListItemValue,
+      childCount: 1,
+    },
+    text('{\n  "button": {\n    "tag": "button",\n    "text": "Click me"\n  }\n}'),
+  ])
 })
