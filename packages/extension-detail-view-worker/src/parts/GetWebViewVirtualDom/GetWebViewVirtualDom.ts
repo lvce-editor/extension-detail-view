4e9daae90487996ef9f8eb756bac61543705da66
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-29 10:20:20 +0200

    Discard changes to packages/extension-detail-view-worker/src/parts/GetWebViewVirtualDom/GetWebViewVirtualDom.ts

--- packages/extension-detail-view-worker/src/parts/GetWebViewVirtualDom/GetWebViewVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetWebViewVirtualDom/GetWebViewVirtualDom.ts
@@ -3,7 +3,6 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import type { WebView } from '../WebView/WebView.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
-import * as GetJsonSyntaxHighlightedVirtualDom from '../GetJsonSyntaxHighlightedVirtualDom/GetJsonSyntaxHighlightedVirtualDom.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 const heading: VirtualDomNode = {
@@ -12,11 +11,11 @@ const heading: VirtualDomNode = {
   childCount: 1,
 }
 
-const createPre = (tokenCount: number): VirtualDomNode => ({
+const pre: VirtualDomNode = {
   type: VirtualDomElements.Pre,
   className: ClassNames.DefinitionListItemValue,
-  childCount: tokenCount,
-})
+  childCount: 1,
+}
 
 const item: VirtualDomNode = {
   type: VirtualDomElements.Div,
@@ -30,12 +29,6 @@ export const getWebViewVirtualDom = (webView: WebView): readonly VirtualDomNode[
   const textSelector = ExtensionDetailStrings.selector()
   const textContentSecurityPolicy = ExtensionDetailStrings.contentSecurityPolicy()
   const textElements = ExtensionDetailStrings.elements()
-
-  // Use syntax highlighting for JSON strings
-  const selectorTokens = GetJsonSyntaxHighlightedVirtualDom.getJsonSyntaxHighlightedVirtualDom(selectorString)
-  const cspTokens = GetJsonSyntaxHighlightedVirtualDom.getJsonSyntaxHighlightedVirtualDom(contentSecurityPolicyString)
-  const elementsTokens = GetJsonSyntaxHighlightedVirtualDom.getJsonSyntaxHighlightedVirtualDom(elementsString)
-
   return [
     {
       type: VirtualDomElements.Div,
@@ -45,22 +38,22 @@ export const getWebViewVirtualDom = (webView: WebView): readonly VirtualDomNode[
     item,
     heading,
     text(textId),
-    createPre(1),
+    pre,
     text(id),
     item,
     heading,
     text(textSelector),
-    createPre(selectorTokens.length),
-    ...selectorTokens,
+    pre,
+    text(selectorString),
     item,
     heading,
     text(textContentSecurityPolicy),
-    createPre(cspTokens.length),
-    ...cspTokens,
+    pre,
+    text(contentSecurityPolicyString),
     item,
     heading,
     text(textElements),
-    createPre(elementsTokens.length),
-    ...elementsTokens,
+    pre,
+    text(elementsString),
   ]
 }
