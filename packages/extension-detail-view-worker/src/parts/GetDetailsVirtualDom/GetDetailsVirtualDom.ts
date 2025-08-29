9060eb4cd5acaab29fa788bef2465d8bf720293d
Le Vivilet <levivlet@pm.me>
2025-08-29 10:43:30 +0200

    feature: render side bar depending on showSideBar property

--- packages/extension-detail-view-worker/src/parts/GetDetailsVirtualDom/GetDetailsVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetDetailsVirtualDom/GetDetailsVirtualDom.ts
@@ -25,12 +25,13 @@ export const getDetailsVirtualDom = (
   installationEntries: readonly MoreInfoEntry[],
   marketplaceEntries: readonly MoreInfoEntry[],
   hasReadme: boolean,
+  showSideBar: boolean,
 ): readonly VirtualDomNode[] => {
   const firstHeading = ExtensionDetailStrings.installation()
   const secondHeading = ExtensionDetailStrings.marketplace()
   const thirdHeading = ExtensionDetailStrings.categories()
   const fourthHeading = ExtensionDetailStrings.resources()
-  const showAdditionalDetails = width > showAdditionalDetailsBreakpoint
+  const showAdditionalDetails = showSideBar
   const childCount = getChildCount(showAdditionalDetails, scrollToTopButtonEnabled)
   const contentDom = hasReadme
     ? sanitizedReadmeHtml
