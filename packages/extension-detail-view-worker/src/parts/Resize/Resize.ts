3aaccef3bb37d77179ca9469e968e3a5377accc7
Le Vivilet <levivlet@pm.me>
2025-08-29 11:06:55 +0200

    feature: update sideBarWidth on resize

--- packages/extension-detail-view-worker/src/parts/Resize/Resize.ts
+++ packages/extension-detail-view-worker/src/parts/Resize/Resize.ts
@@ -1,16 +1,18 @@
 import type { Dimensions } from '../Dimensions/Dimensions.ts'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
-import { getPadding } from '../GetPadding/GetPadding.ts'
+import { getPadding, getSideBarWidth } from '../GetPadding/GetPadding.ts'
 
 export const resize = (state: ExtensionDetailState, dimensions: Dimensions): ExtensionDetailState => {
   const { showAdditionalDetailsBreakpoint } = state
   const showSideBar = dimensions.width > showAdditionalDetailsBreakpoint
   const padding = getPadding(dimensions.width)
+  const sideBarWidth = getSideBarWidth(dimensions.width)
   return {
     ...state,
     ...dimensions,
     showSideBar,
     paddingLeft: padding,
     paddingRight: padding,
+    sideBarWidth,
   }
 }
