54191217eeb251529805aaea587ea88ab9fea4d1
Le Vivilet <levivlet@pm.me>
2025-08-29 10:41:32 +0200

    feature: update showSideBar on resize

--- packages/extension-detail-view-worker/src/parts/Resize/Resize.ts
+++ packages/extension-detail-view-worker/src/parts/Resize/Resize.ts
@@ -2,8 +2,11 @@ import type { Dimensions } from '../Dimensions/Dimensions.ts'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 
 export const resize = (state: ExtensionDetailState, dimensions: Dimensions): ExtensionDetailState => {
+  const { showAdditionalDetailsBreakpoint } = state
+  const showSideBar = dimensions.width > showAdditionalDetailsBreakpoint
   return {
     ...state,
     ...dimensions,
+    showSideBar,
   }
 }
