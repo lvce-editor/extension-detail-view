9976ecda68ffcdcd1834f94ed4f65782d8ad1e68
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-30 19:47:25 +0200

    feature: move getColorThemeId into separate function (#898)
    * feature: move getColorThemeId into separate function
    
    * lint
    
    * lint
    
    * lint

--- packages/extension-detail-view-worker/src/parts/HandleClickSetColorTheme/HandleClickSetColorTheme.ts
+++ packages/extension-detail-view-worker/src/parts/HandleClickSetColorTheme/HandleClickSetColorTheme.ts
@@ -1,13 +1,6 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import { getColorThemeId } from '../GetColorThemeId/GetColorThemeId.ts'
 import * as SetColorTheme from '../SetColorTheme/SetColorTheme.ts'
-
-export const getColorThemeId = (extension: any): string | undefined => {
-  if (extension && Array.isArray(extension.colorThemes) && extension.colorThemes.length > 0) {
-    const colorTheme = extension.colorThemes[0]
-    return colorTheme.id || colorTheme.label
-  }
-  return undefined
-}
 
 export const handleClickSetColorTheme = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
   const { extension } = state
@@ -17,3 +10,5 @@ export const handleClickSetColorTheme = async (state: ExtensionDetailState): Pro
   }
   return state
 }
+
+export { getColorThemeId } from '../GetColorThemeId/GetColorThemeId.ts'
