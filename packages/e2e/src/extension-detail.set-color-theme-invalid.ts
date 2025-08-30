9d716e04180574d11c34e342692199a73e8ab495
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-30 19:40:24 +0200

    feature: add invalid theme e2e test (#900)

---
+++ packages/e2e/src/extension-detail.set-color-theme-invalid.ts
@@ -0,0 +1,22 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'extension-detail.set-color-theme-invalid'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-detail-theme-invalid')
+  await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.theme-test')
+  await ExtensionDetail.selectFeatures()
+  const setColorThemeButton = Locator('.Button[name="SetColorTheme"]')
+  await expect(setColorThemeButton).toBeVisible()
+
+  // act
+  await Command.execute('ExtensionDetail.handleClickSetColorTheme')
+
+  // assert
+  const activityBar = Locator('.ActivityBar')
+  await expect(activityBar).toHaveCSS('background-color', 'rgb(255, 165, 0)')
+}
