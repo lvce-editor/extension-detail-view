58b9475ca51bdd3e5f2b9a913b4284e03d50f21a
Le Vivilet <levivilet@pm.me>
2025-10-28 23:13:52 +0100

    feature: improve markdown cache (#928)

--- packages/extension-detail-view-worker/src/parts/MarkDownCache/MarkDownCache.ts
+++ packages/extension-detail-view-worker/src/parts/MarkDownCache/MarkDownCache.ts
@@ -1,11 +1,24 @@
 export const has = async (key: string): Promise<boolean> => {
-  return false
+  const cache = await caches.open('lvce-editor/extension-detail-markdown-cache')
+  const response = await cache.match(key)
+  return Boolean(response)
 }
 
-export const get = async (key: string): Promise<any> => {
-  return {}
+export const get = async (key: string): Promise<string> => {
+  const cache = await caches.open('lvce-editor/extension-detail-markdown-cache')
+  const response = await cache.match(key)
+  const text = await response?.text()
+  return text || ''
 }
 
-export const set = async (key: string, value: any): Promise<void> => {
-  // TODO
+export const set = async (key: string, value: string): Promise<void> => {
+  const cache = await caches.open('lvce-editor/extension-detail-markdown-cache')
+  await cache.put(
+    key,
+    new Response(value, {
+      headers: {
+        'Content-Type': 'application/markdown',
+      },
+    }),
+  )
 }
