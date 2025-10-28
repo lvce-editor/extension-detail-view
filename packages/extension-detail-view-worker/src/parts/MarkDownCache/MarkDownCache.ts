88ac7d43b6a94adcd12c6f14f65cebe9e41ff049
Le Vivilet <levivilet@pm.me>
2025-10-28 23:35:24 +0100

    feature: cache the cache (#932)

--- packages/extension-detail-view-worker/src/parts/MarkDownCache/MarkDownCache.ts
+++ packages/extension-detail-view-worker/src/parts/MarkDownCache/MarkDownCache.ts
@@ -1,21 +1,23 @@
+import { getCache } from '../GetCache/GetCache.ts'
+
 // TODO pass application name from renderer worker to not hardcode it
 const cacheName = 'lvce-editor/markdown-cache'
 
 export const has = async (key: string): Promise<boolean> => {
-  const cache = await caches.open(cacheName)
+  const cache = await getCache(cacheName)
   const response = await cache.match(key)
   return Boolean(response)
 }
 
 export const get = async (key: string): Promise<string> => {
-  const cache = await caches.open(cacheName)
+  const cache = await getCache(cacheName)
   const response = await cache.match(key)
   const text = await response?.text()
   return text || ''
 }
 
 export const set = async (key: string, value: string): Promise<void> => {
-  const cache = await caches.open(cacheName)
+  const cache = await getCache(cacheName)
   await cache.put(
     key,
     new Response(value, {
