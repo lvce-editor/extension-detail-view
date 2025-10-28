a4c7710b10de163469b2f6e0c26cc247d9e312c2
Le Vivilet <levivilet@pm.me>
2025-10-28 23:05:59 +0100

    feature: add markdown cache (#926)

---
+++ packages/extension-detail-view-worker/src/parts/MarkDownCache/MarkDownCache.ts
@@ -0,0 +1,11 @@
+export const has = async (key: string): Promise<boolean> => {
+  return false
+}
+
+export const get = async (key: string): Promise<any> => {
+  return {}
+}
+
+export const set = async (key: string, value: any): Promise<void> => {
+  // TODO
+}
