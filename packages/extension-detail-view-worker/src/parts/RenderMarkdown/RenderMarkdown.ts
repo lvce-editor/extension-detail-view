222921de1d8ddfc8b817d1c8052685deec5b34db
Le Vivilet <levivilet@pm.me>
2025-10-29 00:56:49 +0100

    feature: enable markdown caching by default (#938)

--- packages/extension-detail-view-worker/src/parts/RenderMarkdown/RenderMarkdown.ts
+++ packages/extension-detail-view-worker/src/parts/RenderMarkdown/RenderMarkdown.ts
@@ -1,6 +1,6 @@
-import * as MarkdownWorker from '../MarkdownWorker/MarkdownWorker.ts'
+import { renderMarkdownCached } from '../RenderMarkdownCached/RenderMarkdownCached.ts'
 
 export const renderMarkdown = async (markdown: string, options: { readonly baseUrl?: string } = {}): Promise<string> => {
-  const html = await MarkdownWorker.render(markdown, options)
+  const html = await renderMarkdownCached(markdown, options)
   return html
 }
