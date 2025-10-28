b7b9422ea6cdb676a348d803c498e0cf6fd9ed96
Le Vivilet <levivilet@pm.me>
2025-10-28 23:40:46 +0100

    feature: handle storage bucket not available

--- packages/extension-detail-view-worker/src/parts/GetCache/GetCache.ts
+++ packages/extension-detail-view-worker/src/parts/GetCache/GetCache.ts
@@ -1,9 +1,29 @@
 // TODO pass application name from renderer worker to not hardcode it
 const bucketName = 'markdown-cache'
 
-const cachedCaches: Record<string, Promise<Cache>> = Object.create(null)
+export interface ICache {
+  readonly put: (request: RequestInfo | URL, response: Response) => Promise<void>
+  readonly match: (request: RequestInfo | URL, options?: CacheQueryOptions) => Promise<Response | undefined>
+}
 
-const getCacheInternal = async (cacheName: string): Promise<Cache> => {
+const cachedCaches: Record<string, Promise<ICache>> = Object.create(null)
+
+const noopCache: ICache = {
+  async match() {
+    return undefined
+  },
+  async put() {},
+}
+
+const supportsStorageBuckets = (): boolean => {
+  // @ts-ignore
+  return Boolean(navigator.storageBuckets)
+}
+
+const getCacheInternal = async (cacheName: string): Promise<ICache> => {
+  if (!supportsStorageBuckets()) {
+    return noopCache
+  }
   const twoWeeks = 14 * 24 * 60 * 60 * 1000
   // @ts-ignore
   const bucket = await navigator.storageBuckets.open(bucketName, {
@@ -14,7 +34,7 @@ const getCacheInternal = async (cacheName: string): Promise<Cache> => {
   return cache
 }
 
-export const getCache = (cacheName: string): Promise<Cache> => {
+export const getCache = (cacheName: string): Promise<ICache> => {
   if (!(cacheName in cachedCaches)) {
     cachedCaches[cacheName] = getCacheInternal(cacheName)
   }
