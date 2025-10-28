36aabcace778cf1c0ca57dad510cc296d424aa6f
Le Vivilet <levivilet@pm.me>
2025-10-29 00:54:04 +0100

    feature: add readme link test (#937)

---
+++ packages/e2e/src/extension-detail.readme-link.ts
@@ -0,0 +1,17 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-readme-link')
+  await Extension.addWebExtension(extensionUri)
+
+  // act
+  await ExtensionDetail.open('test.extension-basics')
+
+  // assert
+  const detailView = Locator('.ExtensionDetail')
+  await expect(detailView).toBeVisible()
+  const markDown = Locator('.Markdown')
+  await expect(markDown).toBeVisible()
+  await expect(markDown).toHaveText('test readme\nexample link\n')
+}
