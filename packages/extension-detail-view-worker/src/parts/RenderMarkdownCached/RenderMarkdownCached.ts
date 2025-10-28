5bc806d33bcea08b5991834330550fa465dfb771
Le Vivilet <levivilet@pm.me>
2025-10-28 23:34:20 +0100

    feature: add cached markdown renderer

---
+++ packages/extension-detail-view-worker/src/parts/RenderMarkdownCached/RenderMarkdownCached.ts
@@ -0,0 +1,16 @@
+import { hash } from '../Hash/Hash.ts'
+import * as MarkdownWorker from '../MarkdownWorker/MarkdownWorker.ts'
+import * as MarkDownCache from '../MarkDownCache/MarkDownCache.ts'
+
+export const renderMarkdownCached = async (markdown: string, options: { readonly baseUrl?: string } = {}): Promise<string> => {
+  const markdownHash = await hash(markdown) // TODO hash options also
+  const cacheKey = `/markdown/${markdownHash}`
+  const hasItem = await MarkDownCache.has(cacheKey)
+  if (hasItem) {
+    const value = await MarkDownCache.get(cacheKey)
+    return value // TODO validate if it's valid
+  }
+  const html = await MarkdownWorker.render(markdown, options)
+  await MarkDownCache.set(cacheKey, html)
+  return html
+}
