9f5e6f48071f17f2838722c90712ff70050514be
Le Vivilet <levivilet@pm.me>
2025-10-28 23:22:56 +0100

    feature: improve cache (#929)

--- packages/extension-detail-view-worker/src/parts/MarkDownCache/MarkDownCache.ts
+++ packages/extension-detail-view-worker/src/parts/MarkDownCache/MarkDownCache.ts
@@ -1,23 +1,27 @@
+// TODO pass application name from renderer worker to not hardcode it
+const cacheName = 'lvce-editor/markdown-cache'
+
 export const has = async (key: string): Promise<boolean> => {
-  const cache = await caches.open('lvce-editor/extension-detail-markdown-cache')
+  const cache = await caches.open(cacheName)
   const response = await cache.match(key)
   return Boolean(response)
 }
 
 export const get = async (key: string): Promise<string> => {
-  const cache = await caches.open('lvce-editor/extension-detail-markdown-cache')
+  const cache = await caches.open(cacheName)
   const response = await cache.match(key)
   const text = await response?.text()
   return text || ''
 }
 
 export const set = async (key: string, value: string): Promise<void> => {
-  const cache = await caches.open('lvce-editor/extension-detail-markdown-cache')
+  const cache = await caches.open(cacheName)
   await cache.put(
     key,
     new Response(value, {
       headers: {
         'Content-Type': 'application/markdown',
+        'Content-Length': `${value.length}`,
       },
     }),
   )
