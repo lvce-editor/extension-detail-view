45070aae94b1055b09e10a20c851aa0a04a078c7
Le Vivilet <levivilet@pm.me>
2025-10-29 09:41:20 +0100

    feature: add interface

--- packages/extension-detail-view-worker/src/parts/RenderMarkdownCached/RenderMarkdownCached.ts
+++ packages/extension-detail-view-worker/src/parts/RenderMarkdownCached/RenderMarkdownCached.ts
@@ -1,8 +1,9 @@
+import type { MarkdownOptions } from '../MarkdownOptions/MarkdownOptions.ts'
 import { hash } from '../Hash/Hash.ts'
 import * as MarkDownCache from '../MarkDownCache/MarkDownCache.ts'
 import * as MarkdownWorker from '../MarkdownWorker/MarkdownWorker.ts'
 
-export const renderMarkdownCached = async (markdown: string, options: { readonly baseUrl?: string } = {}): Promise<string> => {
+export const renderMarkdownCached = async (markdown: string, options: MarkdownOptions = {}): Promise<string> => {
   const markdownHash = await hash(markdown) // TODO hash options also
   const cacheKey = `/markdown/${markdownHash}`
   const hasItem = await MarkDownCache.has(cacheKey)
