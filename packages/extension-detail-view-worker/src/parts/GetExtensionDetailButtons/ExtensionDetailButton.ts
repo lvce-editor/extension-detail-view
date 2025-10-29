18661c1c8da49ad962707278e32d82642261dc11
Le Vivilet <levivilet@pm.me>
2025-10-29 14:35:08 +0100

    tsc

--- packages/extension-detail-view-worker/src/parts/GetExtensionDetailButtons/ExtensionDetailButton.ts
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailButtons/ExtensionDetailButton.ts
@@ -1,6 +1,6 @@
 export interface ExtensionDetailButton {
   readonly label: string
-  readonly onClick: string
+  readonly onClick: string | number
   readonly enabled: boolean
   readonly name: string
 }
