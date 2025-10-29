97345287036e175a9e9874f708a1499db26c5aa0
Le Vivilet <levivilet@pm.me>
2025-10-29 14:17:13 +0100

    feature: when title is undefined, don't set title

--- packages/extension-detail-view-worker/src/parts/GetMoreInfoEntryValueVirtualDom/GetMoreInfoEntryValueVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetMoreInfoEntryValueVirtualDom/GetMoreInfoEntryValueVirtualDom.ts
@@ -4,17 +4,29 @@ import * as GetMoreInfoEntryValueClassName from '../GetMoreInfoEntryValueClassNa
 import * as GetMoreInfoEntryValueTag from '../GetMoreInfoEntryValueTag/GetMoreInfoEntryValueTag.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
+interface ExtraProps {
+  readonly title?: string
+}
+
+const getExtraProps = (title: string | undefined): ExtraProps => {
+  if (title) {
+    return { title }
+  }
+  return {}
+}
+
 export const getMoreInfoEntryValueVirtualDom = (item: MoreInfoEntry): readonly VirtualDomNode[] => {
   const { value, onClick, code, title } = item
   const type = GetMoreInfoEntryValueTag.getMoreInfoEntryValueTag(onClick, code)
   const className = GetMoreInfoEntryValueClassName.getMoreInfoEntryValueClassName(onClick, code)
+  const extraProps = getExtraProps(title)
   return [
     {
       type,
       className,
       childCount: 1,
       onClick,
-      title,
+      ...extraProps,
     },
     text(value),
   ]
