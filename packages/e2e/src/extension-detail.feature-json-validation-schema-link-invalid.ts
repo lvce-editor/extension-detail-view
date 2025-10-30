aba048caf8d6f9ed3c7412809d794d2beb2c2a34
Le Vivilet <levivilet@pm.me>
2025-10-30 17:32:10 +0100

    feature: add more link tests

---
+++ packages/e2e/src/extension-detail.feature-json-validation-schema-link-invalid.ts
@@ -0,0 +1,27 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'extension-detail.feature-json-validation-link-invalid'
+
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-json-validation-schema-link-invalid')
+  await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.json-validation-schema-link-invalid')
+  await ExtensionDetail.selectFeatures()
+
+  // act
+  await ExtensionDetail.openFeature('JsonValidation')
+
+  // assert
+  const heading = Locator('.FeatureContent h1')
+  await expect(heading).toBeVisible()
+  await expect(heading).toHaveText('Json Validation')
+  const commandsTable = Locator('.FeatureContent .Table')
+  await expect(commandsTable).toBeVisible()
+  const cell2 = commandsTable.locator('tbody td').nth(1)
+  await expect(cell2).toHaveText('...')
+  const link = cell2.locator('a')
+  await expect(link).toBeVisible()
+  const href = new URL('...', extensionUri).toString() // TODO should not display as link
+  await expect(link).toHaveAttribute('href', href)
+}
