9f0ce2c985147b40fecc65ce068aeb063b64bb48
Le Vivilet <levivilet@pm.me>
2025-10-30 18:20:55 +0100

    feature: add javascript readme link e2e test

---
+++ packages/e2e/src/extension-detail.readme-link-javascript.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-readme-link-javascript')
+  await Extension.addWebExtension(extensionUri)
+
+  // act
+  await ExtensionDetail.open('test.extension-readme-link-javascript')
+
+  // assert
+  const detailView = Locator('.ExtensionDetail')
+  await expect(detailView).toBeVisible()
+  const markDown = Locator('.Markdown')
+  await expect(markDown).toBeVisible()
+  await expect(markDown).toHaveText('test readme\ntest\n')
+  const link = markDown.locator('a')
+  await expect(link).toHaveAttribute('href', 'javascript:alert(1)') // TODO should be sanitized
+}
