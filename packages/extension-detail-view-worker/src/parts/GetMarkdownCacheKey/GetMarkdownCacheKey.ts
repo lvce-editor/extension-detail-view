8f598cf349e2f7dd0bd409975df724675b7a90c4
Le Vivilet <levivilet@pm.me>
2025-10-30 12:01:57 +0100

    feature: update markdown cache key

--- packages/extension-detail-view-worker/src/parts/GetMarkdownCacheKey/GetMarkdownCacheKey.ts
+++ packages/extension-detail-view-worker/src/parts/GetMarkdownCacheKey/GetMarkdownCacheKey.ts
@@ -1,11 +1,9 @@
-const supportsNormalCacheKey = (locationProtocol: string): boolean => {
-  return locationProtocol === 'http:' || locationProtocol === 'https:'
-}
+import { supportsNormalCacheKey } from '../SupportsNormalCacheKey/SupportsNormalCacheKey.ts'
 
 export const getMarkdownCacheKey = (hash: string, locationProtocol: string): string => {
   if (supportsNormalCacheKey(locationProtocol)) {
     return `/markdown/${hash}`
   }
   // workaround for electron bug
-  return `https://markdown/${hash}`
+  return `https://-/markdown/${hash}`
 }
