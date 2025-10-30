244d0c475d1b8df2eac5b589cb4963a8bfac54ca
Le Vivilet <levivilet@pm.me>
2025-10-30 16:02:08 +0100

    feature: add json validation link e2e test (#961)

---
+++ packages/e2e/src/extension-detail.feature-json-validation-schema-link.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'extension-detail.feature-json-validation'
+
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-json-validation-schema-link')
+  await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.json-validation-schema-link')
+  await ExtensionDetail.selectFeatures()
+
+  // act
+  await ExtensionDetail.openFeature('JsonValidation')
+
+  // assert
+  const heading = Locator('.FeatureContent h1')
+  await expect(heading).toBeVisible()
+  await expect(heading).toHaveText('Json Validation')
+  const commandsTable = Locator('.FeatureContent .Table')
+  await expect(commandsTable).toBeVisible()
+  const cell2 = commandsTable.locator('tbody td').nth(1)
+  await expect(cell2).toHaveText('./schema.json')
+}
