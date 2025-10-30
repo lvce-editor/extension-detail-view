e8418e6e3366bc2d3ae977b228b88012e3ace840
Le Vivilet <levivilet@pm.me>
2025-10-30 11:42:53 +0100

    feature: update markdown cache key so that it works in electron (#953)

--- packages/extension-detail-view-worker/src/parts/RenderMarkdownCached/RenderMarkdownCached.ts
+++ packages/extension-detail-view-worker/src/parts/RenderMarkdownCached/RenderMarkdownCached.ts
@@ -1,11 +1,12 @@
 import type { MarkdownOptions } from '../MarkdownOptions/MarkdownOptions.ts'
+import * as GetMarkdownCacheKey from '../GetMarkdownCacheKey/GetMarkdownCacheKey.ts'
 import { hash } from '../Hash/Hash.ts'
 import * as MarkDownCache from '../MarkDownCache/MarkDownCache.ts'
 import * as MarkdownWorker from '../MarkdownWorker/MarkdownWorker.ts'
 
 export const renderMarkdownCached = async (markdown: string, options: MarkdownOptions = {}): Promise<string> => {
   const markdownHash = await hash(markdown) // TODO hash options also
-  const cacheKey = `/markdown/${markdownHash}`
+  const cacheKey = GetMarkdownCacheKey.getMarkdownCacheKey(markdownHash)
   const hasItem = await MarkDownCache.has(cacheKey)
   if (hasItem) {
     const value = await MarkDownCache.get(cacheKey)
