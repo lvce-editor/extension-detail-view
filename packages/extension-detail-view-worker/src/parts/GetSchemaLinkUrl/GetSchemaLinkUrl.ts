c040a6518fd0378b27e5ae00aac34d65d64bb372
Le Vivilet <levivilet@pm.me>
2025-10-30 17:33:59 +0100

    feature: move schema function into separate file (#966)

---
+++ packages/extension-detail-view-worker/src/parts/GetSchemaLinkUrl/GetSchemaLinkUrl.ts
@@ -0,0 +1,10 @@
+const isExternalLink = (schema: string): boolean => {
+  return schema.startsWith('http://') || schema.startsWith('https://')
+}
+
+export const getSchemaLinkUrl = (schema: string, extensionUri: string): string => {
+  if (isExternalLink(schema)) {
+    return schema
+  }
+  return new URL(schema, extensionUri).toString()
+}
