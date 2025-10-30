95159d94645eae1ea6537ca3f62311c7145c40c3
Le Vivilet <levivilet@pm.me>
2025-10-30 11:55:59 +0100

    simplify

--- packages/extension-detail-view-worker/src/parts/GetMarkdownCacheKey/GetMarkdownCacheKey.ts
+++ packages/extension-detail-view-worker/src/parts/GetMarkdownCacheKey/GetMarkdownCacheKey.ts
@@ -1,5 +1,9 @@
+const supportsNormalCacheKey = (locationProtocol: string): boolean => {
+  return locationProtocol === 'http:' || locationProtocol === 'https:'
+}
+
 export const getMarkdownCacheKey = (hash: string, locationProtocol: string): string => {
-  if (locationProtocol === 'http:' || locationProtocol === 'https:') {
+  if (supportsNormalCacheKey(locationProtocol)) {
     return `/markdown/${hash}`
   }
   // workaround for electron bug
