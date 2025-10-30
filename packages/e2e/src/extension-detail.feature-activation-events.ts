6ec0e93fa6914bc766ad673852118cb7dafd62c5
Le Vivilet <levivilet@pm.me>
2025-10-30 21:07:13 +0100

    feature: add activation event e2e test (#984)
    * feature: add activation event e2e test
    
    * typo
    
    * unused

---
+++ packages/e2e/src/extension-detail.feature-activation-events.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'extension-detail.feature-activation-events'
+
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-activation-events')
+  await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.activation-events')
+  await ExtensionDetail.selectFeatures()
+
+  // act
+  await ExtensionDetail.openFeature('ActivationEvents')
+
+  // assert
+  const heading = Locator('.FeatureContent h1')
+  await expect(heading).toBeVisible()
+  await expect(heading).toHaveText('Activation Events')
+  const list = Locator('.FeatureContent ul')
+  await expect(list).toBeVisible()
+  const item1 = list.locator('li').nth(0)
+  await expect(item1).toHaveText('onWebview:builtin.chat-view')
+}
