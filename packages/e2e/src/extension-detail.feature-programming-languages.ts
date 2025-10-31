04c6059ce8b60473dd165e83073f4b2b338328c2
Le Vivilet <levivilet@pm.me>
2025-10-31 11:33:19 +0100

    feature: enable programming languages e2e test

--- packages/e2e/src/extension-detail.feature-programming-languages.ts
+++ packages/e2e/src/extension-detail.feature-programming-languages.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'extension-detail.feature-programming-languages'
 
-export const skip = 1
-
 export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/extension-programming-languages')
@@ -12,7 +10,7 @@ export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }
   await ExtensionDetail.selectFeatures()
 
   // act
-  await ExtensionDetail.openFeature('programming-languages')
+  await ExtensionDetail.openFeature('ProgrammingLanguages')
 
   // assert
   const heading = Locator('.FeatureContent h1')
@@ -23,9 +21,15 @@ export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }
   const heading1 = programmingLanguagesTable.locator('th').nth(0)
   await expect(heading1).toHaveText('ID')
   const heading2 = programmingLanguagesTable.locator('th').nth(1)
-  await expect(heading2).toHaveText('Label')
-  const cell1 = programmingLanguagesTable.locator('tbody td').nth(0)
-  await expect(cell1).toHaveText('test')
-  const cell2 = programmingLanguagesTable.locator('tbody td').nth(1)
-  await expect(cell2).toHaveText('Test')
+  await expect(heading2).toHaveText('Name')
+  const heading3 = programmingLanguagesTable.locator('th').nth(2)
+  await expect(heading3).toHaveText('File Extensions')
+  const heading4 = programmingLanguagesTable.locator('th').nth(3)
+  await expect(heading4).toHaveText('Grammar')
+  const heading5 = programmingLanguagesTable.locator('th').nth(4)
+  await expect(heading5).toHaveText('Snippets')
+  // const cell1 = programmingLanguagesTable.locator('tbody td').nth(0)
+  // await expect(cell1).toHaveText('test')
+  // const cell2 = programmingLanguagesTable.locator('tbody td').nth(1)
+  // await expect(cell2).toHaveText('Test')
 }
