b5e3439c06802616b1e04c5fda46460cf6994696
Le Vivilet <levivilet@pm.me>
2025-10-31 15:56:31 +0100

    feature: add handleReadmeClick (#1007)

---
+++ packages/extension-detail-view-worker/src/parts/HandleReadmeClick/HandleReadmeClick.ts
@@ -0,0 +1,6 @@
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+
+export const handleReadmeClick = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
+  // TODO check node name and href
+  return state
+}
