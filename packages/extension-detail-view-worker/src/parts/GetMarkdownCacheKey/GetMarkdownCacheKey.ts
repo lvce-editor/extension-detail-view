b7bce754e1d68ce54a47bcf5f0a1aad58bd4a016
Le Vivilet <levivilet@pm.me>
2025-10-31 15:49:47 +0100

    feature: include markdown options in markdown cache key (#1005)

--- packages/extension-detail-view-worker/src/parts/GetMarkdownCacheKey/GetMarkdownCacheKey.ts
+++ packages/extension-detail-view-worker/src/parts/GetMarkdownCacheKey/GetMarkdownCacheKey.ts
@@ -1,7 +1,16 @@
+import type { MarkdownOptions } from '../MarkdownOptions/MarkdownOptions.ts'
+import { hash } from '../Hash/Hash.ts'
 import { supportsNormalCacheKey } from '../SupportsNormalCacheKey/SupportsNormalCacheKey.ts'
 
-export const getMarkdownCacheKey = (hash: string, locationProtocol: string): string => {
-  if (supportsNormalCacheKey(locationProtocol)) {
+const getMarkdownCacheHash = async (markdown: string, options: MarkdownOptions): Promise<string> => {
+  const stringifiedOptions = JSON.stringify(options)
+  const contents = `${markdown}:${stringifiedOptions}`
+  return hash(contents)
+}
+
+export const getMarkdownCacheKey = async (markdown: string, options: MarkdownOptions): Promise<string> => {
+  const hash = await getMarkdownCacheHash(markdown, options)
+  if (supportsNormalCacheKey(options.locationProtocol)) {
     return `/markdown/${hash}`
   }
   // workaround for electron bug
--- packages/extension-detail-view-worker/src/parts/RenderMarkdownCached/RenderMarkdownCached.ts
+++ packages/extension-detail-view-worker/src/parts/RenderMarkdownCached/RenderMarkdownCached.ts
@@ -1,12 +1,10 @@
 import type { MarkdownOptions } from '../MarkdownOptions/MarkdownOptions.ts'
 import * as GetMarkdownCacheKey from '../GetMarkdownCacheKey/GetMarkdownCacheKey.ts'
-import { hash } from '../Hash/Hash.ts'
 import * as MarkDownCache from '../MarkDownCache/MarkDownCache.ts'
 import * as MarkdownWorker from '../MarkdownWorker/MarkdownWorker.ts'
 
 export const renderMarkdownCached = async (markdown: string, options: MarkdownOptions): Promise<string> => {
-  const markdownHash = await hash(markdown) // TODO hash options also
-  const cacheKey = GetMarkdownCacheKey.getMarkdownCacheKey(markdownHash, options.locationProtocol)
+  const cacheKey = await GetMarkdownCacheKey.getMarkdownCacheKey(markdown, options)
   const hasItem = await MarkDownCache.has(cacheKey)
   if (hasItem) {
     const value = await MarkDownCache.get(cacheKey)
