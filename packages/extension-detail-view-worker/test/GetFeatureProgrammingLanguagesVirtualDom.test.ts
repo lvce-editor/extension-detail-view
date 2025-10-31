d9a3f8538a13e54bc76be5c3d64fa3fedb0738f0
Le Vivilet <levivilet@pm.me>
2025-10-31 11:10:40 +0100

    tsc

--- packages/extension-detail-view-worker/test/GetFeatureProgrammingLanguagesVirtualDom.test.ts
+++ packages/extension-detail-view-worker/test/GetFeatureProgrammingLanguagesVirtualDom.test.ts
@@ -1,11 +1,13 @@
 import { expect, test } from '@jest/globals'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { Row } from '../src/parts/Row/Row.ts'
 import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
 import * as GetFeatureProgrammingLanguagesVirtualDom from '../src/parts/GetFeatureProgrammingLanguagesVirtualDom/GetFeatureProgrammingLanguagesVirtualDom.ts'
 import { text } from '../src/parts/VirtualDomHelpers/VirtualDomHelpers.ts'
 
 test('feature programming languages virtual dom', () => {
-  expect(GetFeatureProgrammingLanguagesVirtualDom.getFeatureProgrammingLanguagesVirtualDom()).toEqual([
+  const programmingLanguages: readonly Row[] = []
+  expect(GetFeatureProgrammingLanguagesVirtualDom.getFeatureProgrammingLanguagesVirtualDom(programmingLanguages)).toEqual([
     {
       type: VirtualDomElements.Div,
       className: ClassNames.FeatureContent,
