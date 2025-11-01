fcc66244d560d4a50d1dcb343fc7b4ae9d654025
Le Vivilet <levivilet@pm.me>
2025-11-01 14:25:03 +0100

    feature: enable multiple webviews e2e test (#1028)

--- packages/e2e/src/extension-detail.feature-webviews-multiple.ts
+++ packages/e2e/src/extension-detail.feature-webviews-multiple.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'extension-detail.feature-webviews-multiple'
 
-export const skip = 1
-
 export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/extension-webviews-multiple')
@@ -15,27 +13,24 @@ export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }
   await ExtensionDetail.openFeature('WebViews')
 
   // assert
-  const items = Locator('.DefinitionListItem')
-  const item1 = items.nth(0)
+  const features = Locator('.FeatureWebView')
+  await expect(features).toHaveCount(3)
+  const item1 = Locator('.FeatureWebView:nth-of-type(1) .DefinitionListItem:nth-of-type(1)')
   await expect(item1).toBeVisible()
   const item1Heading = item1.locator('.DefinitionListItemHeading')
-  await expect(item1Heading).toBeVisible()
   await expect(item1Heading).toHaveText('ID')
   const item1Value = item1.locator('.DefinitionListItemValue')
-  await expect(item1Value).toBeVisible()
-  await expect(item1Value).toHaveText('builtin.test-webviews')
-  const item2 = items.nth(1)
+  await expect(item1Value).toHaveText('builtin.test-webviews-1')
+  const item2 = Locator('.FeatureWebView:nth-of-type(2) .DefinitionListItem:nth-of-type(1)')
+  await expect(item2).toBeVisible()
   const item2Heading = item2.locator('.DefinitionListItemHeading')
-  await expect(item2Heading).toBeVisible()
-  await expect(item2Heading).toHaveText('Selector')
+  await expect(item2Heading).toHaveText('ID')
   const item2Value = item2.locator('.DefinitionListItemValue')
-  await expect(item2Value).toBeVisible()
-  await expect(item2Value).toHaveText('[".test-webviews"]')
-  const item3 = items.nth(2)
+  await expect(item2Value).toHaveText('builtin.test-webviews-2')
+  const item3 = Locator('.FeatureWebView:nth-of-type(3) .DefinitionListItem:nth-of-type(1)')
+  await expect(item3).toBeVisible()
   const item3Heading = item3.locator('.DefinitionListItemHeading')
-  await expect(item3Heading).toBeVisible()
-  await expect(item3Heading).toHaveText('ContentSecurityPolicy')
+  await expect(item3Heading).toHaveText('ID')
   const item3Value = item3.locator('.DefinitionListItemValue')
-  await expect(item3Value).toBeVisible()
-  await expect(item3Value).toHaveText(`["default-src 'none'","script-src 'self'"]`)
+  await expect(item3Value).toHaveText('builtin.test-webviews-3')
 }
