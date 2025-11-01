b159cf844767c077606ec6479417ec387c91c43c
Le Vivilet <levivilet@pm.me>
2025-11-01 14:31:28 +0100

    feature: render programming languages table cells (#1025)

--- packages/extension-detail-view-worker/src/parts/GetProgrammingLanguageTableEntry/GetProgrammingLanguageTableEntry.ts
+++ packages/extension-detail-view-worker/src/parts/GetProgrammingLanguageTableEntry/GetProgrammingLanguageTableEntry.ts
@@ -1,5 +1,33 @@
 import type { Row } from '../Row/Row.ts'
+import * as TableCellType from '../TableCellType/TableCellType.ts'
 
 export const getProgrammingLanguageTableEntry = (programmingLanguage: any): Row => {
-  return []
+  const { id, configuration, extensions } = programmingLanguage
+  const name = '' // TODO
+  const snippets = '' // TODO
+  return [
+    {
+      type: TableCellType.Text,
+      value: id,
+    },
+    {
+      type: TableCellType.Text,
+      value: name,
+    },
+    {
+      type: TableCellType.CodeList,
+      value: '',
+      listItems: extensions,
+    },
+    {
+      type: TableCellType.CheckMark,
+      value: '',
+      checked: Boolean(configuration),
+    },
+    {
+      type: TableCellType.CheckMark,
+      value: '',
+      checked: Boolean(snippets),
+    },
+  ]
 }
