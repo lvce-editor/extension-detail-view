689896af3be16089c2dfee670c4ca8d831b17f9b
Le Vivilet <levivilet@pm.me>
2025-11-01 18:22:05 +0100

    Discard changes to packages/extension-detail-view-worker/test/SendMessagePortToMarkdownWorker.test.ts

--- packages/extension-detail-view-worker/test/SendMessagePortToMarkdownWorker.test.ts
+++ packages/extension-detail-view-worker/test/SendMessagePortToMarkdownWorker.test.ts
@@ -1,17 +1,22 @@
-import { expect, test } from '@jest/globals'
+import { expect, test, jest } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
 import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import * as SendMessagePortToMarkdownWorker from '../src/parts/SendMessagePortToMarkdownWorker/SendMessagePortToMarkdownWorker.ts'
 
 test('sends message port to markdown worker', async () => {
   const mockPort = { id: 'test-port' }
+  const invokeAndTransfer = jest.fn(async () => {})
 
-  const mockRpc = RendererWorker.registerMockRpc({
-    sendMessagePortToMarkdownWorker: () => {
-      /**/
-    },
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: async () => {},
+    invokeAndTransfer,
   })
+  RendererWorker.set(mockRpc)
 
   await SendMessagePortToMarkdownWorker.sendMessagePortToMarkdownWorker(mockPort)
 
-  expect(mockRpc.invocations).toEqual([['sendMessagePortToMarkdownWorker', mockPort, 0]])
+  expect(invokeAndTransfer).toHaveBeenCalled()
+  const call = invokeAndTransfer.mock.calls[0] as unknown as [string, any, any]
+  expect(call[1]).toBe(mockPort)
 })
