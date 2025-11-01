38b204d078d592b35c064b54e18ae4421b77418b
Le Vivilet <levivilet@pm.me>
2025-11-01 14:47:30 +0100

    feature: update readme context menu function

--- packages/extension-detail-view-worker/src/parts/HandleReadmeContextMenu/HandleReadmeContextMenu.ts
+++ packages/extension-detail-view-worker/src/parts/HandleReadmeContextMenu/HandleReadmeContextMenu.ts
@@ -1,6 +1,15 @@
+import { MenuEntryId } from '@lvce-editor/constants'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
 
-export const handleReadmeContextMenu = async (state: ExtensionDetailState, nodeName: string, href: string): Promise<ExtensionDetailState> => {
+export const handleReadmeContextMenu = async (
+  state: ExtensionDetailState,
+  x: number,
+  y: number,
+  nodeName: string,
+  href: string,
+): Promise<ExtensionDetailState> => {
+  await ContextMenu.show(MenuEntryId.ExtensionDetailReadme, x, y)
   // TODO
   return state
 }
