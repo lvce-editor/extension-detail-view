89c32444573947b40761f8ba9f621fc67124fbf2
Le Vivilet <levivilet@pm.me>
2025-11-03 16:22:02 +0100

    feature: update image context menu

--- packages/extension-detail-view-worker/src/parts/HandleImageContextMenu/HandleImageContextMenu.ts
+++ packages/extension-detail-view-worker/src/parts/HandleImageContextMenu/HandleImageContextMenu.ts
@@ -3,6 +3,9 @@ import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
 import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 
 export const handleImageContextMenu = async (state: ExtensionDetailState, eventX: number, eventY: number): Promise<ExtensionDetailState> => {
-  await ContextMenu.show(MenuEntryId.ExtensionDetailIconContextMenu, eventX, eventY)
+  const { uid } = state
+  await ContextMenu.show2(uid, MenuEntryId.ExtensionDetailIconContextMenu, eventX, eventY, {
+    menuId: MenuEntryId.ExtensionDetailIconContextMenu,
+  })
   return state
 }
