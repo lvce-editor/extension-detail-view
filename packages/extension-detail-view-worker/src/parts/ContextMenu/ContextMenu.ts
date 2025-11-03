e9c70d5a3bfd366ea6ec4c6a9eafb3c1126a8f70
Le Vivilet <levivilet@pm.me>
2025-11-03 11:31:28 +0100

    feature: update function types

--- packages/extension-detail-view-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/extension-detail-view-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -4,7 +4,7 @@ export const show = async (menuId: any, x: number, y: number): Promise<void> => 
   await RendererWorker.showContextMenu(x, y, menuId)
 }
 
-export const show2 = async (uid: number, menuId: any, x: number, y: number): Promise<void> => {
+export const show2 = async (uid: number, menuId: any, x: number, y: number, args: any): Promise<void> => {
   // @ts-ignore
-  await RendererWorker.invoke('ContextMenu.show2', uid, menuId, x, y)
+  await RendererWorker.invoke('ContextMenu.show2', uid, menuId, x, y, args)
 }
