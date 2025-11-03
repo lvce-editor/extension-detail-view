cc36ce1e0da79a3498bfd727676209832e7a4a16
Le Vivilet <levivilet@pm.me>
2025-11-03 13:32:46 +0100

    feature: add context menu link test (#1082)

---
+++ packages/e2e/src/extension-detail.readme-context-menu-link.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-readme-context-menu')
+  await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.extension-readme-context-menu')
+  const detailView = Locator('.ExtensionDetail')
+  await expect(detailView).toBeVisible()
+  const markDown = Locator('.Markdown')
+  await expect(markDown).toBeVisible()
+
+  // act
+  await Command.execute('ExtensionDetail.handleReadmeContextMenu', 0, 0, 'a', 'https://example.com')
+
+  // assert
+  // TODO verify that context menu is open
+  // TODO should have copy link item
+}
