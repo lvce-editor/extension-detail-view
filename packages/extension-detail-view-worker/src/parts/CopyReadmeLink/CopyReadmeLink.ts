c9ed1ca9ac7551a43b5402b25dc5a52537965edd
Le Vivilet <levivilet@pm.me>
2025-11-03 11:43:26 +0100

    feature: implement copy link (#1080)

--- packages/extension-detail-view-worker/src/parts/CopyReadmeLink/CopyReadmeLink.ts
+++ packages/extension-detail-view-worker/src/parts/CopyReadmeLink/CopyReadmeLink.ts
@@ -1,6 +1,7 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import * as ClipBoard from '../Clipboard/Clipboard.ts'
 
-export const copyReadmeLink = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  // TODO
+export const copyReadmeLink = async (state: ExtensionDetailState, href: string): Promise<ExtensionDetailState> => {
+  await ClipBoard.writeText(href)
   return state
 }
