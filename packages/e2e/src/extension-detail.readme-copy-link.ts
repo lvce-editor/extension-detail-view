3ee981281de7c4b24f7408532ac45e338812ed54
Le Vivilet <levivilet@pm.me>
2025-11-03 11:42:54 +0100

    feature: add copy link e2e test (#1079)

---
+++ packages/e2e/src/extension-detail.readme-copy-link.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const skip = 1
+
+export const test: Test = async ({ ClipBoard, Command, Locator, expect, Extension, ExtensionDetail }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-readme-copy-link')
+  await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.extension-readme-copy-link')
+  const detailView = Locator('.ExtensionDetail')
+  await expect(detailView).toBeVisible()
+  const markDown = Locator('.Markdown')
+  await expect(markDown).toBeVisible()
+  await ClipBoard.enableMemoryClipBoard()
+
+  // act
+  await Command.execute('ExtensionDetail.copyReadmeLink', 'https://example.com')
+
+  // assert
+  await ClipBoard.shouldHaveText('https://example.com')
+}
