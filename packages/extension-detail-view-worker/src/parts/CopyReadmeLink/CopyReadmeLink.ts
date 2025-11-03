adb2fa4c3751a45288caef8766a97e7b8803d6e4
Le Vivilet <levivilet@pm.me>
2025-11-03 11:35:36 +0100

    feature: add copyReadmeLink

---
+++ packages/extension-detail-view-worker/src/parts/CopyReadmeLink/CopyReadmeLink.ts
@@ -0,0 +1,6 @@
+import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+
+export const copyReadmeLink = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
+  // TODO
+  return state
+}
