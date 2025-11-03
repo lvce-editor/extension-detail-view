961244f39a620201e22dc224ac17ee97cdb85c3b
Le Vivilet <levivilet@pm.me>
2025-11-03 16:13:43 +0100

    feature: move interface

--- packages/extension-detail-view-worker/src/parts/GetMenuEntries2/GetMenuEntries2.ts
+++ packages/extension-detail-view-worker/src/parts/GetMenuEntries2/GetMenuEntries2.ts
@@ -1,43 +1,17 @@
 import { MenuItemFlags } from '@lvce-editor/constants'
+import type { ContextMenuProps } from '../ContextMenuProps/ContextMenuProps.ts'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import * as ExtensionDetailStrings from '../ExtensionDetailStrings/ExtensionDetailStrings.ts'
 
-const getMenuEntriesReadmeLink = (props: ContextMenuProps): readonly MenuEntry[] => {
-  return [
-    {
-      id: 'copyLink',
-      label: ExtensionDetailStrings.copyLink(),
-      flags: MenuItemFlags.None,
-      command: 'ExtensionDetail.copyReadmeLink',
-      args: [props.href],
-    },
-  ]
-}
-
-const getMenuEntriesReadme = (props: ContextMenuProps): readonly MenuEntry[] => {
-  if (props.href) {
-    return getMenuEntriesReadmeLink(props)
-  }
+export const getMenuEntries2 = (state: ExtensionDetailState, props: ContextMenuProps): readonly MenuEntry[] => {
   return [
     {
       id: 'copy',
       label: ExtensionDetailStrings.copy(),
       flags: MenuItemFlags.None,
-      command: 'ExtensionDetail.copyReadmeText',
+      command: 'ExtensionDetail.executeCopy',
       args: [],
     },
   ]
-}
-
-// TODO maybe add props object as second property with properties like imagesrc, linksrc, nodeName
-
-interface ContextMenuProps {
-  readonly menuId: number
-  readonly nodeName: string
-  readonly href: string
-}
-
-export const getMenuEntries2 = (state: ExtensionDetailState, props: ContextMenuProps): readonly MenuEntry[] => {
-  return getMenuEntriesReadme(props)
 }
