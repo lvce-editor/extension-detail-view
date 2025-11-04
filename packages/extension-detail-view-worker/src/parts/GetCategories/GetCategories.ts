4127105c79f5428a4d96afff263ae1500a65466a
Le Vivilet <levivilet@pm.me>
2025-11-04 12:03:30 +0100

    feature: also display invalid categories

--- packages/extension-detail-view-worker/src/parts/GetCategories/GetCategories.ts
+++ packages/extension-detail-view-worker/src/parts/GetCategories/GetCategories.ts
@@ -1,16 +1,25 @@
 import type { Category } from '../Category/Category.ts'
 import { hasProperty } from '../HasProperty/HasProperty.ts'
 
+const stringifyCategory = (category: unknown): string => {
+  if (typeof category === 'string') {
+    return category
+  }
+  return JSON.stringify(category)
+}
+
+const toCategory = (categoryString: string): Category => {
+  return {
+    id: categoryString.toLowerCase(),
+    label: categoryString,
+  }
+}
+
 export const getCategories = (extension: unknown): readonly Category[] => {
   if (!hasProperty(extension, 'categories') || !Array.isArray(extension.categories)) {
     return []
   }
-  const categoryStrings = extension.categories.filter((item) => typeof item === 'string')
-  const categories = categoryStrings.map((categoryString) => {
-    return {
-      id: categoryString.toLowerCase(),
-      label: categoryString,
-    }
-  })
+  const categoryStrings = extension.categories.map(stringifyCategory)
+  const categories = categoryStrings.map(toCategory)
   return categories
 }
