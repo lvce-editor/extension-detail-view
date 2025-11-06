ce2d38c3e3d54794533aa5bbe8dbe156373fc838
Le Vivilet <levivilet@pm.me>
2025-11-06 23:17:55 +0100

    feature: move link dom into separate file

---
+++ packages/extension-detail-view-worker/src/parts/GetResourceLinkVirtualDom/GetResourceLinkVirtualDom.ts
@@ -0,0 +1,20 @@
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { Resource } from '../Resource/Resource.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+
+export const getResourceLinkVirtualDom = (resource: Resource): readonly VirtualDomNode[] => {
+  const { label, url } = resource
+  return [
+    {
+      type: VirtualDomElements.A,
+      className: ClassNames.Resource,
+      childCount: 1,
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      href: url,
+    },
+    text(label),
+  ]
+}
--- packages/extension-detail-view-worker/src/parts/GetResourceVirtualDom/GetResourceVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetResourceVirtualDom/GetResourceVirtualDom.ts
@@ -2,6 +2,7 @@ import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { Resource } from '../Resource/Resource.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { getResourceLinkVirtualDom } from '../GetResourceLinkVirtualDom/GetResourceLinkVirtualDom.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 const resourceNode: VirtualDomNode = {
@@ -14,17 +15,7 @@ const resourceNode: VirtualDomNode = {
 export const getResourceVirtualDom = (resource: Resource): readonly VirtualDomNode[] => {
   const { label, url } = resource
   if (url && url !== '#') {
-    return [
-      {
-        type: VirtualDomElements.A,
-        className: ClassNames.Resource,
-        childCount: 1,
-        target: '_blank',
-        rel: 'noopener noreferrer',
-        href: url,
-      },
-      text(label),
-    ]
+    return getResourceLinkVirtualDom(resource)
   }
   return [resourceNode, text(label)]
 }
