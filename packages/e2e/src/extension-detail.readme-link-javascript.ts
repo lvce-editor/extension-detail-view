bef1455ff5614fa0a588f7b76a2bc1d3334adeea
Le Vivilet <levivilet@pm.me>
2025-11-06 23:34:00 +0100

    feature: filter out javascript and mail links

--- packages/e2e/src/extension-detail.readme-link-javascript.ts
+++ packages/e2e/src/extension-detail.readme-link-javascript.ts
@@ -1,7 +1,5 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const skip = 1
-
 export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/extension-readme-link-javascript')
