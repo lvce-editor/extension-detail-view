881a6e0529e96cf67e0d2622f7520c53cbb3d746
Le Vivilet <levivilet@pm.me>
2025-11-07 20:19:15 +0100

    feature: enable disable extension e2e test

--- packages/e2e/src/extension-detail.disable-extension.ts
+++ packages/e2e/src/extension-detail.disable-extension.ts
@@ -2,20 +2,20 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'extension-detail.disable-extension'
 
-export const skip = 1
-
 export const test: Test = async ({ Command, Extension, ExtensionDetail, Locator, expect }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/extension-disable')
   await Extension.addWebExtension(extensionUri)
   await ExtensionDetail.open('test.extension-enable-error')
+  const disableButton = Locator('.ExtensionDetail [name="Disable"]')
+  await expect(disableButton).toBeVisible()
+  const enableButton = Locator('.ExtensionDetail [name="Enable"]')
+  await expect(enableButton).toBeHidden()
 
   // act
   await Command.execute('ExtensionDetail.handleClickDisable')
 
   // assert
-  const disableButton = Locator('.ExtensionDetail [name="Disable"]')
   await expect(disableButton).toBeHidden()
-  const enableButton = Locator('.ExtensionDetail [name="Enable"]')
   await expect(enableButton).toBeVisible()
 }
