25821f825cc608cf19defe84349b7f8e126bb505
Le Vivilet <levivilet@pm.me>
2025-11-10 10:20:04 +0100

    hide

--- packages/extension-detail-view-worker/src/parts/HideSizeLink/HideSizeLink.ts
+++ packages/extension-detail-view-worker/src/parts/HideSizeLink/HideSizeLink.ts
@@ -3,16 +3,18 @@ import type { MoreInfoEntry } from '../MoreInfoEntry/MoreInfoEntry.ts'
 import * as GetInstallationEntries from '../GetInstallationEntries/GetInstallationEntries.ts'
 
 export const hideSizeLink = (state: ExtensionDetailState): ExtensionDetailState => {
-  const { displaySize, extensionId, extensionVersion, extensionUri, showSizeLink } = state
+  const { displaySize, extensionId, extensionVersion, extensionUri } = state
+  const newShowSizeLink = false
   const installationEntries: readonly MoreInfoEntry[] = GetInstallationEntries.getInstallationEntries(
     displaySize,
     extensionId,
     extensionVersion,
     extensionUri,
-    showSizeLink,
+    newShowSizeLink,
   )
   return {
     ...state,
     installationEntries,
+    showSizeLink: newShowSizeLink,
   }
 }
