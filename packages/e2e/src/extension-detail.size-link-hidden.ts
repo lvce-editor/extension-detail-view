68b2d86c9561b5fa79d90564d3afd0a524d5314b
Le Vivilet <levivilet@pm.me>
2025-11-10 10:03:21 +0100

    feature: add link hidden e2e test (#1144)

---
+++ packages/e2e/src/extension-detail.size-link-hidden.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const skip = 1
+
+export const test: Test = async ({ Locator, expect, Extension, ExtensionDetail, Command }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-basics')
+  await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.extension-basics')
+
+  // act
+  await Command.execute('ExtensionDetail.hideSizeLink')
+
+  // assert
+  const additionalDetails = Locator('.AdditionalDetailsEntry:nth-of-type(1)')
+  await expect(additionalDetails).toBeVisible()
+  const moreInfoEntry = additionalDetails.locator('.MoreInfoEntry:nth-of-type(4)')
+  await expect(moreInfoEntry).toBeHidden()
+}
