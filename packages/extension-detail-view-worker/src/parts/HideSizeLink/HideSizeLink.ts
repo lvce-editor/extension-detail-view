7ec0f5908ca19ff6173923bdf0c55eafe5917895
Le Vivilet <levivilet@pm.me>
2025-11-10 10:10:31 +0100

    lint

--- packages/extension-detail-view-worker/src/parts/HideSizeLink/HideSizeLink.ts
+++ packages/extension-detail-view-worker/src/parts/HideSizeLink/HideSizeLink.ts
@@ -1,6 +1,6 @@
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
-import * as GetInstallationEntries from '../GetInstallationEntries/GetInstallationEntries.ts'
 import type { MoreInfoEntry } from '../MoreInfoEntry/MoreInfoEntry.ts'
+import * as GetInstallationEntries from '../GetInstallationEntries/GetInstallationEntries.ts'
 
 export const hideSizeLink = (state: ExtensionDetailState): ExtensionDetailState => {
   const { displaySize, extensionId, extensionVersion, extensionUri, showSizeLink } = state
