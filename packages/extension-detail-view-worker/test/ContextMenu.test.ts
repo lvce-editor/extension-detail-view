efb91e5d4bb58182e8de3df4203adceefd41639b
Le Vivilet <levivilet@pm.me>
2025-11-26 12:34:05 +0100

    feature: remove unused code

--- packages/extension-detail-view-worker/test/ContextMenu.test.ts
+++ packages/extension-detail-view-worker/test/ContextMenu.test.ts
@@ -2,22 +2,6 @@ import { expect, test } from '@jest/globals'
 import * as ContextMenu from '../src/parts/ContextMenu/ContextMenu.ts'
 import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 
-test('show calls showContextMenu with correct parameters', async () => {
-  const mockRpc = RendererWorker.registerMockRpc({
-    'ContextMenu.show': () => {
-      /**/
-    },
-  })
-
-  const menuId = 4091
-  const x = 100
-  const y = 200
-
-  await ContextMenu.show(menuId, x, y)
-
-  expect(mockRpc.invocations).toEqual([['ContextMenu.show', x, y, menuId]])
-})
-
 test('show2 calls invoke with correct parameters', async () => {
   const mockRpc = RendererWorker.registerMockRpc({
     'ContextMenu.show2': () => {
