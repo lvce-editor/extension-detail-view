bf0197d020bbabae3df959f836b5ddda390872e9
Le Vivilet <levivilet@pm.me>
2025-12-13 22:33:03 +0100

    feature: initialize extension management worker

--- packages/extension-detail-view-worker/src/parts/InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts
+++ packages/extension-detail-view-worker/src/parts/InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts
@@ -2,7 +2,11 @@ import { set } from '@lvce-editor/rpc-registry'
 import { createExtensionManagementWorkerRpc } from '../CreateExtensionManagementWorkerRpc/CreateExtensionManagementWorkerRpc.ts'
 
 export const initializeExtensionManagementWorker = async (): Promise<void> => {
-  const rpc = await createExtensionManagementWorkerRpc()
-  const id = 9006
-  set(id, rpc)
+  try {
+    const rpc = await createExtensionManagementWorkerRpc()
+    const id = 9006
+    set(id, rpc)
+  } catch {
+    // ignore
+  }
 }
