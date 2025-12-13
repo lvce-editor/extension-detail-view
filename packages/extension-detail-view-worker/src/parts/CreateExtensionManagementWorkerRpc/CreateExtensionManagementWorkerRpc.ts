62a20e10f48f4cede3211bc468ebbe1710110bc3
Le Vivilet <levivilet@pm.me>
2025-12-13 22:50:43 +0100

    feature: update sending port

--- packages/extension-detail-view-worker/src/parts/CreateExtensionManagementWorkerRpc/CreateExtensionManagementWorkerRpc.ts
+++ packages/extension-detail-view-worker/src/parts/CreateExtensionManagementWorkerRpc/CreateExtensionManagementWorkerRpc.ts
@@ -1,13 +1,12 @@
 import { type Rpc, TransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { VError } from '@lvce-editor/verror'
-import * as SendMessagePortToExtensionHostWorker from '../SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts'
 
 export const createExtensionManagementWorkerRpc = async (): Promise<Rpc> => {
   try {
     const rpc = await TransferMessagePortRpcParent.create({
       commandMap: {},
-      // @ts-ignore
-      send: SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionManagementWorker,
+      send: (port) => RendererWorker.sendMessagePortToExtensionManagementWorker(port, 0),
     })
     return rpc
   } catch (error) {
