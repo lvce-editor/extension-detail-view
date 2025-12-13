1a1ed86035dd96301ca86e482bf557a95bd2f6fd
Le Vivilet <levivilet@pm.me>
2025-12-13 22:35:18 +0100

    feature: initialize extension management worker (#1204)

--- packages/extension-detail-view-worker/src/parts/Initialize/Initialize.ts
+++ packages/extension-detail-view-worker/src/parts/Initialize/Initialize.ts
@@ -1,8 +1,14 @@
 import { initializeExtensionHostWorker } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
+import { initializeExtensionManagementWorker } from '../InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts'
 import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 import { initializeMarkdownWorker } from '../InitializeMarkdownWorker/InitializeMarkdownWorker.ts'
 
 export const initialize = async (): Promise<void> => {
   // TODO load markdown worker only when needed
-  await Promise.all([initializeMarkdownWorker(), initializeFileSystemWorker(), initializeExtensionHostWorker()])
+  await Promise.all([
+    initializeMarkdownWorker(),
+    initializeFileSystemWorker(),
+    initializeExtensionHostWorker(),
+    initializeExtensionManagementWorker(),
+  ])
 }
