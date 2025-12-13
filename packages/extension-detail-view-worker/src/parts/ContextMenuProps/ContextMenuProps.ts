cc739cbf35713277185947234145aca78b4002aa
Le Vivilet <levivilet@pm.me>
2025-12-13 11:06:33 +0100

    tsc

--- packages/extension-detail-view-worker/src/parts/ContextMenuProps/ContextMenuProps.ts
+++ packages/extension-detail-view-worker/src/parts/ContextMenuProps/ContextMenuProps.ts
@@ -8,6 +8,10 @@ export interface ContextMenuPropsExplorer extends ContextMenuPropsBase {
   readonly menuId: typeof MenuEntryId.Explorer
 }
 
+export interface ContextMenuPropsIcon extends ContextMenuPropsBase {
+  readonly menuId: typeof MenuEntryId.ExtensionDetailIconContextMenu
+}
+
 export interface ContextMenuPropsReadme extends ContextMenuPropsBase {
   readonly href: string
   readonly menuId: typeof MenuEntryId.ExtensionDetailReadme
