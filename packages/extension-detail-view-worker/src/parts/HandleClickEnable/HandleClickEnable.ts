9422435d330a906bcab4ab747f1db3745c7643e1
Le Vivilet <levivilet@pm.me>
2025-12-14 10:20:24 +0100

    log

--- packages/extension-detail-view-worker/src/parts/HandleClickEnable/HandleClickEnable.ts
+++ packages/extension-detail-view-worker/src/parts/HandleClickEnable/HandleClickEnable.ts
@@ -3,6 +3,5 @@ import { enableExtension } from '../EnableExtension/EnableExtension.ts'
 import { updateExtensionStatus } from '../UpdateExtensionStatus/UpdateExtensionStatus.ts'
 
 export const handleClickEnable = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  console.log('enable')
   return updateExtensionStatus(state, enableExtension)
 }
