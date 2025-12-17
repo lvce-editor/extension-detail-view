fb27bc4bcb74231412f3327aae700573168578cc
Le Vivilet <levivilet@pm.me>
2025-12-17 12:14:54 +0100

    update

--- packages/extension-detail-view-worker/src/parts/RenderFocus/RenderFocus.ts
+++ packages/extension-detail-view-worker/src/parts/RenderFocus/RenderFocus.ts
@@ -2,5 +2,10 @@ import { ViewletCommand } from '@lvce-editor/constants'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 
 export const renderFocus = (oldState: ExtensionDetailState, newState: ExtensionDetailState): readonly any[] => {
+  const { focus, focusedTabIndex, tabs, uid } = newState
+  if (focus === 451) {
+    const { name } = tabs[focusedTabIndex]
+    return [ViewletCommand.FocusElementByName, uid, name]
+  }
   return [ViewletCommand.FocusElementByName, '']
 }
