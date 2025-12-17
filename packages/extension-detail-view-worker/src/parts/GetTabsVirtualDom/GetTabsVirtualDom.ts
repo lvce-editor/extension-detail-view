9a585a88eb4114b8fa48742b1a8497afccd17ce8
Le Vivilet <levivilet@pm.me>
2025-12-17 12:01:42 +0100

    fix

--- packages/extension-detail-view-worker/src/parts/GetTabsVirtualDom/GetTabsVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetTabsVirtualDom/GetTabsVirtualDom.ts
@@ -4,18 +4,16 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetTabVirtualDom from '../GetTabVirtualDom/GetTabVirtualDom.ts'
-import * as InputName from '../InputName/InputName.ts'
 
 export const getTabsVirtualDom = (tabs: readonly Tab[], focusedTabIndex: number): readonly VirtualDomNode[] => {
-  const detailsAndFeaturesTabs = tabs.filter((tab) => tab.name === InputName.Details || tab.name === InputName.Features)
   return [
     {
-      childCount: detailsAndFeaturesTabs.length,
+      childCount: tabs.length,
       className: ClassNames.ExtensionDetailTabs,
       onClick: DomEventListenerFunctions.HandleTabsClick,
       role: AriaRoles.TabList,
       type: VirtualDomElements.Div,
     },
-    ...detailsAndFeaturesTabs.flatMap((tab, index) => GetTabVirtualDom.getTabVirtualDom(tab, index, focusedTabIndex)),
+    ...tabs.flatMap((tab, index) => GetTabVirtualDom.getTabVirtualDom(tab, index, focusedTabIndex)),
   ]
 }
