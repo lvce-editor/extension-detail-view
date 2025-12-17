cad14b5c57ba4104cb531189f76d5fd136648aec
Le Vivilet <levivilet@pm.me>
2025-12-17 12:12:10 +0100

    update code

--- packages/extension-detail-view-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
+++ packages/extension-detail-view-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
@@ -2,6 +2,10 @@ import { ViewletCommand } from '@lvce-editor/constants'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
 
 export const renderFocusContext = (oldState: ExtensionDetailState, newState: ExtensionDetailState): readonly any[] => {
+  const { focus, uid } = newState
+  if (focus === 451) {
+    return [ViewletCommand.SetFocusContext, uid, focus]
+  }
   // TODO set focus context
   return [ViewletCommand.FocusElementByName, '']
 }
