5d4b06601c2a0ac982b2f91716fabcb932b77b02
Le Vivilet <levivilet@pm.me>
2025-12-28 09:04:47 +0100

    fix

--- packages/extension-detail-view-worker/test/EnableExtension.test.ts
+++ packages/extension-detail-view-worker/test/EnableExtension.test.ts
@@ -1,24 +1,25 @@
 import { expect, test } from '@jest/globals'
 import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
 import * as EnableExtension from '../src/parts/EnableExtension/EnableExtension.ts'
+import * as PlatformType from '../src/parts/PlatformType/PlatformType.ts'
 
 test('enable extension', async () => {
   const mockRpc = ExtensionManagementWorker.registerMockRpc({
-    'Extensions.enable': () => {
+    'Extensions.enable2': () => {
       /**/
     },
   })
-  await EnableExtension.enableExtension('test-id')
-  expect(mockRpc.invocations).toEqual([['Extensions.enable', 'test-id']])
+  await EnableExtension.enableExtension('test-id', PlatformType.Electron)
+  expect(mockRpc.invocations).toEqual([['Extensions.enable2', 'test-id', PlatformType.Electron]])
 })
 
 test('handles error during enable', async () => {
   const error = new Error('Failed to enable extension')
   const mockRpc = ExtensionManagementWorker.registerMockRpc({
-    'Extensions.enable': async () => {
+    'Extensions.enable2': async () => {
       throw error
     },
   })
-  await expect(EnableExtension.enableExtension('test-id')).rejects.toThrow('Failed to enable extension')
-  expect(mockRpc.invocations).toEqual([['Extensions.enable', 'test-id']])
+  await expect(EnableExtension.enableExtension('test-id', PlatformType.Electron)).rejects.toThrow('Failed to enable extension')
+  expect(mockRpc.invocations).toEqual([['Extensions.enable2', 'test-id', PlatformType.Electron]])
 })
