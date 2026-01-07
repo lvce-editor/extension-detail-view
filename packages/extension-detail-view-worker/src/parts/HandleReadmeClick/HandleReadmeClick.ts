250e1fc758cf998712da63d1a1884182d8aeeac8
Le Vivilet <levivilet@pm.me>
2026-01-07 20:28:12 +0100

    feature: move function (#1246)

--- packages/extension-detail-view-worker/src/parts/HandleReadmeClick/HandleReadmeClick.ts
+++ packages/extension-detail-view-worker/src/parts/HandleReadmeClick/HandleReadmeClick.ts
@@ -1,5 +1,5 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
+import { handleReadmeLinkClick } from '../HandleReadmeLinkClick/HandleReadmeLinkClick.ts'
 
 const isExternalLink = (href: string): boolean => {
   return href.startsWith('http://') || href.startsWith('https://')
@@ -12,14 +12,6 @@ export const handleReadmeClick = async (state: ExtensionDetailState, nodeName: s
   }
   // TODO what to do about relative links? open them in editor?
   // TODO what to do about mail links?
-  if (linkProtectionEnabled) {
-    const message = `Do you want to open this external link?\n\n${href}`
-    const confirmed = await RendererWorker.confirm(message)
-    if (!confirmed) {
-      return state
-    }
-  }
-  await RendererWorker.openUrl(href)
-  // TODO check node name and href
+  await handleReadmeLinkClick(linkProtectionEnabled, href)
   return state
 }
