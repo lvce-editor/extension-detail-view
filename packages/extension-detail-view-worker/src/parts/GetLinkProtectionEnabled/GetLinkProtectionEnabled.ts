09ec20cbcb51676b7641b94f7a7b4cb373ed58d0
Le Vivilet <levivilet@pm.me>
2026-01-07 21:59:58 +0100

    fix

--- packages/extension-detail-view-worker/src/parts/GetLinkProtectionEnabled/GetLinkProtectionEnabled.ts
+++ packages/extension-detail-view-worker/src/parts/GetLinkProtectionEnabled/GetLinkProtectionEnabled.ts
@@ -1,15 +1,10 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const getLinkProtectionEnabled = async (): Promise<boolean> => {
-<<<<<<< HEAD
   try {
-    const setting = await RendererWorker.getPreference('extensionDetail.linkProtectionEnabled')
+    const setting = await RendererWorker.getPreference('application.linkProtectionEnabled')
     return setting === true || setting === 'true'
   } catch {
     return false
   }
-=======
-  const setting = await RendererWorker.getPreference('application.linkProtectionEnabled')
-  return setting === true || setting === 'true'
->>>>>>> origin/main
 }
