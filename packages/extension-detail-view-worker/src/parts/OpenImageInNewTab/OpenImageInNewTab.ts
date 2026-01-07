836e8e21e53553de24775753bf05b781f1fcacd6
Le Vivilet <levivilet@pm.me>
2026-01-07 21:03:49 +0100

    feature: handle openLinkExternal differently in electron (#1248)

--- packages/extension-detail-view-worker/src/parts/OpenImageInNewTab/OpenImageInNewTab.ts
+++ packages/extension-detail-view-worker/src/parts/OpenImageInNewTab/OpenImageInNewTab.ts
@@ -3,8 +3,8 @@ import { getImageCopyUrl } from '../GetImageCopyUrl/GetImageCopyUrl.ts'
 import * as OpenUrl from '../OpenExternal/OpenExternal.ts'
 
 export const openImageInNewTab = async (state: ExtensionDetailState): Promise<ExtensionDetailState> => {
-  const { iconSrc, locationHost, locationProtocol } = state
+  const { iconSrc, locationHost, locationProtocol, platform } = state
   const absoluteIconSrc = getImageCopyUrl(iconSrc, locationProtocol, locationHost)
-  await OpenUrl.openUrl(absoluteIconSrc)
+  await OpenUrl.openExternal(absoluteIconSrc, platform)
   return state
 }
