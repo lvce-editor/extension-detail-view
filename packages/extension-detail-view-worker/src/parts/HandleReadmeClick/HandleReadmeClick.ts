2eec8e043aac29c46be74956c9df209de511475a
Le Vivilet <levivilet@pm.me>
2026-01-07 20:18:26 +0100

    feature: in readme click, confirm before opening external links (#1244)

--- packages/extension-detail-view-worker/src/parts/HandleReadmeClick/HandleReadmeClick.ts
+++ packages/extension-detail-view-worker/src/parts/HandleReadmeClick/HandleReadmeClick.ts
@@ -6,11 +6,19 @@ const isExternalLink = (href: string): boolean => {
 }
 
 export const handleReadmeClick = async (state: ExtensionDetailState, nodeName: string, href: string): Promise<ExtensionDetailState> => {
+  const { linkProtectionEnabled } = state
   if (!href || !isExternalLink(href)) {
     return state
   }
   // TODO what to do about relative links? open them in editor?
   // TODO what to do about mail links?
+  if (linkProtectionEnabled) {
+    const message = `Do you want to open this external link?\n\n${href}`
+    const confirmed = await RendererWorker.confirm(message)
+    if (!confirmed) {
+      return state
+    }
+  }
   await RendererWorker.openUrl(href)
   // TODO check node name and href
   return state
