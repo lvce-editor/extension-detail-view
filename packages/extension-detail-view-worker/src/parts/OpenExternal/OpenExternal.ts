2d9e790198875daf592a5c283607cf59e26e2f3c
Le Vivilet <levivilet@pm.me>
2026-01-07 21:07:09 +0100

    feature: simplify openexternal

--- packages/extension-detail-view-worker/src/parts/OpenExternal/OpenExternal.ts
+++ packages/extension-detail-view-worker/src/parts/OpenExternal/OpenExternal.ts
@@ -1,10 +1,18 @@
 import { PlatformType } from '@lvce-editor/constants'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
+const openExternalElectron = async (uri: string): Promise<void> => {
+  await RendererWorker.openExternal(uri)
+}
+
+const openExternalWeb = async (uri: string): Promise<void> => {
+  await RendererWorker.openUrl(uri)
+}
+
 export const openExternal = async (uri: string, platform: number): Promise<void> => {
   if (platform === PlatformType.Electron) {
-    // TODo call electron openexternal
+    await openExternalElectron(uri)
   } else {
-    await RendererWorker.openUrl(uri)
+    await openExternalWeb(uri)
   }
 }
