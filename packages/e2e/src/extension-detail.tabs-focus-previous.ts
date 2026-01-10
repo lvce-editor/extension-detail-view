2f91fa2e649fc537a8c185c61f289b431e440559
Le Vivilet <levivilet@pm.me>
2026-01-10 13:55:08 +0100

    feature: add focusPrevious e2e test (#1289)

---
+++ packages/e2e/src/extension-detail.tabs-focus-previous.ts
@@ -0,0 +1,24 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'extension-detail.tabs-focus-previous'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, expect, Extension, ExtensionDetail, Locator }) => {
+  // arrange
+  const extensionUri = import.meta.resolve('../fixtures/extension-basics')
+  await Extension.addWebExtension(extensionUri)
+  await ExtensionDetail.open('test.extension-basics')
+  const tabDetails = Locator('.ExtensionDetailTab[name="Details"]')
+  await expect(tabDetails).toBeVisible()
+  await expect(tabDetails).toHaveAttribute('aria-selected', 'true')
+  await Command.execute('ExtensionDetail.handleTabFocus', 'Features')
+  const tabFeatures = Locator('.ExtensionDetailTab[name="Features"]')
+  await expect(tabFeatures).toBeFocused()
+
+  // act
+  await Command.execute('ExtensionDetail.focusPrevious')
+
+  // assert
+  await expect(tabDetails).toBeFocused()
+}
