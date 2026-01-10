4ff03ecbe218eabdf1601b8fcda4ea24dcef41eb
Le Vivilet <levivilet@pm.me>
2026-01-10 11:00:14 +0100

    feature: simplify functions (#1250)

--- packages/extension-detail-view-worker/src/parts/EnableExtension/EnableExtension.ts
+++ packages/extension-detail-view-worker/src/parts/EnableExtension/EnableExtension.ts
@@ -1,5 +1,5 @@
 import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
 
 export const enableExtension = (id: string, platform: number): Promise<any> => {
-  return ExtensionManagementWorker.invoke('Extensions.enable2', id, platform)
+  return ExtensionManagementWorker.enable2(id, platform)
 }
