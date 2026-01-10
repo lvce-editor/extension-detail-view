34f6e6cc82c5aa092673f8a92684bc69a570359f
Le Vivilet <levivilet@pm.me>
2026-01-10 11:04:05 +0100

    feature: if extension has badge property, group name in a separate span so that when selecting text, they are separated

--- packages/extension-detail-view-worker/src/parts/GetExtensionDetailNameVirtualDom/GetExtensionDetailNameVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailNameVirtualDom/GetExtensionDetailNameVirtualDom.ts
@@ -3,14 +3,36 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getNameBadgeVirtualDom } from '../GetNameBadgeVirtualDom/GetNameBadgeVirtualDom.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
-export const getExtensionDetailNameVirtualDom = (name: string, badge: string): readonly VirtualDomNode[] => {
+export const getExtensionDetailNameWithBadgeVirtualDom = (name: string, badge: string): readonly VirtualDomNode[] => {
   return [
     {
-      childCount: badge ? 2 : 1,
+      childCount: 2,
       className: ClassNames.ExtensionDetailName,
       type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 1,
+      type: VirtualDomElements.Span,
     },
     text(name),
     ...getNameBadgeVirtualDom(badge),
   ]
+}
+
+export const getExtensionDetailNameDefaultVirtualDom = (name: string): readonly VirtualDomNode[] => {
+  return [
+    {
+      childCount: 1,
+      className: ClassNames.ExtensionDetailName,
+      type: VirtualDomElements.Div,
+    },
+    text(name),
+  ]
+}
+
+export const getExtensionDetailNameVirtualDom = (name: string, badge: string): readonly VirtualDomNode[] => {
+  if (badge) {
+    return getExtensionDetailNameWithBadgeVirtualDom(name, badge)
+  }
+  return getExtensionDetailNameDefaultVirtualDom(name)
 }
