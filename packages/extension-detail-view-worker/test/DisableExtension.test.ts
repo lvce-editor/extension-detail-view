44e310b51fa98d6115d690ef1c8a8df2894eb237
Le Vivilet <levivilet@pm.me>
2026-01-10 13:33:39 +0100

    fix

--- packages/extension-detail-view-worker/test/DisableExtension.test.ts
+++ packages/extension-detail-view-worker/test/DisableExtension.test.ts
@@ -5,21 +5,21 @@ import * as PlatformType from '../src/parts/PlatformType/PlatformType.ts'
 
 test('disable extension', async () => {
   const mockRpc = ExtensionManagementWorker.registerMockRpc({
-    'Extensions.disable': () => {
+    'Extensions.disable2': () => {
       /**/
     },
   })
   await DisableExtension.disableExtension('test-id', PlatformType.Electron)
-  expect(mockRpc.invocations).toEqual([['Extensions.disable', 'test-id', PlatformType.Electron]])
+  expect(mockRpc.invocations).toEqual([['Extensions.disable2', 'test-id', PlatformType.Electron]])
 })
 
 test('handles error during disable', async () => {
   const error = new Error('Failed to disable extension')
   const mockRpc = ExtensionManagementWorker.registerMockRpc({
-    'Extensions.disable': async () => {
+    'Extensions.disable2': async () => {
       throw error
     },
   })
   await expect(DisableExtension.disableExtension('test-id', PlatformType.Electron)).rejects.toThrow('Failed to disable extension')
-  expect(mockRpc.invocations).toEqual([['Extensions.disable', 'test-id', PlatformType.Electron]])
+  expect(mockRpc.invocations).toEqual([['Extensions.disable2', 'test-id', PlatformType.Electron]])
 })
