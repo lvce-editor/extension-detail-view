464047d6cbc9f7cad5dbc113c48968a742b188aa
Le Vivilet <levivilet@pm.me>
2026-01-10 13:35:25 +0100

    update variable

--- packages/extension-detail-view-worker/src/parts/HandleTabFocus/HandleTabFocus.ts
+++ packages/extension-detail-view-worker/src/parts/HandleTabFocus/HandleTabFocus.ts
@@ -1,13 +1,12 @@
+import { WhenExpression } from '@lvce-editor/constants'
 import type { ExtensionDetailState } from '../ExtensionDetailState/ExtensionDetailState.ts'
-
-const focusId = 451
 
 export const handleTabFocus = (state: ExtensionDetailState, name: string): ExtensionDetailState => {
   const tabIndex = state.tabs.findIndex((tab) => tab.name === name)
   const newFocusedTabIndex = tabIndex === -1 ? state.focusedTabIndex : tabIndex
   return {
     ...state,
-    focus: focusId,
+    focus: WhenExpression.FocusExtensioNDetailTabs,
     focusedTabIndex: newFocusedTabIndex,
   }
 }
