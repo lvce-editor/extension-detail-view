fed4744129724550e051116f660bc1216dea46fa
Le Vivilet <levivilet@pm.me>
2026-05-27 00:21:50 +0200

    feature: simplify createMarkdownWorkerRpc by removing error handling (#1352)

--- packages/extension-detail-view-worker/src/parts/CreateMarkdownWorkerRpc/CreateMarkdownWorkerRpc.ts
+++ packages/extension-detail-view-worker/src/parts/CreateMarkdownWorkerRpc/CreateMarkdownWorkerRpc.ts
@@ -1,15 +1,10 @@
 import { type Rpc, LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
-import { VError } from '@lvce-editor/verror'
 import * as SendMessagePortToMarkdownWorker from '../SendMessagePortToMarkdownWorker/SendMessagePortToMarkdownWorker.ts'
 
 export const createMarkdownWorkerRpc = async (): Promise<Rpc> => {
-  try {
-    const rpc = await LazyTransferMessagePortRpcParent.create({
-      commandMap: {},
-      send: SendMessagePortToMarkdownWorker.sendMessagePortToMarkdownWorker,
-    })
-    return rpc
-  } catch (error) {
-    throw new VError(error, `Failed to create markdown worker rpc`)
-  }
+  const rpc = await LazyTransferMessagePortRpcParent.create({
+    commandMap: {},
+    send: SendMessagePortToMarkdownWorker.sendMessagePortToMarkdownWorker,
+  })
+  return rpc
 }
