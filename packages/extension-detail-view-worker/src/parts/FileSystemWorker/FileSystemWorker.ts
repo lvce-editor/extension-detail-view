f2a92ca454449533a5d0d0532d1d215c874c89f9
Le Vivilet <levivilet@pm.me>
2026-06-26 13:08:13 +0200

    fix worker knip issues (#1363)

--- packages/extension-detail-view-worker/src/parts/FileSystemWorker/FileSystemWorker.ts
+++ packages/extension-detail-view-worker/src/parts/FileSystemWorker/FileSystemWorker.ts
@@ -1,13 +1,10 @@
 /* eslint-disable unicorn/prefer-export-from */
-import type { MockRpc } from '@lvce-editor/rpc'
 import { FileSystemWorker } from '@lvce-editor/rpc-registry'
 
-export const { exists, invoke, readDirWithFileTypes, readFile, registerMockRpc, set } = FileSystemWorker
+export const { exists, invoke, readFile, registerMockRpc, set } = FileSystemWorker
 
 export const readFileAsBlob = async (uri: string): Promise<any> => {
   // TODO maybe readAsObjectUrl?
   // @ts-ignore
   return invoke('FileSystem.readFileAsBlob', uri)
 }
-
-export type { MockRpc }
