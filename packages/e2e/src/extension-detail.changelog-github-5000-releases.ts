88767a7bc87bffbc19c93771caa26c22dbce7d3d
Le Vivilet <levivilet@pm.me>
2026-07-11 10:38:20 +0200

    fix: reduce extreme changelog display limit (#1388)

--- packages/e2e/src/extension-detail.changelog-github-5000-releases.ts
+++ packages/e2e/src/extension-detail.changelog-github-5000-releases.ts
@@ -3,8 +3,8 @@ import { openGithubChangelog } from './_GithubReleaseTest.js'
 
 export const test: Test = async (api) => {
   await openGithubChangelog(api, { releaseCount: 5000, type: 'generated' })
-  await api.expect(api.Locator('.Changelog h1')).toHaveCount(1000)
-  await api.expect(api.Locator('.Changelog')).toContainText('Showing the newest 1000 of 5000 GitHub releases')
+  await api.expect(api.Locator('.Changelog h1')).toHaveCount(250)
+  await api.expect(api.Locator('.Changelog')).toContainText('Showing the newest 250 of 5000 GitHub releases')
   await api.expect(api.Locator('.Changelog')).toContainText('Version 5000')
-  await api.expect(api.Locator('.Changelog')).toContainText('Version 4001')
+  await api.expect(api.Locator('.Changelog')).toContainText('Version 4751')
 }
