7c7cd4f13f09649ec587e8b7139ae8114b09ce2c
Le Vivilet <levivilet@pm.me>
2026-07-24 15:55:12 +0000

    feature: migrate to npm workspaces

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -9,4 +9,4 @@ export const instantiationsPath = join(root, 'packages', 'extension-detail-view-
 
 export const workerPath = join(root, '.tmp/dist/dist/extensionDetailViewWorkerMain.js')
 
-export const playwrightPath = import.meta.resolve('../../e2e/node_modules/playwright/index.mjs')
+export const playwrightPath = new URL('../../../node_modules/playwright/index.mjs', import.meta.url).toString()
