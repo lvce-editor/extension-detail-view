ce78937bbdcf7a24e290f5770fb049fe8134b616
Le Vivilet <levivilet@pm.me>
2026-07-24 18:12:43 +0200

    feature: migrate build scripts to typescript (#1418)

---
+++ packages/build/src/build-static.ts
@@ -0,0 +1,43 @@
+import { cp, readFile, writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
+import { pathToFileURL } from 'node:url'
+import { root } from './root.ts'
+
+const main = async (): Promise<void> => {
+  const sharedProcessPath = join(root, 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
+
+  const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
+
+  const sharedProcess = await import(sharedProcessUrl)
+
+  process.env.PATH_PREFIX = '/extension-detail-view'
+  const { commitHash } = await sharedProcess.exportStatic({
+    root,
+    extensionPath: '',
+    testPath: 'packages/e2e',
+  })
+
+  const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+
+  const getRemoteUrl = (path: string): string => {
+    const url = pathToFileURL(path).toString().slice(8)
+    return `/remote/${url}`
+  }
+
+  const content = await readFile(rendererWorkerPath, 'utf8')
+  const workerPath = join(root, '.tmp/dist/dist/extensionDetailViewWorkerMain.js')
+  const remoteUrl = getRemoteUrl(workerPath)
+
+  const occurrence = `// const extensionDetailViewWorkerUrl = \`\${assetDir}/packages/extension-detail-view-worker/dist/extensionDetailViewWorkerMain.js\`
+const extensionDetailViewWorkerUrl = \`${remoteUrl}\``
+  const replacement = `const extensionDetailViewWorkerUrl = \`\${assetDir}/packages/extension-detail-view-worker/dist/extensionDetailViewWorkerMain.js\``
+  if (!content.includes(occurrence)) {
+    throw new Error('occurrence not found')
+  }
+  const newContent = content.replace(occurrence, replacement)
+  await writeFile(rendererWorkerPath, newContent)
+
+  await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
+}
+
+main()
