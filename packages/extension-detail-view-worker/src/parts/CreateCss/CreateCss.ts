ef5d7da7aa8b5b869fe5074cd443bcc22360146f
Le Vivilet <levivilet@pm.me>
2026-07-24 16:16:14 +0000

    fix: type css map callback

--- packages/extension-detail-view-worker/src/parts/CreateCss/CreateCss.ts
+++ packages/extension-detail-view-worker/src/parts/CreateCss/CreateCss.ts
@@ -1,11 +1,9 @@
-const mapEntry=([key, value])=>{
-  return  `  --${key}: ${value}px;`
-
+const mapEntry = ([key, value]: [string, number]): string => {
+  return `  --${key}: ${value}px;`
 }
+
 export const createCss = (object: Record<string, number>): string => {
-  const inner = Object.entries(object)
-    .map(mapEntry)
-    .join('\n')
+  const inner = Object.entries(object).map(mapEntry).join('\n')
   const full = `:root {\n${inner}\n}`
   return full
 }
