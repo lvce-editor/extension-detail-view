2e0b277ed9a7d0b7085520658a375c45e0c64170
Le Vivilet <levivilet@pm.me>
2026-07-24 18:23:32 +0200

    feature: add image not found test (#1365)

--- packages/e2e/src/extension-detail.readme-image-not-found.ts
+++ packages/e2e/src/extension-detail.readme-image-not-found.ts
@@ -1,8 +1,8 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const skip = 1
+export const name = 'extension-detail.readme-image-not-found'
 
-export const test: Test = async ({ expect, Extension, ExtensionDetail, Locator }) => {
+export const test: Test = async ({ Command, expect, Extension, ExtensionDetail, Locator }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/extension-readme-image-not-found')
   await Extension.addWebExtension(extensionUri)
@@ -15,7 +15,13 @@ export const test: Test = async ({ expect, Extension, ExtensionDetail, Locator }
   await expect(detailView).toBeVisible()
   const markDown = Locator('.Markdown')
   await expect(markDown).toBeVisible()
-  await expect(markDown).toHaveText('test readme\n\n')
-  const link = markDown.locator('img')
-  await expect(link).toHaveAttribute('onerror', null)
+  await expect(markDown).toContainText('test readme')
+  const image = markDown.locator('img[src="./not-found.png"]')
+  await expect(image).toBeVisible()
+  await expect(image).toHaveAttribute('onerror', null)
+  await Command.execute('ExtensionDetail.handleMarkdownImageError', './not-found.png')
+  const imageError = markDown.locator('.MarkdownImageError')
+  await expect(imageError).toBeVisible()
+  await expect(imageError).toHaveText('Image cannot be loaded')
+  await expect(image).toHaveCount(0)
 }
