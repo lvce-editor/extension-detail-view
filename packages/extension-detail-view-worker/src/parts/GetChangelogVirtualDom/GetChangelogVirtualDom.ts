ed95de36c1924491f126fa54636aaa867d9b82d5
Le Vivilet <levivilet@pm.me>
2026-07-29 08:59:29 +0000

    fix: hoist static virtual dom nodes

--- packages/extension-detail-view-worker/src/parts/GetChangelogVirtualDom/GetChangelogVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetChangelogVirtualDom/GetChangelogVirtualDom.ts
@@ -3,16 +3,15 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
+const changelogNode: VirtualDomNode = {
+  childCount: 1,
+  className: MergeClassNames.mergeClassNames(ClassNames.ExtensionDetailPanel, ClassNames.Changelog),
+  role: AriaRoles.Panel,
+  type: VirtualDomElements.Div,
+}
+
 export const getChangelogVirtualDom = (changelogDom: readonly VirtualDomNode[]): readonly VirtualDomNode[] => {
   // const notImplemented = ExtensionDetailStrings.notImplemented()
   // TODO set tabpanel role
-  return [
-    {
-      childCount: 1,
-      className: MergeClassNames.mergeClassNames(ClassNames.ExtensionDetailPanel, ClassNames.Changelog),
-      role: AriaRoles.Panel,
-      type: VirtualDomElements.Div,
-    },
-    ...changelogDom,
-  ]
+  return [changelogNode, ...changelogDom]
 }
--- packages/extension-detail-view-worker/src/parts/GetExtensionDetailErrorVirtualDom/GetExtensionDetailErrorVirtualDom.ts
+++ packages/extension-detail-view-worker/src/parts/GetExtensionDetailErrorVirtualDom/GetExtensionDetailErrorVirtualDom.ts
@@ -3,10 +3,22 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
+const extensionDetailErrorNode: VirtualDomNode = {
+  childCount: 1,
+  className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.ExtensionDetail, ClassNames.ExtensionDetailError),
+  type: VirtualDomElements.Div,
+}
+
 const extensionDetailErrorCardNode: VirtualDomNode = {
   childCount: 3,
   className: ClassNames.ExtensionDetailErrorCard,
   role: AriaRoles.Alert,
+  type: VirtualDomElements.Div,
+}
+
+const extensionDetailErrorIconNode: VirtualDomNode = {
+  childCount: 0,
+  className: MergeClassNames.mergeClassNames(ClassNames.MaskIcon, ClassNames.MaskIconWarning, ClassNames.ExtensionDetailErrorIcon),
   type: VirtualDomElements.Div,
 }
 
@@ -24,17 +36,9 @@ const extensionDetailErrorMessageNode: VirtualDomNode = {
 
 export const getExtensionDetailErrorVirtualDom = (title: string, message: string): readonly VirtualDomNode[] => {
   return [
-    {
-      childCount: 1,
-      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.ExtensionDetail, ClassNames.ExtensionDetailError),
-      type: VirtualDomElements.Div,
-    },
+    extensionDetailErrorNode,
     extensionDetailErrorCardNode,
-    {
-      childCount: 0,
-      className: MergeClassNames.mergeClassNames(ClassNames.MaskIcon, ClassNames.MaskIconWarning, ClassNames.ExtensionDetailErrorIcon),
-      type: VirtualDomElements.Div,
-    },
+    extensionDetailErrorIconNode,
     extensionDetailErrorTitleNode,
     text(title),
     extensionDetailErrorMessageNode,
