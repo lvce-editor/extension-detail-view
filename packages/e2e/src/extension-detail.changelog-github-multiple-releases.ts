159a240953b2235de1810fe031a13bf041b714b0
Le Vivilet <levivilet@pm.me>
2026-08-13 03:08:28 -0700

    fix: verify all GitHub releases are rendered (#1483)

--- packages/e2e/src/extension-detail.changelog-github-multiple-releases.ts
+++ packages/e2e/src/extension-detail.changelog-github-multiple-releases.ts
@@ -3,5 +3,7 @@ import { createRelease, openGithubChangelog } from './_GithubReleaseTest.js'
 
 export const test: Test = async (api) => {
   await openGithubChangelog(api, { body: [createRelease(), createRelease({ name: 'Version 0.9.0', tag_name: 'v0.9.0' })], type: 'success' })
-  await api.expect(api.Locator('.Changelog h1')).toHaveCount(2)
+  const releaseHeadings = api.Locator('.Changelog .Markdown > h1')
+  await api.expect(releaseHeadings).toHaveCount(2)
+  await api.expect(releaseHeadings.nth(1)).toBeVisible()
 }
