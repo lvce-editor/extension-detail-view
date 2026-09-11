492b82f0370d9224003ce040b53330c15b87294f
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-11 15:02:03 +0000

    fix: add codes to extension detail errors

--- packages/extension-detail-view-worker/src/parts/ExtensionNotFoundError/ExtensionNotFoundError.ts
+++ packages/extension-detail-view-worker/src/parts/ExtensionNotFoundError/ExtensionNotFoundError.ts
@@ -1,4 +1,8 @@
+import * as ErrorCodes from '../ErrorCodes/ErrorCodes.ts'
+
 export class ExtensionNotFoundError extends Error {
+  readonly code = ErrorCodes.E_EXTENSION_NOT_FOUND
+
   constructor(extensionId: string) {
     super(`extension not found: ${extensionId}`)
     this.name = 'ExtensionNotFoundError'
